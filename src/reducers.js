0dd4150f852e6734db4152b5150fb36b74d6eb67
nimrodshn <user@example.com>
2018-07-29 15:04:52 +0300

    Create cluster: submit button sends request - mock data

--- src/reducers.js
+++ src/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import clusterList from './ducks/clusterlist';
 import userProfile from './ducks/users';
 import clusterDetails from './ducks/clusterdetails';
+import createCluster from './ducks/createcluster';
 
 const reducers = combineReducers({
   clusterList,
   userProfile,
   clusterDetails,
+  createCluster,
 });
 
 export default reducers;
