c52170278d5bbd39a9e23ec32d1454c1a77838a4
Elad Alfassa <user@example.com>
2018-08-13 12:56:20 +0300

    move clusterDetails redux code to the new directory structure

--- src/reducers.js
+++ src/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import clusterList from './ducks/clusterlist';
 import userProfile from './ducks/users';
-import clusterDetails from './ducks/clusterdetails';
+import clusterDetails from './reducers/clusterDetails';
 import createCluster from './ducks/createcluster';
 import clusterListFilters from './reducers/clusterListFilter';
 
