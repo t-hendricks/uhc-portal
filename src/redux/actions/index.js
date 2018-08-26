5e96515745eaee4d2d653914b6943405ff1aaf44
Elad Alfassa <user@example.com>
2018-08-26 10:58:35 +0300

    remove ClusterDetails actions import
    ClusterDetails actions no longer exists, they're part of clusterActions.

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,7 @@
 import clusterActions from './clusterActions';
-import clusterDetails from './clusterDetails';
 import clusterListFilter from './clusterListFilter';
 import userActions from './userActions';
 
 export {
-  clusterActions, clusterDetails, clusterListFilter, userActions,
+  clusterActions, clusterListFilter, userActions,
 };
