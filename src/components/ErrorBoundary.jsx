c8ef3ac43f7bf84e3d0c9ca03c94d16c45b65c5e
Beni Cherniavsky-Paskin <user@example.com>
2018-10-10 18:02:41 +0300

    ErrorBoundary: show the error string as well
    Also improve styling

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -7,24 +7,26 @@ import {
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { error: null };
+    this.state = { error: null, componentStack: null };
   }
 
   componentDidCatch(error, info) {
-    this.setState({ error: info });
+    this.setState({ error: error.toString(), componentStack: info.componentStack });
   }
 
   render() {
-    const { error } = this.state;
+    const { error, componentStack } = this.state;
     if (error) {
       return (
         // Fallback UI
         <EmptyState>
           <Alert type="error">
-            Something went wrong:
-            <pre>
-              {error.componentStack}
-            </pre>
+            <h3>Something went wrong:</h3>
+            <div style={{ 'white-space': 'pre-wrap', 'text-align': 'left', 'font-family': 'monospace' }}>
+              {error}
+
+              {componentStack}
+            </div>
           </Alert>
         </EmptyState>
       );
