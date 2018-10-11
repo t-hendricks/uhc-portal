8a1f89f8b06217a8c41260831950f2678db5e91f
Elad Alfassa <user@example.com>
2018-10-11 17:50:26 +0300

    Add /cluster/create route for the OCP cluster creation
    And an empty component so people can get started

---
+++ src/components/clusters/InstallCluster.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class InstallCluster extends Component {
+    render() {
+        return <h1>TODO - Implement this dialog</h1>
+    }
+}
+
+export default InstallCluster;
