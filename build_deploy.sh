2eb79e6fd195a91654c3b950feee602571d98660
Juan Hernandez <user@example.com>
2018-11-05 17:16:27 +0100

    Prepare Jenkins scripts for building Go
    In order to build the Go programs that will be introduced by later
    patches, it is necessary to run the build process in a directory that is
    in the Go path. This patch makes the required changes to the
    `pr_check.sh` and `build_deploy.sh` scripts.
    
    Signed-off-by: Juan Hernandez <jhernand@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -66,6 +66,21 @@ fi
 # Set the directory for docker configuration:
 DOCKER_CONFIG="${PWD}/.docker"
 
+# Create the project directory inside the Go path and copy all the files of
+# the project:
+PROJECT="${GOPATH}/src/gitlab.cee.redhat.com/service/uhc-portal"
+mkdir -p "${PROJECT}"
+rsync -ap \
+  --exclude=.gopath \
+  --exclude=.git \
+  . "${PROJECT}"
+cd "${PROJECT}"
+
+# Enable the dep cache:
+if [ -n "${JENKINS_HOME}" ]; then
+  export DEPCACHEDIR="${JENKINS_HOME}/.cache/dep"
+fi
+
 # Build the image:
 make \
   build_id="${BUILD_ID}" \
