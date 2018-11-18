39329f4f89f08d8d06c4d42dc85c5697428e1d3b
Elad Alfassa <user@example.com>
2018-11-18 17:22:24 +0200

    Remove old navigation
    In the previous sprint, we hid the navigation bar using a CSS hack,
    because the fate of the navigation was not decided yet.
    
    Since then, the fate of navigation has been decided: if the user
    goes through the try.openshift.com flow, they should get no navigation,
    and if the user comes through cloud.openshift.com, they should
    get navigation.
    
    However, there's no point to show the user any navigation until
    we actually have more screens to navigate to.
    
    This commit removes the old navigation code, so that there's no
    longer a big gap on the left side on the screen, and the code
    is much cleaner.
    
    We'll add navigation back when we'll add more screens.

---
+++ src/components/header.jsx
@@ -0,0 +1,63 @@
+/*
+Copyright (c) 2018 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Dropdown, Icon, MenuItem, Masthead,
+} from 'patternfly-react';
+import helpers from '../common/helpers';
+import rhProductTitle from '../styles/images/logo.svg';
+
+const Header = ({
+  isLoggedIn, userProfile, logoutUser,
+}) => (
+  <Masthead titleImg={rhProductTitle} navToggle={false}>
+    {isLoggedIn && (
+      <nav className="collapse navbar-collapse">
+        <ul className="navbar-iconic nav navbar-nav navbar-right">
+          <Dropdown componentClass="li" id="user">
+            <Dropdown.Toggle useAnchor className="nav-item-iconic">
+              <Icon type="pf" name="user" />
+              <span className="dropdown-title">
+                {userProfile.name || userProfile.preferred_username}
+              </span>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <MenuItem onClick={logoutUser}>
+                Log out
+              </MenuItem>
+            </Dropdown.Menu>
+          </Dropdown>
+        </ul>
+      </nav>
+    )}
+  </Masthead>
+);
+
+Header.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  userProfile: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func,
+};
+
+Header.defaultProps = {
+  logoutUser: helpers.noop,
+  isLoggedIn: false,
+};
+
+export default Header;
