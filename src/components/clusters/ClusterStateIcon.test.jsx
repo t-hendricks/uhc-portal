433db943fe311f6560e17de0fb7bba642b4691c6
Elad Alfassa <user@example.com>
2019-01-02 11:37:06 +0200

    RefreshButton, ClusterStateIcon: add tests
    More basic tests for some of our simpler components

---
+++ src/components/clusters/ClusterStateIcon.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ClusterStateIcon from './ClusterStateIcon';
+
+describe('<ClusterStateIcon />', () => {
+  it('renders correctly for unknown status', () => {
+    const wrapper = shallow(<ClusterStateIcon clusterState="whatever" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders correctly for pending status', () => {
+    const wrapper = shallow(<ClusterStateIcon clusterState="pending" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders correctly for installing status', () => {
+    const wrapper = shallow(<ClusterStateIcon clusterState="installing" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders correctly for error status', () => {
+    const wrapper = shallow(<ClusterStateIcon clusterState="error" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders correctly for ready status', () => {
+    const wrapper = shallow(<ClusterStateIcon clusterState="ready" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders correctly for uninstalling status', () => {
+    const wrapper = shallow(<ClusterStateIcon clusterState="uninstalling" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
