11f505b8c93717b55ce624af46bc8d5d6ff5c521
lizagilman <user@example.com>
2019-06-05 14:10:26 +0300

    Move CA field to textarea

--- src/components/clusters/ClusterDetails/components/IdentityProvidersModal/components/OpenIDForm.jsx
+++ src/components/clusters/ClusterDetails/components/IdentityProvidersModal/components/OpenIDForm.jsx
@@ -15,6 +15,8 @@ function OpenIDForm({ createIDPResponse }) {
         placeholder="CA"
         helpText="PEM encoded certificate bundle to use to validate server certificates for the configured URL"
         disabled={createIDPResponse.pending}
+        className="ca-textarea"
+        componentClass="textarea"
       />
       <Field
         component={ReduxVerticalFormGroup}
