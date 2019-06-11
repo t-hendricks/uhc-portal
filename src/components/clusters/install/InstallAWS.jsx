94a27aacab3d756702a67a263b1e0440104b9d02
Greg Sheremeta <user@example.com>
2019-06-11 09:29:38 -0400

    AWS instructions scrolling refactor
    Move `scrollToTop()` from InstructionsAWS to InstallAWS
    to match the other components.

--- src/components/clusters/install/InstallAWS.jsx
+++ src/components/clusters/install/InstallAWS.jsx
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { tollboothActions } from '../../../redux/actions';
 import InstructionsAWS from './components/instructions/InstructionsAWS';
+import { scrollToTop } from '../../../common/helpers';
 
 
 class InstallAWS extends Component {
   componentDidMount() {
+    scrollToTop();
     document.title = 'Install OpenShift 4 | Red Hat OpenShift Cluster Manager | AWS';
 
     const { dispatch } = this.props;
