745185e16943bb57a781c248279cb1de6ff8b12b
lizagilman <user@example.com>
2019-06-18 14:33:51 +0300

    LDAP IDP - form validations

--- src/components/clusters/ClusterDetails/components/IdentityProvidersModal/components/LDAPForm.jsx
+++ src/components/clusters/ClusterDetails/components/IdentityProvidersModal/components/LDAPForm.jsx
@@ -5,29 +5,52 @@ import { Field } from 'redux-form';
 import ReduxCheckbox from '../../../../../common/ReduxFormComponents/ReduxCheckbox';
 import ReduxVerticalFormGroup from '../../../../../common/ReduxFormComponents/ReduxVerticalFormGroup';
 
-function LDAPForm({ createIDPResponse }) {
-  return (
-    <React.Fragment>
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="ldap_ca"
-        label="CA"
-        type="text"
-        placeholder="CA"
-        helpText="PEM encoded certificate bundle to use to validate server certificates for the configured URL"
-        disabled={createIDPResponse.pending}
-        className="ca-textarea"
-        componentClass="textarea"
-        spellcheck="false"
-      />
-      <Field
-        component={ReduxCheckbox}
-        name="ldap_insecure"
-        label="insecure"
-        disabled={createIDPResponse.pending}
-      />
-    </React.Fragment>
-  );
+class LDAPForm extends React.Component {
+  state = this.getInitialState();
+
+  getInitialState() {
+    return ({
+      isInsecure: false,
+      caDisabledHelpText: '',
+    });
+  }
+
+  toggleCADisabled = (e, value) => {
+    if (value) {
+      this.setState({ isInsecure: true, caDisabledHelpText: 'Cannot be used if insecure is set' });
+    } else {
+      this.setState(this.getInitialState());
+    }
+  };
+
+  render() {
+    const { createIDPResponse } = this.props;
+    const { isInsecure, caDisabledHelpText } = this.state;
+
+    return (
+      <React.Fragment>
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="ldap_ca"
+          label="CA"
+          type="text"
+          placeholder="CA"
+          helpText={`PEM encoded certificate bundle to use to validate server certificates for the configured URL ${caDisabledHelpText}.`}
+          disabled={isInsecure || createIDPResponse.pending}
+          className="ca-textarea"
+          componentClass="textarea"
+          spellcheck="false"
+        />
+        <Field
+          component={ReduxCheckbox}
+          name="ldap_insecure"
+          label="insecure"
+          disabled={createIDPResponse.pending}
+          onChange={this.toggleCADisabled}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 LDAPForm.propTypes = {
--- src/components/clusters/ClusterDetails/components/IdentityProvidersModal/components/LDAPFormRequired.jsx
+++ src/components/clusters/ClusterDetails/components/IdentityProvidersModal/components/LDAPFormRequired.jsx
@@ -6,70 +6,89 @@ import ReduxVerticalFormGroup from '../../../../../common/ReduxFormComponents/Re
 
 import validators from '../../../../../../common/validators';
 
-function LDAPFormRequired({ createIDPResponse }) {
-  return (
-    <React.Fragment>
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="bind_dn"
-        label="Bind DN"
-        type="text"
-        placeholder="Bind DN"
-        disabled={createIDPResponse.pending}
-      />
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="bind_password"
-        label="Bind Password"
-        type="password"
-        placeholder="Bind Password"
-        disabled={createIDPResponse.pending}
-      />
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="ldap_url"
-        label="LDAP url"
-        type="text"
-        placeholder="url"
-        disabled={createIDPResponse.pending}
-        validate={validators.required}
-      />
-      <h4>Attributes</h4>
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="ldap_id"
-        label="ID"
-        type="text"
-        placeholder="comma separated, example: 'id1, id-2"
-        disabled={createIDPResponse.pending}
-        validate={validators.required}
-      />
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="ldap_email"
-        label="Email"
-        type="text"
-        placeholder="comma separated, example: ***REMOVED***, ***REMOVED***"
-        disabled={createIDPResponse.pending}
-      />
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="ldap_name"
-        label="Name"
-        type="text"
-        placeholder="comma separated, example: 'name1, name2"
-        disabled={createIDPResponse.pending}
-      />
-      <Field
-        component={ReduxVerticalFormGroup}
-        name="ldap_preferred_username"
-        label="Preferred Username"
-        type="text"
-        placeholder="comma separated, example: 'name1, name2, name3"
-        disabled={createIDPResponse.pending}
-      />
-    </React.Fragment>
-  );
+class LDAPFormRequired extends React.Component {
+  state={
+    hasBindDN: false,
+  }
+
+  toggleBindPasswordDisabled = (e, value) => {
+    if (value) {
+      this.setState({ hasBindDN: true });
+    } else {
+      this.setState({ hasBindDN: false });
+    }
+  }
+
+  render() {
+    const { createIDPResponse } = this.props;
+    const { hasBindDN } = this.state;
+
+    return (
+      <React.Fragment>
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="bind_dn"
+          label="Bind DN"
+          type="text"
+          placeholder="Bind DN"
+          disabled={createIDPResponse.pending}
+          onChange={this.toggleBindPasswordDisabled}
+        />
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="bind_password"
+          label="Bind Password"
+          type="password"
+          placeholder="Bind Password"
+          helpText={!hasBindDN ? 'Cannot be used if Bind DN is not set' : ''}
+          disabled={!hasBindDN || createIDPResponse.pending}
+        />
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="ldap_url"
+          label="LDAP url"
+          type="text"
+          placeholder="url"
+          disabled={createIDPResponse.pending}
+          validate={validators.required}
+        />
+        <h4>Attributes</h4>
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="ldap_id"
+          label="ID"
+          type="text"
+          placeholder="comma separated, example: 'id1, id-2"
+          disabled={createIDPResponse.pending}
+          validate={validators.required}
+        />
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="ldap_email"
+          label="Email"
+          type="text"
+          placeholder="comma separated, example: ***REMOVED***, ***REMOVED***"
+          disabled={createIDPResponse.pending}
+        />
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="ldap_name"
+          label="Name"
+          type="text"
+          placeholder="comma separated, example: 'name1, name2"
+          disabled={createIDPResponse.pending}
+        />
+        <Field
+          component={ReduxVerticalFormGroup}
+          name="ldap_preferred_username"
+          label="Preferred Username"
+          type="text"
+          placeholder="comma separated, example: 'name1, name2, name3"
+          disabled={createIDPResponse.pending}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 LDAPFormRequired.propTypes = {
