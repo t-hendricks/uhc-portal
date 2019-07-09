5d718c2aa38a47853b5b1b5b64aa6a846835c26e
lizagilman <user@example.com>
2019-07-09 17:35:03 +0300

    Move to pf4 tabs

---
+++ src/components/clusters/ClusterDetails/__test__/TabsRow.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TabsRow from '../components/TabsRow';
+
+describe('<TabsRow />', () => {
+  let wrapper;
+  beforeEach(() => {
+    const mockRef = { current: null };
+    const props = { overviewTabRef: mockRef, usersTabRef: mockRef, logsTabRef: mockRef };
+
+    wrapper = shallow(
+      <TabsRow {...props} />,
+    );
+  });
+
+  it('should render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should display users tab and logs tab', () => {
+    wrapper.setProps({ displayLogs: true, displayUsersTab: true }, () => {
+      expect(wrapper.find('ForwardRef').length).toEqual(3);
+    });
+  });
+});
