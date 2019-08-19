2f9cf71c56c07fce2ccf51a7c79b3f24fafbfc7e
Victor Kareh <user@example.com>
2019-08-19 10:02:35 -0400

    Avoid breaking on API change
    Since the backend will change with uhc-clusters-service!1157, we want to
    make sure that the website does not break in case it gets deployed
    first. This change allows for this component to be used with either
    version of the API and can be safely reverted once the API is deployed.

--- src/common/normalize.js
+++ src/common/normalize.js
@@ -14,6 +14,16 @@ const normalizeCluster = (cluster) => {
       },
     };
   }
+
+  // Convert data from older backend
+  // See https://gitlab.cee.redhat.com/service/uhc-clusters-service/merge_requests/1175
+  if (!result.metrics.upgrade) {
+    result.metrics.upgrade = {};
+    if (result.metrics.version_update_available) {
+      result.metrics.upgrade.available = result.metrics.version_update_available;
+    }
+  }
+
   return result;
 };
 
