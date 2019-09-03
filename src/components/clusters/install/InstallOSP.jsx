7382e682928396e63c64279b01b5e4ce768fb727
Justin Pierce <user@example.com>
2019-09-03 14:25:59 -0400

    Adding OSP

---
+++ src/components/clusters/install/InstallOSP.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { tollboothActions } from '../../../redux/actions';
+import InstructionsOSP from './components/instructions/InstructionsOSP';
+import { scrollToTop } from '../../../common/helpers';
+
+class InstallOSP extends Component {
+  componentDidMount() {
+    scrollToTop();
+    document.title = 'Install OpenShift 4 | Red Hat OpenShift Cluster Manager | OSP';
+
+    const { dispatch } = this.props;
+    dispatch(tollboothActions.createAuthToken());
+  }
+
+  render() {
+    const { token } = this.props;
+
+    return (
+      <InstructionsOSP token={token} />);
+  }
+}
+
+InstallOSP.propTypes = {
+  token: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({ token: state.tollbooth.token });
+
+export default connect(mapStateToProps)(InstallOSP);
