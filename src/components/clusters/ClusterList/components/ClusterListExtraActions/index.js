c9fb38826aa3621d97fd8e8e6c06c3622cae0b98
Elad Alfassa <user@example.com>
2019-11-05 14:04:27 +0200

    ArchivedClusterList: invalidate on mount
    The usual flow for getting into archived clusters calls
    invalidateClusters() when clicking on the menu item, but instead of
    relying on the page that links to /archived to call this function, it
    should happen on mount, to make sure it always happens.
    
    Otherwise, the list in memory is considered "valid" even if it was valid
    for active clusters and not archived ones, so the archived list won't be
    fetched again.
    
    This should fix https://jira.coreos.com/browse/SDA-1557

--- src/components/clusters/ClusterList/components/ClusterListExtraActions/index.js
+++ src/components/clusters/ClusterList/components/ClusterListExtraActions/index.js
@@ -2,14 +2,9 @@
 import { connect } from 'react-redux';
 import { viewConstants } from '../../../../../redux/constants';
 import ClusterListExtraActions from './ClusterListExtraActions';
-import { clustersActions } from '../../../../../redux/actions';
 
 const mapStateToProps = state => ({
   currentFlags: state.viewOptions[viewConstants.CLUSTERS_VIEW].flags,
 });
 
-const mapDispatchToProps = {
-  invalidateClusters: () => clustersActions.invalidateClusters(),
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ClusterListExtraActions);
+export default connect(mapStateToProps)(ClusterListExtraActions);
