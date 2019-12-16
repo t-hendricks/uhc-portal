6e84a2034bae834227447a7268a3e1e074ece3fd
Noa Leshem <user@example.com>
2019-12-16 16:45:33 +0200

    Retouches in Monitoring tab UI
    Removing table borders, adding alert severity texts, etc
    https://issues.redhat.com/browse/SDA-1631

--- src/components/clusters/ClusterDetails/components/Monitoring/components/AlertsTable.jsx
+++ src/components/clusters/ClusterDetails/components/Monitoring/components/AlertsTable.jsx
@@ -6,8 +6,16 @@ import {
   TableBody,
   TableVariant,
 } from '@patternfly/react-table';
-
-import { ExclamationCircleIcon, ExclamationTriangleIcon } from '@patternfly/react-icons';
+import {
+  Title,
+  EmptyState,
+  EmptyStateIcon,
+} from '@patternfly/react-core';
+import {
+  ExclamationCircleIcon,
+  ExclamationTriangleIcon,
+  CheckCircleIcon,
+} from '@patternfly/react-icons';
 // eslint-disable-next-line camelcase
 import { global_danger_color_100, global_warning_color_100 } from '@patternfly/react-tokens';
 
@@ -19,15 +27,37 @@ import {
 
 
 function AlertsTable({ alerts = [], clusterConsole }) {
-  const errorIcon = <ExclamationCircleIcon color={global_danger_color_100.value} size="md" />;
-  const warningIcon = <ExclamationTriangleIcon color={global_warning_color_100.value} size="md" />;
+  const errorIcon = (
+    <>
+      <ExclamationCircleIcon className="status-icon" color={global_danger_color_100.value} size="md" />
+      <span>Warning</span>
+    </>
+  );
+  const warningIcon = (
+    <>
+      <ExclamationTriangleIcon className="status-icon" color={global_warning_color_100.value} size="md" />
+      <span>Critical</span>
+    </>
+  );
 
   const columns = [
     { title: 'Name' },
     { title: 'Severity' },
   ];
 
+  if (alerts.length === 1 && alerts[0].name === 'Watchdog') {
+    return (
+      <EmptyState>
+        <EmptyStateIcon icon={CheckCircleIcon} />
+        <Title headingLevel="h5" size="lg">No alerts firing</Title>
+      </EmptyState>
+    );
+  }
+
   const rows = alerts.map((alert) => {
+    if (alert.name === 'Watchdog') {
+      return null;
+    }
     let severityIcon = null;
     if (alert.severity === alertsSeverity.WARNING) {
       severityIcon = warningIcon;
@@ -48,7 +78,7 @@ function AlertsTable({ alerts = [], clusterConsole }) {
   });
 
   return (
-    <Table variant={TableVariant.compact} cells={columns} rows={rows} aria-label="alerts">
+    <Table variant={TableVariant.compact} borders={false} cells={columns} rows={rows} aria-label="alerts">
       <TableHeader />
       <TableBody />
     </Table>
