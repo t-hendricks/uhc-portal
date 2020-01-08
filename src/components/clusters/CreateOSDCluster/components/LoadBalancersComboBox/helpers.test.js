6abc63b7ad959bc1f4291772cd43582bc56a318d
Nimrod Shneor <user@example.com>
2020-01-08 18:00:23 +0200

    Dont filter out modal data for LB and Storage in EditCluster screen.

---
+++ src/components/clusters/CreateOSDCluster/components/LoadBalancersComboBox/helpers.test.js
@@ -0,0 +1,35 @@
+import filterLoadBalancerValuesByQuota from './helpers';
+
+test('filterLoadBalancerValuesByQuota works without currentValue and with positive quota', () => {
+  const remainingQuota = 4;
+  const loadBalancerValues = { values: [0, 4, 8] };
+  expect(filterLoadBalancerValuesByQuota(null, loadBalancerValues, remainingQuota)).toEqual(
+    { values: [0, 4] },
+  );
+});
+
+test('filterLoadBalancerValuesByQuota works without currentValue and with zero remaining quota', () => {
+  const remainingQuota = 0;
+  const loadBalancerValues = { values: [0, 4, 8] };
+  expect(filterLoadBalancerValuesByQuota(null, loadBalancerValues, remainingQuota)).toEqual(
+    { values: [0] },
+  );
+});
+
+test('filterLoadBalancerValuesByQuota works with currentValue and with zero remaining quota', () => {
+  const remainingQuota = 0;
+  const loadBalancerValues = { values: [0, 4, 8, 12] };
+  const currentValue = 8;
+  expect(filterLoadBalancerValuesByQuota(currentValue, loadBalancerValues, remainingQuota)).toEqual(
+    { values: [0, 4, 8] },
+  );
+});
+
+test('filterLoadBalancerValuesByQuota works with currentValue and with positive remaining quota', () => {
+  const remainingQuota = 4;
+  const loadBalancerValues = { values: [0, 4, 8, 12] };
+  const currentValue = 8;
+  expect(filterLoadBalancerValuesByQuota(currentValue, loadBalancerValues, remainingQuota)).toEqual(
+    { values: [0, 4, 8, 12] },
+  );
+});
