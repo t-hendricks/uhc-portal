33f1e0744d0c23257dbbab32924e3ce1356b4d29
Noa Leshem <user@example.com>
2020-02-24 12:54:36 +0200

    Use Immer.js in persistentStorage reudcer
    https://issues.redhat.com/browse/SDA-1933

--- src/redux/reducers/persistentStorageReducer.js
+++ src/redux/reducers/persistentStorageReducer.js
@@ -1,59 +1,37 @@
+import produce from 'immer';
 import {
-  setStateProp, REJECTED_ACTION, PENDING_ACTION, FULFILLED_ACTION, baseRequestState,
+  REJECTED_ACTION, PENDING_ACTION, FULFILLED_ACTION, baseRequestState,
 } from '../reduxHelpers';
 import { getErrorState } from '../../common/errors';
 
 import { persistentStorageConstants } from '../constants';
 
 const initialState = {
-  persistentStorageValues: {
-    ...baseRequestState,
-    values: [],
-  },
+  ...baseRequestState,
+  values: [],
 };
 
 function persistentStorageReducer(state = initialState, action) {
-  switch (action.type) {
-    case REJECTED_ACTION(persistentStorageConstants.GET_PERSISTENT_STORAGE_VALUES):
-      return setStateProp(
-        'persistentStorageValues',
-        getErrorState(action),
-        {
-          state,
-          initialState,
-        },
-      );
-
-    case PENDING_ACTION(persistentStorageConstants.GET_PERSISTENT_STORAGE_VALUES):
-      return setStateProp(
-        'persistentStorageValues',
-        {
-          pending: true,
-        },
-        {
-          state,
-          initialState,
-        },
-      );
-
-    case FULFILLED_ACTION(persistentStorageConstants.GET_PERSISTENT_STORAGE_VALUES):
-      return setStateProp(
-        'persistentStorageValues',
-        {
-          values: action.payload,
-          pending: false,
+  // eslint-disable-next-line consistent-return
+  return produce(state, (draft) => {
+    // eslint-disable-next-line default-case
+    switch (action.type) {
+      case REJECTED_ACTION(persistentStorageConstants.GET_PERSISTENT_STORAGE_VALUES):
+        return {
+          ...initialState,
+          ...getErrorState(action),
+        };
+      case PENDING_ACTION(persistentStorageConstants.GET_PERSISTENT_STORAGE_VALUES):
+        draft.pending = true;
+        break;
+      case FULFILLED_ACTION(persistentStorageConstants.GET_PERSISTENT_STORAGE_VALUES):
+        return {
+          ...initialState,
           fulfilled: true,
-          error: false, // Unset error on successful request
-          errorMessage: '',
-        },
-        {
-          state,
-          initialState,
-        },
-      );
-    default:
-      return state;
-  }
+          values: action.payload,
+        };
+    }
+  });
 }
 persistentStorageReducer.initialState = initialState;
 
