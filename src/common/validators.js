c724dc27d412cd4f4a301b5be5ab5a78c47da793
Victor Kareh <user@example.com>
2020-03-09 08:04:27 -0400

    Networking: Change Machine CIDR max subnet to /23
    There was a previously missed ELB IP address that also gets provisioned
    in the private subnet. For this specific reason, we should consider
    restricting the machineCIDR to a maximum /23. New math being `usable IPs
    - 9` (5 originally reserved IPs, plus 4 more IPs to account for
    2 IPs for each ELB, private Master/private Router)

--- src/common/validators.js
+++ src/common/validators.js
@@ -10,7 +10,7 @@ const UUID_REGEXP = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}
 
 // Regular expression used to check whether input is a valid IPv4 CIDR range
 const CIDR_REGEXP = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$/;
-const MACHINE_CIDR_MAX = 24;
+const MACHINE_CIDR_MAX = 23;
 const SERVICE_CIDR_MAX = 24;
 const POD_CIDR_MAX = 18;
 
@@ -225,7 +225,7 @@ const machineCidr = (value) => {
 
   if (prefixLength > MACHINE_CIDR_MAX) {
     const maxComputeNodes = 2 ** (28 - MACHINE_CIDR_MAX);
-    const singleAZ = maxComputeNodes - 5;
+    const singleAZ = maxComputeNodes - 9;
     const multiAZ = Math.floor(maxComputeNodes / 3);
     return `The subnet length can't be higher than '/${MACHINE_CIDR_MAX}', which provides up to ${singleAZ} nodes for single-zone clusters or ${multiAZ} nodes for each zone in a multi-zone clusters.`;
   }
