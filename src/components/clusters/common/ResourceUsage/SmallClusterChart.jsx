8332a9331d45d88e15f29a2ef2af38b0437a8b19
nshneor <user@example.com>
2020-04-04 14:07:12 +0300

    Add SmallClusterChart component to support the new 'Overview' screen

---
+++ src/components/clusters/common/ResourceUsage/SmallClusterChart.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { ChartDonut } from '@patternfly/react-charts';
+
+import { humanizeValueWithUnit, roundValueWithUnit } from '../../../../common/units';
+
+function SmallClusterChart(props) {
+  const {
+    used, total, unit, humanize, donutId,
+  } = props;
+
+  const format = humanize ? humanizeValueWithUnit : roundValueWithUnit;
+  const formattedUsed = format(used, unit);
+  const formattedTotal = format(total, unit);
+  const formattedUnused = format(total - used, unit);
+
+  // Step 1: used / total * 100 - calculate "used" in percentage out of the total
+  // Step 2: Math.round(Step1 * 100) / 100 - round to 2 decimal places
+  const usedPercentage = Math.round((used / total) * 100 * 100) / 100;
+  const unusedPrecentage = Math.round(((total - used) / total) * 100 * 100) / 100;
+  return (
+    <div>
+      <div style={{ height: '205px', width: '410px' }}>
+        <ChartDonut
+          id={donutId}
+          labels={({ datum }) => (datum.x ? `${datum.x}` : null)}
+          data={[{ x: `${formattedUsed.value} ${formattedUsed.unit}`, y: usedPercentage },
+            { x: `${formattedUnused.value} ${formattedUnused.unit}`, y: unusedPrecentage }]}
+          constrainToVisibleArea
+          legendData={[{ name: `Used: ${formattedUsed.value} ${formattedUsed.unit}` }, { name: `Available: ${formattedUnused.value} ${formattedUnused.unit}` }]}
+          legendOrientation="vertical"
+          padding={{
+            bottom: 20,
+            left: 20,
+            right: 195, // Adjusted to accommodate legend
+            top: 20,
+          }}
+          title={formattedTotal.value}
+          height={205}
+          width={410}
+        />
+      </div>
+    </div>
+  );
+}
+
+SmallClusterChart.propTypes = {
+  used: PropTypes.number,
+  total: PropTypes.number,
+  unit: PropTypes.string,
+  humanize: PropTypes.bool,
+  donutId: PropTypes.string.isRequired,
+};
+
+export default SmallClusterChart;
---
+++ src/components/clusters/common/ResourceUsage/SmallClusterChart.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { parseValueWithUnit } from '../../../../common/units';
+import SmallClusterChart from './SmallClusterChart';
+
+const getValue = ({ value, unit }) => parseValueWithUnit(value, unit);
+const memory = {
+  used: {
+    value: 16546058240,
+    unit: 'B',
+  },
+  total: {
+    value: 82293346304,
+    unit: 'B',
+  },
+};
+
+describe('<ResourceUsage />', () => {
+  it('should render', () => {
+    const wrapper = shallow(
+      <SmallClusterChart
+        title="Memory"
+        total={getValue(memory.total)}
+        used={getValue(memory.used)}
+        unit="B"
+        humanize
+        donutId="memory_donut"
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
