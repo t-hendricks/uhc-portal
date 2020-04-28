59f843b9899d037658ff5ea81384970fe8d1cbc4
Elad Alfassa <user@example.com>
2020-04-28 13:28:28 +0300

    Add OCP on Power page
    https://issues.redhat.com/browse/SDA-2252

---
+++ src/components/clusters/install/InstallPower.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { PageSection } from '@patternfly/react-core';
+import Breadcrumbs from '../common/Breadcrumbs';
+import PageTitle from '../../common/PageTitle';
+
+import { tollboothActions } from '../../../redux/actions';
+import InstructionsPower from './instructions/InstructionsPower';
+import { scrollToTop } from '../../../common/helpers';
+
+class InstallIBM extends Component {
+  componentDidMount() {
+    scrollToTop();
+    document.title = 'Install OpenShift 4 | Red Hat OpenShift Cluster Manager | Power';
+
+    const { dispatch } = this.props;
+    dispatch(tollboothActions.createAuthToken());
+  }
+
+  render() {
+    const { token } = this.props;
+    const breadcrumbs = (
+      <Breadcrumbs path={[
+        { label: 'Clusters' },
+        { label: 'Create', path: '/create' },
+        { label: 'OpenShift Container Platform', path: '/install' },
+        { label: 'Power' },
+      ]}
+      />
+    );
+
+    return (
+      <>
+        <PageTitle title="Install OpenShift Container Platform 4" breadcrumbs={breadcrumbs} />
+        <PageSection className="ocp-instructions">
+          <InstructionsPower token={token} />
+        </PageSection>
+      </>
+    );
+  }
+}
+
+InstallIBM.propTypes = {
+  token: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({ token: state.tollbooth.token });
+
+export default connect(mapStateToProps)(InstallIBM);
