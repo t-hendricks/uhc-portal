d686c1fcb5f9ed7bb3d953302a6f1dc114e3f1d2
Beni Cherniavsky-Paskin <user@example.com>
2020-05-14 23:52:51 +0300

    More correct execution of insights-proxy on Mac OS X
    Match insights-proxy/scripts/run.sh logic only adding --net=host on Linux.
    
    UNTESTED! I don't have a Mac. It's likely that more stuff is broken.

--- run/insights-proxy.sh
+++ run/insights-proxy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Start insights-proxy with our config.
+# This script replaces the builtin env.sh & run.sh scripts from insights-proxy.
 
 set -e -u -o pipefail
 
@@ -10,15 +11,20 @@ yarn wait-on http://localhost:8001/ http://localhost:8010/api
 
 yarn stop-insights-proxy
 
-# Need env.sh to set PLATFORM var for passing into container.
-# Apply our RUNNER default before env.sh makes it default to docker.
-export RUNNER=${RUNNER:-podman}
-source ./insights-proxy/scripts/env.sh
+case "$(uname -s)" in
+    Linux*)
+        OPTS="--net=host --env PLATFORM=linux";;
+    Darwin*)
+        OPTS="--env PLATFORM=darwin";;
+    *)
+        echo 'This only works on Linux or Darwin!'
+        exit 1;;
+esac
 
 ./podman-or-docker.sh run \
                       --rm --name insightsproxy \
                       --env CUSTOM_CONF=true --volume "$PWD"/../profiles/local-frontend.js:/config/spandx.config.js \
                       --security-opt label=disable \
-                      --env PLATFORM --net=host \
+                      $OPTS \
                       -p 1337:1337 \
                       docker.io/redhatinsights/insights-proxy
