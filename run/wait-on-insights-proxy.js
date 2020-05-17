8eba8fdf734e555243e9f7240689a6fcb859c87a
Beni Cherniavsky-Paskin <user@example.com>
2020-05-17 14:53:52 +0300

    Allow e2e-test to work without having qa.foo.redhat.com in /etc/hosts

---
+++ run/wait-on-insights-proxy.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+// Config for https://github.com/jeffbski/wait-on for waiting for insights-proxy
+
+var waitOn = require('wait-on');
+
+const config = {
+  interval: 1000,
+  strictSSL: false,
+
+  // Insights-proxy cares about the requested domain name.
+  // When devoloping locally, we define  qa.foo.redhat.com and friends in /etc/hosts.
+  // But it's possible — and necessary in CI — to run selenium-test with that defined
+  // only in the browser container.
+  //
+  // So, need to request https://qa.foo.redhat.com:1337/ without being able to resolve it!
+  // This is hack, possibly incorrect SNI (would need to specify `servername` somewhere?),
+  // but works.
+  resources: [
+    "https-get://localhost:1337/"
+  ],
+  headers: {
+    Host: "qa.foo.redhat.com"
+  },
+}
+
+waitOn(config)
+  .then(() => {
+    process.exit(0);
+  })
+  .catch(err => {
+    if (err) {
+      console.error(err)
+    }
+    process.exit(1);
+  })
