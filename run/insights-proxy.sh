64f5cefb388e4aa3a4872340b4e3b8ee6de7dbaf
Beni Cherniavsky-Paskin <user@example.com>
2020-05-18 19:44:52 +0300

    Kill insights-proxy.sh on exit rather than next start
    insights-proxy container currently tends to not die when killed with
    SIGTERM, which is what `concurrently` sends.
    
    1. `podman run` usually "detaches" but container stays running.
       (Details on https://github.com/RedHatInsights/insights-proxy/pull/32,
       hope to fix by https://github.com/RedHatInsights/insights-proxy/pull/33)
    
    2. Seen once on Jenkins that `podman run` didn't react to SIGTERM at all,
       so `yarn e2e-test` never completed.  Don't know why.
    
    Previous workaround for (1) was to accept that and kill on next start.
    This should fix both better by ensure we killing container when
    insights-proxy.sh gets signal, and always on exit.

--- run/insights-proxy.sh
+++ run/insights-proxy.sh
@@ -21,10 +21,22 @@ case "$(uname -s)" in
         exit 1;;
 esac
 
+# insights-proxy container currently tends to not die when killed with SIGTERM.
+# `podman run` may "detach" & exit, but container stays running,
+# or may not exit at all!  So abort it ourselves.
+trap 'echo Trapped SIGINT; yarn stop-insights-proxy' INT
+trap 'echo Trapped SIGTERM; yarn stop-insights-proxy' TERM
+trap 'echo Trapped SIGHUP; yarn stop-insights-proxy' HUP
+trap 'yarn stop-insights-proxy' EXIT
+
+# bash doesn't trap signals while a foreground command is running.
+# Running in background + wait allows the trap to work.
 ./podman-or-docker.sh run \
                       --rm --name insightsproxy \
                       --env CUSTOM_CONF=true --volume "$PWD"/../profiles/local-frontend.js:/config/spandx.config.js \
                       --security-opt label=disable \
                       $OPTS \
                       -p 1337:1337 \
-                      docker.io/redhatinsights/insights-proxy
+                      docker.io/redhatinsights/insights-proxy &
+child_pid=$!
+wait $child_pid
