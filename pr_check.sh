a66b189739a56f980916dff45b63b068c5cb85a7
Beni Cherniavsky-Paskin <user@example.com>
2020-06-08 11:40:23 +0300

    Delete ~/.yarnrc leftover

--- pr_check.sh
+++ pr_check.sh
@@ -18,6 +18,10 @@
 # This script is executed by a Jenkins job for each change request. If it
 # doesn't succeed the change won't be merged.
 
+# Were previously created this by `yarn config set cafile ~/RH-IT-Root-CA.crt`
+# but it's not the right config and anyway don't want leftovers between jobs.
+rm --verbose ~/.yarnrc || true
+
 # Configure the cert required to connect to Nexus (yarn respects npm configs too).
 # The file is placed there by
 # https://gitlab.cee.redhat.com/app-sre/infra/blob/master/ansible/playbooks/roles/baseline/tasks/main.yml
