0702171f47270ac15b096eb4c4ac75480f2c15dc
Beni Cherniavsky-Paskin <user@example.com>
2020-06-10 15:58:03 +0300

    Drop `rm ~/.yarnrc` command
    This has completed on both our nodes (uhc-02, uhc-04-provisioning).

--- pr_check.sh
+++ pr_check.sh
@@ -18,10 +18,6 @@
 # This script is executed by a Jenkins job for each change request. If it
 # doesn't succeed the change won't be merged.
 
-# Were previously created this by `yarn config set cafile ~/RH-IT-Root-CA.crt`
-# but it's not the right config and anyway don't want leftovers between jobs.
-rm --verbose ~/.yarnrc || true
-
 # The cert required to connect to Nexus is already in system CA store per:
 # https://gitlab.cee.redhat.com/app-sre/infra/blob/master/ansible/playbooks/roles/baseline/tasks/main.yml
 
