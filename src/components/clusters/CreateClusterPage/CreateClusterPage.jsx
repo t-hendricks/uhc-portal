468e590cc296782e8d5e45841152a2c20ff8ef63
Noa Leshem <user@example.com>
2020-06-11 17:43:55 +0300

    Add page title to osd/create
    So that it doesn't take the page title of the cluster list page.

--- src/components/clusters/CreateClusterPage/CreateClusterPage.jsx
+++ src/components/clusters/CreateClusterPage/CreateClusterPage.jsx
@@ -21,6 +21,7 @@ class CreateCluster extends React.Component {
     // Try to get quota or organization when the component is first mounted.
     const { getOrganizationAndQuota, organization } = this.props;
 
+    document.title = 'Create an OpenShift cluster | Red Hat OpenShift Cluster Manager';
     if (shouldRefetchQuota(organization)) {
       getOrganizationAndQuota();
     }
