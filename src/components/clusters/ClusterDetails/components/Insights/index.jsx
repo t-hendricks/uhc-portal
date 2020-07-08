ae10f9d64eaf3b6432e9964be2533647adb6562c
bond95 <user@example.com>
2020-07-08 13:25:50 +0200

    Added few initial states to insight tab

--- src/components/clusters/ClusterDetails/components/Insights/index.jsx
+++ src/components/clusters/ClusterDetails/components/Insights/index.jsx
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import get from 'lodash/get';
 import { Bullseye } from '@patternfly/react-core';
 import { Spinner } from '@redhat-cloud-services/frontend-components';
-import { NoIssuesMessage, NoRulesMessage } from './EmptyTableMessage';
+import {
+  NoIssuesMessage,
+  NoRulesMessage,
+  AnalysisInProgressMessage,
+  ErrorMessage,
+} from './EmptyTableMessage';
 import InsightsTable from './InsightsTable';
 import './index.css';
 
@@ -18,9 +23,19 @@ const Insights = ({
     );
   }
 
+  // No enabled rules for cluster
   if (insightsData.status === 404) {
     return <NoRulesMessage />;
   }
+  // insights-operator was just installed on cluster, no information yet
+  if (insightsData.status === 204) {
+    return <AnalysisInProgressMessage />;
+  }
+  // Error on aggregator side
+  if (insightsData.status === 500) {
+    return <ErrorMessage />;
+  }
+  // No issues was found
   if (!insightsData || get(insightsData, 'meta.count', 0) === 0) {
     return <NoIssuesMessage lastChecked={get(insightsData, 'meta.last_checked_at')} />;
   }
