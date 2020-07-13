912c9b7151ef5cbd51437761818c0cd3a3f747c5
Elad Alfassa <user@example.com>
2020-07-13 12:18:32 +0300

    [SDA-2616] overview: Adjust page sizes in the "expired trails" card
    Both to make the default value "5" appear in the list of available page
    sizes there and to adjust the other available items (no need for
    hundreds of clusters per page there, that's an highly unlikely use)

--- src/components/overview/ExpiredTrialsCard/ExpiredTrialsCard.jsx
+++ src/components/overview/ExpiredTrialsCard/ExpiredTrialsCard.jsx
@@ -136,6 +136,12 @@ class ExpiredTrialsCard extends React.Component {
             totalPages={viewOptions.totalPages}
             variant="bottom"
             isDisabled={subscriptions.pending}
+            perPageOptions={[
+              { title: '5', value: 5 },
+              { title: '10', value: 10 },
+              { title: '25', value: 25 },
+              { title: '50', value: 50 },
+            ]}
           />
         </CardBody>
       </Card>
