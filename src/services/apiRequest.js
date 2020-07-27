fb95782ff7aefdda0e08fc42db9cbc188551e86d
Elad Alfassa <user@example.com>
2020-07-27 16:29:07 +0300

    Wait 15s before reporting bad token
    Waiting 15s gives Insights a chance to redirect the user to log in again
    when a token expires, before we report the error to Sentry. This is
    needed to reduce the chance of false-positive reporting to Sentry so we
    can determine if the token bug is still there.

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -15,10 +15,17 @@ const authInterceptor = (client) => {
         Authorization: `Bearer ${token}`,
       };
     } else {
-      Sentry.withScope((scope) => {
-        scope.setFingerprint(['empty token']); // group all "empty token" errors together
-        Sentry.captureException(new Error('Got empty token from Insights'));
-      });
+      setTimeout(() => {
+        /* this is in a timeout to give Insights a chance to redirect
+         if the token is expired, before we report it as an error.
+         The assumption is that if Insights is trying to redirect, it might happen asynchronously,
+         so we should wait a bit before reporting this error, assuming once a redirect happens
+         our code will stop running and this timer won't be reached */
+        Sentry.withScope((scope) => {
+          scope.setFingerprint(['empty token']); // group all "empty token" errors together
+          Sentry.captureException(new Error('Got empty token from Insights'));
+        });
+      }, 15000);
     }
     delete updatedCfg.customHost;
     return updatedCfg;
