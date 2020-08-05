b5bc4fe25389ffebd468dda56f6cd5899b204132
bond95 <user@example.com>
2020-08-05 10:59:36 +0200

    Added empty remediation state to Insights page

---
+++ src/components/clusters/InsightsRuleDetails/components/EmptyRemediationInfo.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateIcon,
+  EmptyStateVariant,
+  Title,
+} from '@patternfly/react-core';
+import { WrenchIcon } from '@patternfly/react-icons';
+import PropTypes from 'prop-types';
+
+const EmptyRemediationInfo = ({ title }) => (
+  <EmptyState className="empty-table-message" variant={EmptyStateVariant.large}>
+    <EmptyStateIcon className="disabled-color" icon={WrenchIcon} />
+
+    <Title headingLevel="h5" size="lg">
+      {`No ${title} to display`}
+    </Title>
+
+    <EmptyStateBody>
+      {`This health check has no ${title} description to display. For details how to solve this problem, see the corresponding knowledgebase article.`}
+    </EmptyStateBody>
+  </EmptyState>
+);
+
+EmptyRemediationInfo.propTypes = {
+  title: PropTypes.string,
+};
+
+export default EmptyRemediationInfo;
