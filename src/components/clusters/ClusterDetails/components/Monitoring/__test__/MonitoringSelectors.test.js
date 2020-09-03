45f16ff45ac843cc53e299507edd6eaa55094720
Beni Cherniavsky-Paskin <user@example.com>
2020-09-03 15:13:02 +0300

    [SDA-2763] tests: Separate disconnected X checkin scenarios
    Followup to !1619, 8045b76162b4c282121563b5a9792b38e1da6d13.
    Test for {active, disconnected} X {never, stale, fresh}.

--- src/components/clusters/ClusterDetails/components/Monitoring/__test__/MonitoringSelectors.test.js
+++ src/components/clusters/ClusterDetails/components/Monitoring/__test__/MonitoringSelectors.test.js
@@ -9,138 +9,176 @@ import {
   mockOperators,
   resourceUsageWithIssues,
   mockOSDCluserDetails,
-  mockOCPClusterDetails,
+  mockOCPActiveClusterDetails,
+  mockOCPDisconnectedClusterDetails,
   mockLastCheckIn,
+  oldLastCheckIn,
 } from './Monitoring.fixtures';
 import { monitoringStatuses } from '../monitoringHelper';
 import clusterStates from '../../../../common/clusterStates';
 
 describe('clusterHealthSelector', () => {
-  const stateWithOsdCluster = {
-    clusters: {
-      details: {
-        cluster: {
-          ...mockOSDCluserDetails,
-        },
-      },
-    },
-  };
-
-  it('should return status DISCONNECTED', () => {
-    const state = {
+  describe('OCP', () => {
+    const stateWithOCPActive = {
       clusters: {
         details: {
           cluster: {
-            ...mockOCPClusterDetails,
+            ...mockOCPActiveClusterDetails,
           },
         },
       },
     };
-    expect(clusterHealthSelector(state, lastCheckInSelector(null), null))
-      .toBe(monitoringStatuses.DISCONNECTED);
+
+    const stateWithOCPDisconnected = {
+      clusters: {
+        details: {
+          cluster: {
+            ...mockOCPDisconnectedClusterDetails,
+          },
+        },
+      },
+    };
+
+    it('should return DISCONNECTED when subscription Disconnected and checkin unknown', () => {
+      expect(clusterHealthSelector(stateWithOCPDisconnected, lastCheckInSelector(null), null))
+        .toBe(monitoringStatuses.DISCONNECTED);
+    });
+
+    it('should return DISCONNECTED when subscription Disconnected and metrics are stale', () => {
+      expect(clusterHealthSelector(stateWithOCPDisconnected, oldLastCheckIn, null))
+        .toBe(monitoringStatuses.DISCONNECTED);
+    });
+
+    it.todo("if fresh metrics arrived for a Disconnected cluster, but subscription didn't yet switch to Active, what should we return?");
+
+    it('should return NO_METRICS when checkin unknown', () => {
+      expect(clusterHealthSelector(stateWithOCPActive, lastCheckInSelector(null), null))
+        .toBe(monitoringStatuses.NO_METRICS);
+    });
+
+    it('should return NO_METRICS when metrics are stale', () => {
+      expect(clusterHealthSelector(stateWithOCPActive, oldLastCheckIn, null))
+        .toBe(monitoringStatuses.NO_METRICS);
+    });
+
+    it('should return HEALTHY when metrics are fresh & good', () => {
+      expect(clusterHealthSelector(stateWithOCPActive, mockLastCheckIn, 0))
+        .toBe(monitoringStatuses.HEALTHY);
+    });
   });
 
-  it('should not return status DISCONNECTED for OSD cluster', () => {
-    expect(clusterHealthSelector(stateWithOsdCluster, lastCheckInSelector(null), null))
-      .not.toBe(monitoringStatuses.DISCONNECTED);
-  });
-
-  it('should return status UPGRADING', () => {
-    const { metrics } = mockOSDCluserDetails;
-    // upgrading cluster state
-    const state = {
+  describe('OSD', () => {
+    const stateWithOsdCluster = {
       clusters: {
         details: {
           cluster: {
             ...mockOSDCluserDetails,
-            metrics: { ...metrics, upgrade: { ...metrics.upgrade, state: 'running' } },
           },
         },
       },
     };
-    expect(clusterHealthSelector(state, mockLastCheckIn, 1))
-      .toBe(monitoringStatuses.UPGRADING);
+
+    it('should not return status DISCONNECTED for OSD cluster', () => {
+      expect(clusterHealthSelector(stateWithOsdCluster, lastCheckInSelector(null), null))
+        .not.toBe(monitoringStatuses.DISCONNECTED);
+    });
+
+    it('should return status UPGRADING', () => {
+      const { metrics } = mockOSDCluserDetails;
+      // upgrading cluster state
+      const state = {
+        clusters: {
+          details: {
+            cluster: {
+              ...mockOSDCluserDetails,
+              metrics: { ...metrics, upgrade: { ...metrics.upgrade, state: 'running' } },
+            },
+          },
+        },
+      };
+      expect(clusterHealthSelector(state, mockLastCheckIn, 1))
+        .toBe(monitoringStatuses.UPGRADING);
+    });
+
+    it('should return status INSTALLING', () => {
+      // installing cluster state
+      const state = {
+        clusters: {
+          details: {
+            cluster: {
+              ...mockOSDCluserDetails,
+              state: clusterStates.INSTALLING,
+            },
+          },
+        },
+      };
+      expect(clusterHealthSelector(state, mockLastCheckIn, null))
+        .toBe(monitoringStatuses.INSTALLING);
+    });
+
+    it('should return status HAS_ISSUES', () => {
+      expect(clusterHealthSelector(stateWithOsdCluster, mockLastCheckIn, 2))
+        .toBe(monitoringStatuses.HAS_ISSUES);
+    });
+
+    it('return status NO_METRICS', () => {
+      expect(clusterHealthSelector(stateWithOsdCluster, oldLastCheckIn, 3))
+        .toBe(monitoringStatuses.NO_METRICS);
+    });
+
+    it('return status HEALTHY', () => {
+      expect(clusterHealthSelector(stateWithOsdCluster, mockLastCheckIn, 0))
+        .toBe(monitoringStatuses.HEALTHY);
+    });
   });
 
-  it('should return status INSTALLING', () => {
-    // installing cluster state
-    const state = {
+
+  describe('issuesAndWarningsSelector', () => {
+    const cluster = {
+      ...mockOSDCluserDetails,
+      metrics: {
+        ...resourceUsageWithIssues,
+        memory: {
+          ...resourceUsageWithIssues.memory,
+          updated_timestamp: new Date().toISOString(),
+        },
+        cpu: {
+          ...resourceUsageWithIssues.cpu,
+          updated_timestamp: new Date().toISOString(),
+        },
+      },
+    };
+
+    const stateWithIssues = {
       clusters: {
         details: {
-          cluster: {
-            ...mockOSDCluserDetails,
-            state: clusterStates.INSTALLING,
-          },
+          cluster,
         },
       },
-    };
-    expect(clusterHealthSelector(state, mockLastCheckIn, null))
-      .toBe(monitoringStatuses.INSTALLING);
-  });
-
-  it('should return status HAS_ISSUES', () => {
-    expect(clusterHealthSelector(stateWithOsdCluster, mockLastCheckIn, 2))
-      .toBe(monitoringStatuses.HAS_ISSUES);
-  });
-
-  it('return status NO_METRICS', () => {
-    const oldLastCheckIn = { ...mockLastCheckIn, hours: 3 };
-    expect(clusterHealthSelector(stateWithOsdCluster, oldLastCheckIn, 3))
-      .toBe(monitoringStatuses.NO_METRICS);
-  });
-
-  it('return status HEALTHY', () => {
-    expect(clusterHealthSelector(stateWithOsdCluster, mockLastCheckIn, 0))
-      .toBe(monitoringStatuses.HEALTHY);
-  });
-});
-
-
-describe('issuesAndWarningsSelector', () => {
-  const cluster = {
-    ...mockOSDCluserDetails,
-    metrics: {
-      ...resourceUsageWithIssues,
-      memory: {
-        ...resourceUsageWithIssues.memory,
-        updated_timestamp: new Date().toISOString(),
-      },
-      cpu: {
-        ...resourceUsageWithIssues.cpu,
-        updated_timestamp: new Date().toISOString(),
-      },
-    },
-  };
-
-  const stateWithIssues = {
-    clusters: {
-      details: {
-        cluster,
-      },
-    },
-    monitoring: {
-      alerts: mockAlerts,
-      nodes: { data: [...mockNodes.data.slice(0, 2)] },
-      operators: mockOperators,
-    },
-  };
-
-  it('should count issues, warnings and total issues', () => {
-    const expected = {
-      issues: {
-        alerts: 2,
-        nodes: 0,
-        operators: 1,
-        resourceUsage: 2,
-        totalCount: 5,
-      },
-      warnings: {
-        alerts: 3,
-        operators: 0,
-        resourceUsage: 2,
+      monitoring: {
+        alerts: mockAlerts,
+        nodes: { data: [...mockNodes.data.slice(0, 2)] },
+        operators: mockOperators,
       },
     };
 
-    expect(issuesAndWarningsSelector(stateWithIssues)).toEqual(expected);
+    it('should count issues, warnings and total issues', () => {
+      const expected = {
+        issues: {
+          alerts: 2,
+          nodes: 0,
+          operators: 1,
+          resourceUsage: 2,
+          totalCount: 5,
+        },
+        warnings: {
+          alerts: 3,
+          operators: 0,
+          resourceUsage: 2,
+        },
+      };
+
+      expect(issuesAndWarningsSelector(stateWithIssues)).toEqual(expected);
+    });
   });
 });
