772a3997b4daf426ffb93f8c455096e646bcde6d
Beni Cherniavsky-Paskin <user@example.com>
2020-12-01 14:41:50 +0200

    Drop old normalization
    https://gitlab.cee.redhat.com/service/uhc-clusters-service/merge_requests/800
    landed a year ago, clusters-service always returns a .metrics sub-object.

--- src/common/normalize.js
+++ src/common/normalize.js
@@ -1,19 +1,5 @@
 const normalizeCluster = (cluster) => {
   const result = { ...cluster };
-  // Convert data from older backend, to new shape with .metrics sub-object.  See
-  // https://gitlab.cee.redhat.com/service/uhc-clusters-service/merge_requests/800
-  if (!result.metrics) {
-    result.metrics = {
-      cpu: cluster.cpu,
-      memory: cluster.memory,
-      storage: cluster.storage,
-      nodes: {
-        // total: undefined,
-        // master: undefined,
-        // compute: undefined,
-      },
-    };
-  }
 
   // Convert data from older backend
   // See https://gitlab.cee.redhat.com/service/uhc-clusters-service/merge_requests/1175
