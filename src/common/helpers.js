9044841fc45456ddcbe73f73d2d96de030c70182
Elizabeta Gilman <user@example.com>
2020-12-09 11:10:36 +0000

    Revert "Merge branch 'labels-reduxform' into 'master'"
    This reverts merge request !1949

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -136,26 +136,6 @@ function scrollToFirstError(errors) {
   setTimeout(() => firstError.scrollIntoView({ behavior: 'smooth', block: 'center' }), 0);
 }
 
-
-/**
- * Converts redux form structure to the structure expected by ocm api
- * [{ key: "foo", value: "bar" },{ key: "hello", value: "world" }]
- *   => { foo: "bar", hello: "world" }
- * @param {Array} labelsFormData Array of key value parirs
- */
-const parseReduxFormKeyValueList = labelsFormData => Object.assign(
-  {},
-  ...(labelsFormData.map(label => label.key && label.value
-    && ({ [label.key]: label.value }))),
-);
-
-/**
- * check if there is any user input in the key-value list
- * @param {Array} labelsFormData Array of key value parirs
- */
-const hasLabelsInput = labelsFormData => !(labelsFormData.length === 1
-  && isEmpty(labelsFormData[0]));
-
 export {
   noop,
   isValid,
@@ -169,8 +149,6 @@ export {
   strToCleanObject,
   shouldRefetchQuota,
   scrollToFirstError,
-  parseReduxFormKeyValueList,
-  hasLabelsInput,
 };
 
 export default helpers;
