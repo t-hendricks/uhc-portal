50f0a2639e9ca801d23d7e5dcafe91da3e92adae
Beni Cherniavsky-Paskin <user@example.com>
2021-02-01 22:37:38 +0200

    refactor: rename mispelled method

--- src/components/clusters/ClusterDetails/components/TermsAlert/TermsAlert.jsx
+++ src/components/clusters/ClusterDetails/components/TermsAlert/TermsAlert.jsx
@@ -9,7 +9,7 @@ import getTermsAppLink from '../../../../../common/getTermsAppLink';
 class TermsAlert extends Component {
   componentDidMount() {
     const { selfTermsReview } = this.props;
-    if (this.isTermsReviewRquired()) {
+    if (this.isTermsReviewRequired()) {
       selfTermsReview();
     }
   }
@@ -17,7 +17,7 @@ class TermsAlert extends Component {
   componentDidUpdate(prevProps) {
     const { subscription: prevSubscription } = prevProps;
     const { subscription, selfTermsReview } = this.props;
-    if (get(prevSubscription, 'id') !== get(subscription, 'id') && this.isTermsReviewRquired()) {
+    if (get(prevSubscription, 'id') !== get(subscription, 'id') && this.isTermsReviewRequired()) {
       selfTermsReview();
     }
   }
@@ -29,7 +29,7 @@ class TermsAlert extends Component {
     return getTermsAppLink(baseURL, currentHref, currentHref);
   };
 
-  isTermsReviewRquired() {
+  isTermsReviewRequired() {
     const { subscription } = this.props;
     const product = get(subscription, 'plan.id');
     const status = get(subscription, 'status');
@@ -42,7 +42,7 @@ class TermsAlert extends Component {
   render() {
     const { selfTermsReviewResult } = this.props;
 
-    if (this.isTermsReviewRquired()
+    if (this.isTermsReviewRequired()
       && selfTermsReviewResult.fulfilled
       && selfTermsReviewResult.terms_required) {
       const tncAppURL = this.getTncAppURL(selfTermsReviewResult.redirect_url);
