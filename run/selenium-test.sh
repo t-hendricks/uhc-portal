120f6260509691d54c4f906f59714610b2a8286d
Elad Alfassa <user@example.com>
2021-03-21 13:08:26 +0200

    Remove xyaml/cucumber tests
    These tests are slow, difficult to debug, and prone to strange errors.
    Since it expects both an empty state and a cluster list with actual
    clusters - it doesn't actually test anything, because the selenium user
    doesn't actually have unarchived clusters. The tests we already have for
    cluster creation interact with the cluster creation button, and therefor
    should be sufficient for testing empty state functionality.
    
    Keeping these tests seems to have zero benefits, and we'll need to
    rewrite tests for the cluster list itself (with clusters!) anyway.
    
    So, here's an MR removing these obsolete tests.

--- run/selenium-test.sh
+++ run/selenium-test.sh
@@ -5,8 +5,6 @@ set -e -u -o pipefail
 
 cd "$(dirname "$0")"
 
-. ./selenium-tests.version.sh
-
 echo Waiting on insights-proxy and selenium...
 yarn wait-on http-get://localhost:4444/wd/hub
 ./wait-on-insights-proxy.js
@@ -17,54 +15,3 @@ BROWSER="${BROWSER:-firefox}"
 # JavaScript tests
 
 yarn run wdio
-
-# Ruby/cucumber/xyaml tests
-
-export BUSHSLICER_CONFIG="{
-  \"environments\": {
-    \"ocm_local_dev_prod_sso\": {
-      \"static_users_map\": {
-        \"regularUser\": \"$TEST_SELENIUM_WITHQUOTA_USER:$TEST_SELENIUM_WITHQUOTA_PASSWORD\",
-        \"noAnyQuotaUser\": \"$TEST_SELENIUM_NOANYQUOTA_USERNAME:$TEST_SELENIUM_NOANYQUOTA_PASSWORD\"
-      }
-    }
-  },
-  \"global\": {
-    \"browser\": \"$BROWSER\",
-    \"default_environment\": \"ocm_local_dev_prod_sso\"
-  }
-}"
-export BUSHSLICER_DEFAULT_ENVIRONMENT=ocm_local_dev_prod_sso
-
-if [ -n "${UNATTENDED-}" ]; then
-  FLAGS=('--profile=_debug' '--profile=junit')
-else
-  FLAGS=()
-fi
-
-mkdir -p ./output
-
-CUCUMBER_COMMAND=(
-  bundle exec cucumber "${FLAGS[@]}"
-  # See comment about --guess in our-tests/features/step-definitions/ocm.rb
-  --verbose --require=features/ --require=our-tests/features/ --guess
-  our-tests/features/ocm/login.feature "$@"
-)
-
-# See also Dockerfile.selenium-tests.
-# `--mount type=bind` works similarly on both docker and podman.
-# This syntax doesn't support :z for SELinux relabeling, so need label=disable.
-./podman-or-docker.sh run \
-                      --rm --name selenium-test \
-                      --interactive \
-                      --net=host \
-                      --security-opt label=disable \
-                      --mount type=bind,src="$PWD/verification-tests",dst=/verification-tests,ro=true \
-                      --mount type=bind,src="$PWD/private",dst=/verification-tests/private,ro=true \
-                      --mount type=bind,src="$PWD/our-tests",dst=/verification-tests/our-tests,ro=true \
-                      --mount type=bind,src="$PWD/output",dst=/output \
-                      --user=root \
-                      --env BUSHSLICER_CONFIG \
-                      --env BUSHSLICER_DEFAULT_ENVIRONMENT \
-                      $SELENIUM_TESTS_IMAGE \
-                      "${CUCUMBER_COMMAND[@]}"
