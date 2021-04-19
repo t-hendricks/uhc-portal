351f045f806d4da9e0c319b4977ad2c256c673a4
zallen <user@example.com>
2021-04-19 12:33:24 -0400

    fix jest tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { setAutoFreeze } from 'immer';
 
+setAutoFreeze(false);
 if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
   process.on('unhandledRejection', () => {});
   process.env.LISTENING_TO_UNHANDLED_REJECTION = true;
