93027ed7384d058fdb26e0846ad8dc9ba9cecff2
Eric Himmelreich <user@example.com>
2021-04-21 14:08:53 -0500

    Allow RHM OSD clusters to autoscale

--- src/components/clusters/ClusterDetails/components/MachinePools/components/AddMachinePoolModal/index.js
+++ src/components/clusters/ClusterDetails/components/MachinePools/components/AddMachinePoolModal/index.js
@@ -26,7 +26,9 @@ const mapStateToProps = (state, ownProps) => {
     autoscalingEnabled: !!valueSelector(state, 'autoscalingEnabled'),
     autoScaleMinNodesValue: valueSelector(state, 'min_replicas'),
     autoScaleMaxNodesValue: valueSelector(state, 'max_replicas'),
-    canAutoScale: canAutoScaleSelector(state, ownProps.cluster.subscription.plan.id),
+    canAutoScale: canAutoScaleSelector(state,
+      ownProps.cluster.subscription.plan.id,
+      ownProps.cluster.billing_model),
     initialValues: {
       name: '',
       nodes_compute: '0',
