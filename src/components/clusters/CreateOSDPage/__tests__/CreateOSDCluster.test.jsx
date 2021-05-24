6b961e90b80e484f91d0457773d365523a10c870
Beni Cherniavsky-Paskin <user@example.com>
2021-05-24 13:13:47 +0300

    [SDA-4103] use generic quota queries in CreateOSDForm
    Co-authored-by: Boris Od <borod108@users.noreply.github.com>

--- src/components/clusters/CreateOSDPage/__tests__/CreateOSDCluster.test.jsx
+++ src/components/clusters/CreateOSDPage/__tests__/CreateOSDCluster.test.jsx
@@ -9,7 +9,7 @@ import {
 import { normalizedProducts, billingModels } from '../../../../common/subscriptionTypes';
 
 import CreateOSDCluster from '../CreateOSDPage';
-import CreateOSDForm from '../CreateOSDForm/CreateOSDForm';
+import CreateOSDForm from '../CreateOSDForm';
 
 describe('CreateOSDCluster', () => {
   let resetResponse;
