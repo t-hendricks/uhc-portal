6b777c867222ba96e21667a0ea49a29119f836b6
Beni Cherniavsky-Paskin <user@example.com>
2021-06-13 22:57:46 +0300

    [SDA-4211] Extract CopyPullSecret, DownloadPullSecret components

--- src/components/clusters/install/instructions/components/__tests__/PullSecretSection.test.jsx
+++ src/components/clusters/install/instructions/components/__tests__/PullSecretSection.test.jsx
@@ -1,47 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Button } from '@patternfly/react-core';
 
 import PullSecretSection from '../PullSecretSection';
 
 describe('<PullSecretSection />', () => {
-  describe('PullSecretSection', () => {
-    const token = { error: '' };
+  describe('with token', () => {
+    const token = { auths: { foo: 'bar' } };
     const wrapper = shallow(<PullSecretSection token={token} />);
 
     it('should render', () => {
       expect(wrapper).toMatchSnapshot();
     });
-    it('should have Download', () => {
-      const download = wrapper.find('Download');
-      expect(download.length).toEqual(1);
-      const downloadButton = download.find(Button);
-      expect(downloadButton.length).toEqual(1);
-      expect(downloadButton.props().isDisabled).toEqual(false);
-    });
-    it('should have Copy', () => {
-      const copy = wrapper.find('CopyToClipboard');
-      expect(copy.length).toEqual(1);
-      const copyButton = copy.find(Button);
-      expect(copyButton.length).toEqual(1);
-      expect(copyButton.props().isDisabled).toEqual(false);
-    });
   });
 
-  describe('PullSecretSection Disabled', () => {
+  describe('with error', () => {
     const token = { error: 'my error' };
     const wrapper = shallow(<PullSecretSection token={token} />);
 
     it('should render', () => {
       expect(wrapper).toMatchSnapshot();
-    });
-    it('should disable Download and Copy', () => {
-      const download = wrapper.find('Download');
-      expect(download.length).toEqual(0);
-      const buttons = wrapper.find(Button);
-      expect(buttons.length).toEqual(2);
-      expect(buttons.at(0).props().isDisabled).toEqual(true);
-      expect(buttons.at(1).props().isDisabled).toEqual(true);
     });
   });
 });
---
+++ src/components/downloads/CopyPullSecret.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Tooltip,
+} from '@patternfly/react-core';
+import { PasteIcon } from '@patternfly/react-icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import isEmpty from 'lodash/isEmpty';
+
+class CopyPullSecret extends React.Component {
+  constructor(props) {
+    super(props);
+    this.timer = null;
+  }
+
+  state = { clicked: false };
+
+  onCopy = () => {
+    if (this.timer) {
+      window.clearTimeout(this.timer);
+    }
+    this.setState({ clicked: true }, () => {
+      this.timer = window.setTimeout(() => {
+        this.setState({ clicked: false });
+        this.timer = null;
+      }, 2500);
+    });
+  }
+
+  render() {
+    const { token, text, variant } = this.props;
+    const isDisabled = (!token || !!token.error || isEmpty(token));
+    const { clicked } = this.state;
+    const tokenView = token.error ? '' : `${JSON.stringify(token)}\n`;
+
+    if (variant === 'button') {
+      return (
+        <CopyToClipboard
+          text={isDisabled ? '' : tokenView}
+          onCopy={this.onCopy}
+        >
+          <Button
+            variant="secondary"
+            type="button"
+            tabIndex={0}
+            isDisabled={isDisabled}
+          >
+            {clicked ? 'Copied!' : text}
+          </Button>
+        </CopyToClipboard>
+      );
+    }
+
+    return (
+      <Tooltip
+        trigger="manual"
+        content="Copied!"
+        position="right"
+        isVisible={clicked}
+      >
+        <CopyToClipboard
+          text={isDisabled ? '' : tokenView}
+          onCopy={this.onCopy}
+        >
+          <Button
+            variant="link"
+            type="button"
+            tabIndex={0}
+            isDisabled={isDisabled}
+            icon={<PasteIcon />}
+          >
+            {text}
+          </Button>
+        </CopyToClipboard>
+      </Tooltip>
+    );
+  }
+}
+CopyPullSecret.propTypes = {
+  token: PropTypes.object.isRequired,
+  text: PropTypes.string,
+  variant: PropTypes.oneOf(['link', 'button']).isRequired,
+};
+CopyPullSecret.defaultProps = {
+  text: 'Copy pull secret',
+};
+
+export default CopyPullSecret;
