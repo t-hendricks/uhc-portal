29a5534e7f4edb43ff59d456da2266a009c5362a
Nimrod Shneor <user@example.com>
2021-07-19 15:06:49 +0300

    Fix 'canHibernateClusterSelector' selectors to use the response under .data

--- src/components/clusters/common/HibernateClusterModal/CanHibernateClusterListSelector.js
+++ src/components/clusters/common/HibernateClusterModal/CanHibernateClusterListSelector.js
@@ -5,7 +5,7 @@ const canHibernateClusterListSelector = (state) => {
 
   const canHibernateClusterList = {};
   clusters.forEach((cluster) => {
-    const capabilites = get(state, 'userProfile.organization.details.capabilities', []);
+    const capabilites = get(state, 'userProfile.organization.details.data.capabilities', []);
     const canHibernateCluster = capabilites.find(capability => capability.name === 'capability.organization.hibernate_cluster');
     canHibernateClusterList[cluster.id] = !!(canHibernateCluster && canHibernateCluster.value === 'true');
   });
