07cba1cb8929a3b45d60066443465b7adeeb97cb
Aleksandr Voznesenskii <user@example.com>
2021-10-01 09:17:29 +0000

    CCXDEV-6052 Remove the note about OCP clusters because it's no longer relevant

--- src/components/overview/InsightsAdvisorCard/InsightsAdvisorCard.jsx
+++ src/components/overview/InsightsAdvisorCard/InsightsAdvisorCard.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardBody } from '@patternfly/react-core';
-import { QuestionCircleIcon } from '@patternfly/react-icons';
-// eslint-disable-next-line camelcase
-import { global_Color_dark_200 } from '@patternfly/react-tokens';
 
 import ChartByRisks from './ChartByRisks';
 import ChartByGroups from './ChartByGroups';
@@ -21,14 +18,6 @@ const InsightsAdvisorCard = ({ overview }) => (
         <>
           <ChartByRisks riskHits={overview.hit_by_risk} />
           <ChartByGroups tagHits={overview.hit_by_tag} groups={INSIGHTS_RULE_CATEGORIES} />
-          <div className="ocm-insights--advisor-notice">
-            <QuestionCircleIcon
-              className="ocm-insights--advisor-notice__icon"
-              color={global_Color_dark_200.value}
-            />
-            {' '}
-            Advisor information applies for OCP clusters only
-          </div>
         </>
       )}
     </CardBody>
