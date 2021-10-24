513fc0cc6a34d4d466bf42687cbb29a1dd4f9674
Beni Cherniavsky-Paskin <user@example.com>
2021-10-24 13:10:15 +0300

    [SDA-4533] key ring & key dropdowns
    Dependent fields:
    
                                  gcp_service_account
                                      |         |
                                      v         v
        region -> key_location -> key_ring -> key_name
                          \_____________________^
    
    - [x] test old form
    - [x] test wizard
    - [x] Form values update correctly.
    - [x] no crash on JSON error
    - [x] repeats request after JSON error fixed
    - [x] no looping on error
    - [x] displays error
    - [ ] on change to key_location, can request encryption_keys with
        incorrect key_ring (race between both rings & keys components firing
        off requests before key_ring value is reset).
    - [x] validation: if left on placeholder, can't submit, shows red "Field is required".

---
+++ src/components/common/DynamicSelect.test.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import DynamicSelect from './DynamicSelect';
+
+describe('<DynamicSelect>', () => {
+  let loadData;
+  let onChange;
+  let baseProps;
+  beforeEach(() => {
+    loadData = jest.fn();
+    onChange = jest.fn();
+    baseProps = {
+      loadData,
+      input: {
+        onChange,
+        value: '',
+      },
+      meta: {
+        invalid: false,
+        error: null,
+        touched: false,
+      },
+      label: 'Label',
+      placeholder: 'Select foo',
+      emptyPlaceholder: 'No foos',
+      requestErrorTitle: 'Failed listing foos',
+      hasDependencies: true,
+      matchesDependencies: true,
+      requestStatus: {
+        pending: false,
+        fulfilled: false,
+        error: false,
+      },
+      items: ['Foo', 'Bar'],
+    };
+  });
+
+  it('without dependencies', () => {
+    const select = shallow(
+      <DynamicSelect {...baseProps} hasDependencies={false} />,
+    );
+    expect(select).toMatchSnapshot('blank');
+    expect(select.find('FormSelectOption').at(0).props().label).toEqual('');
+    expect(loadData).not.toBeCalled();
+  });
+
+  it('gets data', () => {
+    const select = shallow(
+      <DynamicSelect {...baseProps} matchesDependencies={false} />,
+    );
+    expect(loadData).toBeCalled();
+
+    select.setProps({
+      requestStatus: { pending: true, fulfilled: false, error: null },
+      matchesDependencies: true,
+    });
+    expect(select).toMatchSnapshot('loading');
+    expect(select.find('FormSelectOption').at(0).props().label).toEqual('Loading...');
+
+    select.setProps({
+      requestStatus: { pending: false, fulfilled: true, error: null },
+    });
+    expect(select).toMatchSnapshot('items');
+    expect(select.find('FormSelectOption').at(0).props().label).toEqual('Select foo');
+    expect(loadData).toBeCalledTimes(1);
+  });
+
+  it('empty data', () => {
+    const status = { pending: false, fulfilled: true, error: null };
+    const select = shallow(
+      <DynamicSelect {...baseProps} requestStatus={status} items={[]} />,
+    );
+    expect(loadData).not.toBeCalled();
+    expect(select).toMatchSnapshot('empty');
+    expect(select.find('FormSelectOption').at(0).props().label).toEqual('No foos');
+  });
+
+  it('error, untouched', () => {
+    const meta = { invalid: true, error: 'Bad!', touched: false };
+    const select = shallow(<DynamicSelect {...baseProps} meta={meta} />);
+    expect(select).toMatchSnapshot('no error');
+  });
+
+  it('error, touched', () => {
+    const meta = { invalid: true, error: 'Bad!', touched: true };
+    const select = shallow(<DynamicSelect {...baseProps} meta={meta} />);
+    expect(select).toMatchSnapshot('shows error');
+  });
+
+  it('resets when selected value no longer valid option', () => {
+    const status = { pending: false, fulfilled: true, error: null };
+    const select = shallow(
+      <DynamicSelect {...baseProps} input={{ value: 'Foo', onChange }} requestStatus={status} />,
+    );
+    expect(loadData).not.toBeCalled();
+    expect(onChange).not.toBeCalled();
+
+    select.setProps({
+      items: ['Bar', 'Baz'],
+    });
+    expect(onChange).toBeCalledWith('');
+    select.setProps({
+      input: { value: '', onChange },
+    });
+    expect(select).toMatchSnapshot('new items');
+    expect(select.find('FormSelectOption').at(0).props().label).toEqual('Select foo');
+  });
+
+  it('resets on lost dependencies', () => {
+    const status = { pending: false, fulfilled: true, error: null };
+    const select = shallow(
+      <DynamicSelect {...baseProps} input={{ value: 'Foo', onChange }} requestStatus={status} />,
+    );
+    expect(loadData).not.toBeCalled();
+    expect(onChange).not.toBeCalled();
+
+    select.setProps({
+      hasDependencies: false,
+      matchesDependencies: false,
+    });
+    expect(onChange).toBeCalledWith('');
+    select.setProps({
+      input: { value: '', onChange },
+    });
+    expect(select.find('FormSelectOption').at(0).props().label).toEqual('');
+  });
+});
