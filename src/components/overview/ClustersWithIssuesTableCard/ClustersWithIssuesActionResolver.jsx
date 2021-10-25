e5ea4b04a72450eebf97c7f0dd2f620872419992
Robb Hamilton <user@example.com>
2021-10-25 15:42:12 -0400

    [SDA-4633] fix bug where Open console menu option is not enabled for clusters with issues

--- src/components/overview/ClustersWithIssuesTableCard/ClustersWithIssuesActionResolver.jsx
+++ src/components/overview/ClustersWithIssuesTableCard/ClustersWithIssuesActionResolver.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import clusterStates from '../../clusters/common/clusterStates';
 
 /**
  * This function is used by PF tables to determine which dropdown items are displayed
  * on each row of the table. It returns a list of objects, containing props for DropdownItem
  * PF table renders automatically.
- * @param {*} cluster             The cluster object corresponding to the current row
+ * @param {*} subscription             The subscription object corresponding to the current row
  */
 function actionResolver(
-  cluster,
+  subscription,
 ) {
   const baseProps = {
     component: 'button',
   };
-  const uninstallingMessage = <span>The cluster is being uninstalled</span>;
   const consoleDisabledMessage = <span>Admin console is not yet available for this cluster</span>;
-  const isClusterUninstalling = cluster.state === clusterStates.UNINSTALLING;
-  const getKey = item => `${cluster.id}.menu.${item}`;
-  const consoleURL = cluster.console ? cluster.console.url : false;
+  const getKey = item => `${subscription.id}.menu.${item}`;
+  const consoleURL = subscription.console_url;
   const adminConsoleEnabled = {
     component: 'a',
     title: 'Open console',
@@ -30,11 +27,10 @@ function actionResolver(
     ...baseProps,
     title: 'Open console',
     isDisabled: true,
-    tooltip: isClusterUninstalling ? uninstallingMessage : consoleDisabledMessage,
+    tooltip: consoleDisabledMessage,
     key: getKey('adminconsole'),
   };
-  const consoleItemProps = consoleURL
-   && !isClusterUninstalling ? adminConsoleEnabled : adminConsoleDisabled;
+  const consoleItemProps = consoleURL ? adminConsoleEnabled : adminConsoleDisabled;
 
   return [consoleItemProps];
 }
