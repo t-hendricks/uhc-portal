t-hendricks/uhc-portal

commit c5cfe6e152e2385210dbb49b2c9e96a712ede146

refactor: selector for AWS/GCP credentials
- Simplify action calling.
- Use deep isEqual() for AWS credentials comparison
  instead of constructing 'a/b/c' string.

src/components/clusters/CreateOSDPage/CreateOSDWizard/credentialsSelector.js
@@ -1,0 +1,27 @@
+import { formValueSelector } from 'redux-form';
+
+/**
+ * Gets AWS or GCP CCS credentials from form state, in form suitable for actions.
+ * TODO: consider moving credentialsFromJSON() here, change actions to take object rather than JSON?
+ * TODO: can/should we reuse these in createClusterRequest()?
+ *   Note that creation request puts more fields under 'aws' e.g. 'kms_key_arn'.
+ */
+
+const ccsCredentialsSelector = (state) => {
+  const valueSelector = formValueSelector('CreateCluster');
+  const cloudProviderID = valueSelector(state, 'cloud_provider');
+  switch (cloudProviderID) {
+    case 'gcp':
+      return valueSelector(state, 'gcp_service_account');
+    case 'aws':
+      return {
+        access_key_id: valueSelector(state, 'access_key_id'),
+        account_id: valueSelector(state, 'account_id'),
+        secret_access_key: valueSelector(state, 'secret_access_key'),
+      };
+    default: // can happen before user selected provider
+      return null;
+  }
+};
+
+export default ccsCredentialsSelector;

src/components/clusters/CreateOSDPage/CreateOSDWizard/index.js
@@ -17,6 +17,7 @@ import {
   hasManagedQuotaSelector,
 } from '../../common/quotaSelectors';
 import isCCSCredentialsValidationNeeded from './isCCSCredentialsValidationNeeded';
+import ccsCredentialsSelector from './credentialsSelector';
 import { getGCPCloudProviderVPCs, getAWSCloudProviderRegions } from './ccsInquiriesActions';
 
 import submitOSDRequest from '../submitOSDRequest';
@@ -35,13 +36,10 @@ const mapStateToProps = (state, ownProps) => {
   return ({
     isValid: isValid('CreateCluster')(state),
     isErrorModalOpen: shouldShowModal(state, 'osd-create-error'),
-    isCCSCredentialsValidationNeeded: isCCSCredentialsValidationNeeded(state, valueSelector),
+    ccsCredentials: ccsCredentialsSelector(state),
+    isCCSCredentialsValidationNeeded: isCCSCredentialsValidationNeeded(state),
     ccsValidationPending: ccsCredentialsValidityResponse.pending && valueSelector(state, 'byoc') === 'true',
     cloudProviderID: valueSelector(state, 'cloud_provider'),
-    gcpCredentialsJSON: valueSelector(state, 'gcp_service_account'),
-    awsAccountID: valueSelector(state, 'account_id'),
-    awsAccessKey: valueSelector(state, 'access_key_id'),
-    awsSecretKey: valueSelector(state, 'secret_access_key'),
 
     createClusterResponse: state.clusters.createdCluster,
     machineTypes: state.machineTypes,
@@ -73,8 +71,8 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   getGCPCloudProviderVPCs: (type, credentials, region) => dispatch(
     getGCPCloudProviderVPCs(type, credentials, region),
   ),
-  getAWSCloudProviderRegions: (accountID, accessKey, secretKey) => dispatch(
-    getAWSCloudProviderRegions(accountID, accessKey, secretKey),
+  getAWSCloudProviderRegions: credentials => dispatch(
+    getAWSCloudProviderRegions(credentials),
   ),
 });
 

src/components/clusters/CreateOSDPage/CreateOSDWizard/isCCSCredentialsValidationNeeded.js
@@ -1,21 +1,24 @@
+import isEqual from 'lodash/isEqual';
+import { formValueSelector } from 'redux-form';
+
+import ccsCredentialsSelector from './credentialsSelector';
+
 /**
  * Returns true when validation is needed for CCS credentials.
  *
  * @param {*} state redux state
- * @param {*} valueSelector redux-form valueSelector
  * @returns {Boolean} is validation needed for CCS credentials
  */
-const isCCSCredentialsValidationNeeded = (state, valueSelector) => {
+const isCCSCredentialsValidationNeeded = (state) => {
+  const valueSelector = formValueSelector('CreateCluster');
   const ccsCredentialsValidityResponse = state.ccsInquiries.ccsCredentialsValidity;
   // credentials is a "key" to check the response we have matches the currently entered details.
   // if the details change, we need to run validations again.
-  const credentialsKey = valueSelector(state, 'cloud_provider') === 'gcp'
-    ? valueSelector(state, 'gcp_service_account')
-    : `${valueSelector(state, 'account_id')}/${valueSelector(state, 'access_key_id')}/${valueSelector(state, 'secret_access_key')}`;
+  const credentialsKey = ccsCredentialsSelector(state);
 
   const areCCSCredentialsValid = ccsCredentialsValidityResponse.fulfilled
-                                 && ccsCredentialsValidityResponse.cloudProvider === valueSelector(state, 'cloud_provider')
-                                 && ccsCredentialsValidityResponse.credentials === credentialsKey;
+    && ccsCredentialsValidityResponse.cloudProvider === valueSelector(state, 'cloud_provider')
+    && isEqual(ccsCredentialsValidityResponse.credentials, credentialsKey);
 
   return valueSelector(state, 'byoc') === 'true' && !areCCSCredentialsValid;
 };

src/components/clusters/CreateOSDPage/CreateOSDWizard/CreateOSDWizard.jsx
@@ -127,11 +127,8 @@ class CreateOSDWizard extends React.Component {
       ccsValidationPending,
       isCCSCredentialsValidationNeeded,
       cloudProviderID,
+      ccsCredentials,
       getGCPCloudProviderVPCs,
-      gcpCredentialsJSON,
-      awsAccountID,
-      awsAccessKey,
-      awsSecretKey,
       getAWSCloudProviderRegions,
     } = this.props;
 
@@ -330,9 +327,9 @@ class CreateOSDWizard extends React.Component {
                     onClick={() => {
                       if (cloudProviderID === 'gcp') {
                         // hard code region since we're just validating credentials
-                        getGCPCloudProviderVPCs(VALIDATE_CLOUD_PROVIDER_CREDENTIALS, gcpCredentialsJSON, 'us-east1');
+                        getGCPCloudProviderVPCs(VALIDATE_CLOUD_PROVIDER_CREDENTIALS, ccsCredentials, 'us-east1');
                       } else {
-                        getAWSCloudProviderRegions(awsAccountID, awsAccessKey, awsSecretKey);
+                        getAWSCloudProviderRegions(ccsCredentials);
                       }
                     }}
                     isLoading={ccsValidationPending}
@@ -361,9 +358,7 @@ CreateOSDWizard.propTypes = {
   isCCSCredentialsValidationNeeded: PropTypes.bool,
   cloudProviderID: PropTypes.string,
   isErrorModalOpen: PropTypes.bool,
-  awsAccountID: PropTypes.string,
-  awsAccessKey: PropTypes.string,
-  awsSecretKey: PropTypes.string,
+  ccsCredentials: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 
   createClusterResponse: PropTypes.shape({
     fulfilled: PropTypes.bool,
@@ -404,7 +399,6 @@ CreateOSDWizard.propTypes = {
 
   // for the /create/osdtrial url
   product: PropTypes.string,
-  gcpCredentialsJSON: PropTypes.string,
 };
 
 export default CreateOSDWizard;

src/components/clusters/CreateOSDPage/CreateOSDWizard/ccsInquiriesActions.js
@@ -34,11 +34,15 @@ export const getGCPCloudProviderVPCs = (type, gcpCredentialsJSON, region) => ({
   meta: { credentials: gcpCredentialsJSON, cloudProvider: 'gcp', region },
 });
 
-export const getAWSCloudProviderRegions = (accountID, accessKey, secretKey) => ({
+/**
+ * Validate AWS credentials.
+ * @param {*} awsCredentials { accountID, accessKey, secretKey } object
+ */
+export const getAWSCloudProviderRegions = awsCredentials => ({
   type: VALIDATE_CLOUD_PROVIDER_CREDENTIALS,
-  payload: listAWSRegions(accountID, accessKey, secretKey),
+  payload: listAWSRegions(awsCredentials),
   meta: {
-    credentials: `${accountID}/${accessKey}/${secretKey}`,
+    credentials: awsCredentials,
     cloudProvider: 'aws',
   },
 });

src/services/clusterService.js
@@ -314,15 +314,15 @@ const listGCPKeys = (credentials, location, ring) => apiRequest({
   },
 });
 
-const listAWSRegions = (accountID, accessKey, secretKey) => apiRequest({
+/**
+ * List AWS regions for given CCS account.
+ * @param {*} credentials { accountID, accessKey, secretKey } object
+ */
+const listAWSRegions = credentials => apiRequest({
   method: 'post',
   url: '/api/clusters_mgmt/v1/aws_inquiries/regions',
   data: {
-    aws: {
-      account_id: accountID,
-      access_key_id: accessKey,
-      secret_access_key: secretKey,
-    },
+    aws: credentials,
   },
 });
 

commit 37626343535d0cb7de8be0a8595cfaa09ecf07a6

[HAC-441] Fix GCP selection
Possibly broken since commit c5cfe6e152e2385210dbb49b2c9e96a712ede146?

- CreateOSDForm has no 'cloud_provider' field, I don't see how it could have worked.
  => Refactored helper funcitons to take explicit cloudProviderID param.
- CreateOSDWizard has 'cloud_provider' field, passing it.
- CreateROSAWizard doesn't now use `isCCSCredentialsValidationNeeded()`
  but I think it'll need it later?
  (This function still accesses 'byoc' field which ROSA wizard gets
  accidentally from `createOSDInitialValues()`, will need care.)

See also https://gitlab.cee.redhat.com/service/uhc-portal/-/merge_requests/3033
unit test for reference which form has which fields...

src/components/clusters/CreateOSDPage/CreateOSDForm/FormSections/EncryptionSection/KMSKeyRingSelect/index.js
@@ -10,7 +10,7 @@ import DynamicSelect from '../../../../../../common/DynamicSelect';
 const mapStateToProps = (state) => {
   const { gcpKeyRings } = state.ccsInquiries;
   const valueSelector = formValueSelector('CreateCluster');
-  const credentials = ccsCredentialsSelector(state);
+  const credentials = ccsCredentialsSelector('gcp', state);
   const keyLocation = valueSelector(state, 'key_location');
   const hasDependencies = !!(credentials && keyLocation);
   const matchesDependencies = (
@@ -32,7 +32,7 @@ const mapDispatchToProps = {
   loadData: () => (dispatch, getState) => {
     const state = getState();
     const valueSelector = formValueSelector('CreateCluster');
-    const credentials = ccsCredentialsSelector(state);
+    const credentials = ccsCredentialsSelector('gcp', state);
     const keyLocation = valueSelector(state, 'key_location');
 
     dispatch(getGCPKeyRings(credentials, keyLocation));

src/components/clusters/CreateOSDPage/CreateOSDForm/FormSections/EncryptionSection/KMSKeySelect/index.js
@@ -10,7 +10,7 @@ import DynamicSelect from '../../../../../../common/DynamicSelect';
 const mapStateToProps = (state) => {
   const { gcpKeys } = state.ccsInquiries;
   const valueSelector = formValueSelector('CreateCluster');
-  const credentials = ccsCredentialsSelector(state);
+  const credentials = ccsCredentialsSelector('gcp', state);
   const keyLocation = valueSelector(state, 'key_location');
   const keyRing = valueSelector(state, 'key_ring');
   const hasDependencies = !!(credentials && keyLocation && keyRing);
@@ -34,7 +34,7 @@ const mapDispatchToProps = {
   loadData: () => (dispatch, getState) => {
     const state = getState();
     const valueSelector = formValueSelector('CreateCluster');
-    const credentials = ccsCredentialsSelector(state);
+    const credentials = ccsCredentialsSelector('gcp', state);
     const keyLocation = valueSelector(state, 'key_location');
     const keyRing = valueSelector(state, 'key_ring');
 

src/components/clusters/CreateOSDPage/CreateOSDWizard/NetworkScreen/GCPVPCName/index.js
@@ -10,7 +10,7 @@ import GCPVPCName from './GCPVPCName';
 const mapStateToProps = (state) => {
   const { vpcs } = state.ccsInquiries;
   const valueSelector = formValueSelector('CreateCluster');
-  const credentials = ccsCredentialsSelector(state);
+  const credentials = ccsCredentialsSelector('gcp', state);
   const region = valueSelector(state, 'region');
   const hasDependencies = !!(credentials && region);
   const matchesDependencies = (

src/components/clusters/CreateOSDPage/CreateOSDWizard/NetworkScreen/GCPVPCSubnet/index.js
@@ -10,7 +10,7 @@ import GCPVPCSubnet from './GCPVPCSubnet';
 const mapStateToProps = (state) => {
   const { vpcs } = state.ccsInquiries;
   const valueSelector = formValueSelector('CreateCluster');
-  const credentials = ccsCredentialsSelector(state);
+  const credentials = ccsCredentialsSelector('gcp', state);
   const region = valueSelector(state, 'region');
   const vpcName = valueSelector(state, 'vpc_name');
   const hasDependencies = !!(credentials && region && vpcName);

src/components/clusters/CreateOSDPage/CreateOSDWizard/credentialsSelector.js
@@ -7,9 +7,8 @@ import { formValueSelector } from 'redux-form';
  *   Note that creation request puts more fields under 'aws' e.g. 'kms_key_arn'.
  */
 
-const ccsCredentialsSelector = (state) => {
+const ccsCredentialsSelector = (cloudProviderID, state) => {
   const valueSelector = formValueSelector('CreateCluster');
-  const cloudProviderID = valueSelector(state, 'cloud_provider'); // TODO: ROSA has no such field!
   switch (cloudProviderID) {
     case 'gcp':
       return valueSelector(state, 'gcp_service_account');

src/components/clusters/CreateOSDPage/CreateOSDWizard/index.js
@@ -31,6 +31,7 @@ const mapStateToProps = (state, ownProps) => {
   // thus it could differ from the product in the URL
   const selectedProduct = valueSelector(state, 'product');
   const product = selectedProduct || ownProps.product;
+  const cloudProviderID = valueSelector(state, 'cloud_provider');
   const ccsCredentialsValidityResponse = state.ccsInquiries.ccsCredentialsValidity;
 
   const isCCS = valueSelector(state, 'byoc') === 'true';
@@ -38,11 +39,11 @@ const mapStateToProps = (state, ownProps) => {
   return ({
     isValid: isValid('CreateCluster')(state),
     isErrorModalOpen: shouldShowModal(state, 'osd-create-error'),
-    ccsCredentials: ccsCredentialsSelector(state),
+    ccsCredentials: ccsCredentialsSelector(cloudProviderID, state),
     isCCS,
-    isCCSCredentialsValidationNeeded: isCCSCredentialsValidationNeeded(state),
+    isCCSCredentialsValidationNeeded: isCCSCredentialsValidationNeeded(cloudProviderID, state),
     ccsValidationPending: ccsCredentialsValidityResponse.pending && isCCS,
-    cloudProviderID: valueSelector(state, 'cloud_provider'),
+    cloudProviderID,
     installToVPCSelected: valueSelector(state, 'install_to_vpc'),
 
     createClusterResponse: state.clusters.createdCluster,

src/components/clusters/CreateOSDPage/CreateOSDWizard/isCCSCredentialsValidationNeeded.js
@@ -6,18 +6,19 @@ import ccsCredentialsSelector from './credentialsSelector';
 /**
  * Returns true when validation is needed for CCS credentials.
  *
+ * @param {String} cloudProviderID (separate since only OSD wizard has 'cloud_provider' field)
  * @param {*} state redux state
  * @returns {Boolean} is validation needed for CCS credentials
  */
-const isCCSCredentialsValidationNeeded = (state) => {
+const isCCSCredentialsValidationNeeded = (cloudProviderID, state) => {
   const valueSelector = formValueSelector('CreateCluster');
   const ccsCredentialsValidityResponse = state.ccsInquiries.ccsCredentialsValidity;
   // credentials is a "key" to check the response we have matches the currently entered details.
   // if the details change, we need to run validations again.
-  const credentialsKey = ccsCredentialsSelector(state);
+  const credentialsKey = ccsCredentialsSelector(cloudProviderID, state);
 
   const areCCSCredentialsValid = ccsCredentialsValidityResponse.fulfilled
-    && ccsCredentialsValidityResponse.cloudProvider === valueSelector(state, 'cloud_provider')
+    && ccsCredentialsValidityResponse.cloudProvider === cloudProviderID
     && isEqual(ccsCredentialsValidityResponse.credentials, credentialsKey);
 
   return valueSelector(state, 'byoc') === 'true' && !areCCSCredentialsValid;