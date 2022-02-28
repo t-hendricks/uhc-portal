83d526f9ebeebb46e5424ee75e209b6afcd86242
Beni Cherniavsky-Paskin <user@example.com>
2022-02-28 10:01:01 +0000

    Merge branch 'ocm-to-advisor-redirect-1' into 'master'
    Hide the Insights Advisor tab; set redirection to OCP Advisor
    
    Closes CCXDEV-6786 and CCXDEV-7179
    
    See merge request service/uhc-portal!3064
    
    (cherry picked from commit 8ebd83b4c2e149182241fddd101f5efabc941aa5)

---
+++ src/components/clusters/InsightsAdvisorRedirector/InsightsAdvisorRedirector.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { matchPath, Redirect } from 'react-router';
+import { Spinner } from '@redhat-cloud-services/frontend-components/Spinner';
+import { Bullseye } from '@patternfly/react-core/dist/js/layouts/Bullseye/Bullseye';
+import { validate as isUuid } from 'uuid';
+import ExternalRedirect from './ExternalRedirect';
+
+// expects the pluginName and errorKey to construct the ruleId recognized by OCP Advisor
+export const composeRuleId = (pluginName, errorKey) => encodeURIComponent(`${
+  decodeURIComponent(pluginName).replace(/\|/g, '.').split('.report')[0]
+}|${decodeURIComponent(errorKey)}`);
+
+class InsightsAdvisorRedirector extends React.Component {
+  componentDidMount() {
+    const { match, clusterDetails, fetchClusterDetails } = this.props;
+    if (!clusterDetails.fulfilled && match.params.id && !isUuid(match.params.id)) {
+      fetchClusterDetails(match.params.id);
+    }
+  }
+
+  render() {
+    const {
+      clusterDetails, match, location, setGlobalError,
+    } = this.props;
+    const externalId = (match.params?.id && isUuid(match.params.id))
+      ? match.params.id
+      : clusterDetails?.cluster?.external_id;
+    const path = location.pathname;
+
+    if (externalId) {
+      if (
+        matchPath(path, {
+          path: '/details/:clusterId',
+          exact: true,
+        })
+        || matchPath(path, {
+          path: '/details/s/:id',
+          exact: true,
+        })
+      ) {
+        return (
+          <ExternalRedirect url={`${window.location.origin}/${APP_BETA ? 'beta/' : ''}openshift/insights/advisor/clusters/${externalId}`} />
+        );
+      } if (
+        matchPath(path, {
+          path: '/details/:id/insights/:reportId/:errorKey',
+          exact: true,
+        })
+        || matchPath(path, {
+          path: '/details/s/:id/insights/:reportId/:errorKey',
+          exact: true,
+          strict: true,
+        })
+      ) {
+        const { reportId, errorKey } = match.params;
+        const ruleId = composeRuleId(reportId, errorKey);
+        return (
+          <ExternalRedirect url={`${window.location.origin}/${APP_BETA ? 'beta/' : ''}openshift/insights/advisor/clusters/${externalId}?first=${ruleId}`} />
+        );
+      }
+    }
+
+    if (clusterDetails.error) {
+      // Cluster not found or no permission to see it - redirect to cluster list with error on top
+      setGlobalError(
+        <>
+          Cluster with the subscription ID
+          {' '}
+          <b>{match.params?.id}</b>
+          {' '}
+          was not found, it might have
+          been deleted or you don&apos;t have permission to see it.
+        </>,
+        'clusterDetails',
+        clusterDetails?.errorMessage,
+      );
+      return <Redirect to="/" />;
+    }
+
+    if (clusterDetails.fulfilled && !externalId) {
+      // Could not find external id for the given cluster
+      setGlobalError(
+        <>
+          There is no external ID for the cluster with the
+          {' '}
+          <b>{match.params?.id}</b>
+          {' '}
+          subscription ID.
+        </>,
+        'clusterDetails',
+        clusterDetails?.errorMessage,
+      );
+      return <Redirect to="/" />;
+    }
+
+    return (
+      <Bullseye>
+        <Spinner size="lg" centered />
+      </Bullseye>
+    );
+  }
+}
+
+InsightsAdvisorRedirector.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+      reportId: PropTypes.string,
+      errorKey: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  fetchClusterDetails: PropTypes.func.isRequired,
+  clusterDetails: PropTypes.shape({
+    cluster: PropTypes.object,
+    error: PropTypes.bool,
+    errorCode: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    errorMessage: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.node,
+      PropTypes.element,
+    ]),
+    fulfilled: PropTypes.bool,
+    history: PropTypes.object,
+  }),
+  setGlobalError: PropTypes.func.isRequired,
+};
+
+export default InsightsAdvisorRedirector;
