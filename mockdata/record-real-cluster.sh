33675d733a795d3660acef2e0010ac8050dde4e9
Beni Cherniavsky-Paskin <user@example.com>
2022-04-07 15:44:42 +0300

    [HAC-530, HAC-1175] mockdata updates
    - capture most requests with size=-1 (specifically machine_types on staging
      has 171 types).
    
    - minor updates from running the script.
    
    - consistent data for quota_cost & machine_types (matches on generic_name)
      quota_cost is synthetic mixture from old & new data from 2 users.

--- mockdata/record-real-cluster.sh
+++ mockdata/record-real-cluster.sh
@@ -50,16 +50,19 @@ else
     jq '.. | select(try .kind | test(".*Link")).href' --raw-output |
     sort --unique |
     while read -r href; do
-      request "  " "$href"
+      # Linked hrefs are a mix of collections (e.g. /api/clusters_mgmt/v1/clusters/.../machine_pools)
+      # and single objects (e.g. /api/clusters_mgmt/v1/machine_types/r5.xlarge).
+      # size=-1 parameter makes no sense for the latter, but backend happily ignores it (as of May 2022).
+      request "  " "$href" --parameter=size=-1
     done
 
   request "" "$cluster_href/status"
   request "" "$cluster_href/logs/install"
   request "" "$cluster_href/logs/uninstall"
-  request "" "$cluster_href/addon_inquiries"
-  request "" "$cluster_href/gate_agreements"
+  request "" "$cluster_href/addon_inquiries" --parameter=size=-1
+  request "" "$cluster_href/gate_agreements" --parameter=size=-1
 
-  request "" "$cluster_href/upgrade_policies" --parameter="search=upgrade_type='OSD'"
+  request "" "$cluster_href/upgrade_policies" --parameter="search=upgrade_type='OSD'" --parameter=size=-1
   cat "mockdata/$cluster_href/upgrade_policies.json" |
     jq '.items[].href' --raw-output |
     while read -r policy_href; do
@@ -70,18 +73,18 @@ else
 
   subscription_href="$(jq .subscription.href "mockdata/$cluster_href".json --raw-output)"
   request "" "$subscription_href" --parameter=fetchAccounts=true --parameter=fetchCpuAndSocket=true --parameter=fetchCapabilities=true
-  request "" "$subscription_href/notification_contacts"
-  request "" "$subscription_href/support_cases"
-  request "" "$subscription_href/ondemand_metrics"
+  request "" "$subscription_href/notification_contacts" --parameter=size=-1
+  request "" "$subscription_href/support_cases" --parameter=size=1000  # SDB-2817: size=-1 here is expensive and may return errors
+  request "" "$subscription_href/ondemand_metrics" --parameter=size=-1
 fi
 
 request "" "/api/accounts_mgmt/v1/current_account"
 
 org_href=$(jq .organization.href  "mockdata/api/accounts_mgmt/v1/current_account".json --raw-output)
 request "" "$org_href" --parameter=fetchCapabilities=true
-request "" "$org_href/quota_cost" --parameter=fetchRelatedResources=true
+request "" "$org_href/quota_cost" --parameter=fetchRelatedResources=true --parameter=size=-1
 
-request "" "/api/clusters_mgmt/v1/version_gates"
+request "" "/api/clusters_mgmt/v1/version_gates" --parameter=size=-1
 
 # Overwrite with more details.
 request "" "/api/clusters_mgmt/v1/cloud_providers" --parameter=size=-1 --parameter=fetchRegions=true
