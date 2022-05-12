e64777a51a4291f846a9fd11227a2adb0f0043c5
David Taylor <user@example.com>
2022-05-12 14:07:34 -0400

    added 'Cluster roles and policies' section to rosa wizard review page

--- src/components/clusters/CreateOSDPage/CreateOSDWizard/ReviewClusterScreen/reviewValues.jsx
+++ src/components/clusters/CreateOSDPage/CreateOSDWizard/ReviewClusterScreen/reviewValues.jsx
@@ -59,9 +59,12 @@ const reviewValues = {
   name: {
     title: 'Cluster name',
   },
-  operator_roles_name: {
-    title: 'Operator roles prefix preview',
-    valueTransform: (value, allValues) => `${allValues.name}-${allValues.custom_operator_roles_prefix}`,
+  rosa_roles_provider_creation_mode: {
+    title: 'Operator roles and OIDC provider mode',
+  },
+  custom_operator_roles_prefix: {
+    title: 'Operator roles prefix',
+    valueTransform: (value, allValues) => `${allValues.name}-${value}`,
   },
   cluster_version: {
     title: 'Version',
