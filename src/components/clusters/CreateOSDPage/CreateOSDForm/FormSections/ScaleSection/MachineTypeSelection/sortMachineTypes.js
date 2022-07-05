0710ad30982e8204d3cbd7b1eeb93ef0b37fc48d
Beni Cherniavsky-Paskin <user@example.com>
2022-07-05 14:29:46 +0300

    refactor: rename internal comparison functions

--- src/components/clusters/CreateOSDPage/CreateOSDForm/FormSections/ScaleSection/MachineTypeSelection/sortMachineTypes.js
+++ src/components/clusters/CreateOSDPage/CreateOSDForm/FormSections/ScaleSection/MachineTypeSelection/sortMachineTypes.js
@@ -6,17 +6,17 @@ import { machineCategories } from './MachineTypeSelection';
 
 const categories = map(machineCategories, 'name');
 
-function sortByMemory(a, b) {
+function compareByMemory(a, b) {
   // sort by memory, ascending order
   const memoryBytesA = parseValueWithUnit(a.memory.value, a.memory.unit);
   const memoryBytesB = parseValueWithUnit(b.memory.value, b.memory.unit);
   return memoryBytesA - memoryBytesB;
 }
 
-function sortMachineTypesByCategories(a, b) {
+function compareByCategoryMemoryCPU(a, b) {
   if (a.category === b.category) {
     // the categories are equal. Within the category, we need to sort by size.
-    const memorySortResult = sortByMemory(a, b);
+    const memorySortResult = compareByMemory(a, b);
     if (memorySortResult === 0) {
       // sort by CPU if two machine types have the same amount of memory
       return a.cpu.value - b.cpu.value;
@@ -31,8 +31,8 @@ function sortMachineTypesByCategories(a, b) {
  * If we ever add new cloud providers that require custom sorting, they can be added here.
  */
 const sortFuncs = {
-  gcp: sortMachineTypesByCategories,
-  aws: sortMachineTypesByCategories,
+  gcp: compareByCategoryMemoryCPU,
+  aws: compareByCategoryMemoryCPU,
 };
 
 function sortMachineTypes(state, cloudProviderID) {
