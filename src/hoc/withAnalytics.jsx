165f6013e9ce2b697214b57c5c566232ab801dda
eliran <user@example.com>
2022-07-22 05:03:08 +0300

    feat: add wizard navigation tracking
    - add jsdoc
    - refactor: rename

--- src/hoc/withAnalytics.jsx
+++ src/hoc/withAnalytics.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import useChrome from '@redhat-cloud-services/frontend-components/useChrome/useChrome';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
-const withAnalytics = Component => (props) => {
+/**
+ * A Higher-Order Component to provide an analytics API instance.
+ *
+ * @param AnalyticsContext {function(*)|React.Component|React.PureComponent}
+ * The component to receive the analytics instance.
+ * @returns {function(*)}
+ * A wrapped component, enriched with the analytics API as a property.
+ */
+const withAnalytics = AnalyticsContext => (props) => {
   const { analytics } = useChrome();
 
   return (
-    <Component
+    <AnalyticsContext
       {...props}
       analytics={analytics}
     />
