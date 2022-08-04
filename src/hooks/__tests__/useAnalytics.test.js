3ff88b77f433397b1e692d4fc585fcc5006a1bd6
eliran <user@example.com>
2022-08-04 05:41:06 +0300

    refactor: analytics encapsulation
    add stub unit-tests

--- src/hooks/__tests__/useAnalytics.test.js
+++ src/hooks/__tests__/useAnalytics.test.js
@@ -1,3 +1,16 @@
-/*
-todo - test all the stuff
-*/
+import { renderHook } from '@testing-library/react-hooks';
+import useAnalytics from '../useAnalytics';
+
+describe('useAnalytics', () => {
+  it.skip('...', () => {
+    renderHook(useAnalytics);
+    expect().toBe();
+  });
+
+  it.skip('... when the hook is unmounted', () => {
+    const { unmount } = renderHook(() => useAnalytics());
+
+    unmount();
+    expect().toBeNull();
+  });
+});
