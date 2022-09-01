06e25063ed17ae371dfdbde8455e42ce485b7cce
Christian Vogt <user@example.com>
2022-09-01 16:00:14 -0400

    add script to generate type definitions for apis

---
+++ scripts/gen-openapi-typescript.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const fs = require('fs');
+const OpenAPI = require('openapi-typescript-codegen');
+
+const sourceDir = path.resolve(__dirname, '../openapi');
+const targetDir = path.resolve(__dirname, '../src/types');
+
+// Ensures that all component schemas have a defined type,
+// otherwise the library will generate an empty model type definition.
+const processJsonAPI = (jsonString) => {
+  const json = JSON.parse(jsonString);
+  if (json.components) {
+    Object.keys(json.components.schemas).forEach((key) => {
+      const schema = json.components.schemas[key];
+      if (schema && typeof schema.type === 'undefined') {
+        schema.type = 'object';
+      }
+    });
+  }
+  return json;
+};
+
+fs.readdir(sourceDir, (err, files) => {
+  files.forEach((file) => {
+    const filepath = path.resolve(sourceDir, file);
+    if (!fs.statSync(filepath).isDirectory()) {
+      const output = path.resolve(
+        targetDir,
+        path.basename(file, path.extname(file)),
+      );
+      // fs.rmSync(output, { recursive: true, force: true });
+      OpenAPI.generate({
+        input: processJsonAPI(fs.readFileSync(filepath).toString()),
+        output,
+        exportCore: false,
+        exportServices: false,
+        exportModels: true,
+        exportSchemas: false,
+      });
+    }
+  });
+});
