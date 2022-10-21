1ed64f132aad039206aba78707e5b4e5b0eb39aa
Yaakov Selkowitz <user@example.com>
2022-10-20 21:37:57 -0400

    install: update Developer Preview text and link
    The ocp-dev-preview/pre-release directory now holds 4.next Early
    Candidates instead of nightlies, and there is a new official page
    defining Developer Preview.

--- src/components/clusters/install/instructions/components/DeveloperPreviewStatements.jsx
+++ src/components/clusters/install/instructions/components/DeveloperPreviewStatements.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Text, TextList, TextListItem } from '@patternfly/react-core';
 
+import links from '../../../../../common/installLinks.mjs';
+import ExternalLink from '../../../../common/ExternalLink';
+
 const DeveloperPreviewStatements = () => (
   <>
-    <Text component="p">Because these are developer preview builds:</Text>
+    <Text component="p">
+      Because these are{' '}
+      <ExternalLink href={links.INSTALL_PRE_RELEASE_SUPPORT_KCS} noIcon>
+        developer preview
+      </ExternalLink>{' '}
+      builds:
+    </Text>
     <TextList>
       <TextListItem>Production use is not permitted.</TextListItem>
       <TextListItem>
         Installation and use is not eligible for Red Hat production support.
       </TextListItem>
       <TextListItem>
-        Clusters installed at pre-release versions cannot be upgraded. As we approach a GA milestone
-        with these nightly builds, we will allow upgrades from a nightly to a nightly; however, we
-        will not support an upgrade from a nightly to the final GA build of OCP.
+        Upgrades to, from, or between developer preview versions are not supported.
       </TextListItem>
     </TextList>
   </>
