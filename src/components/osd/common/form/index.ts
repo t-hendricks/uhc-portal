4db6b12e7578812ed6d63b378e139287b9f2ccae
Jeff Puzzo <user@example.com>
2022-11-04 00:06:26 +0000

    [HAC-2205] OSD wizard: Refactor Cluster Settings - Details step

--- src/components/osd/common/form/index.ts
+++ src/components/osd/common/form/index.ts
@@ -1,3 +1,5 @@
 export { TextInputField } from './TextInputField';
 export { CheckboxField } from './CheckboxField';
 export { FileUploadField } from './FileUploadField';
+export { RadioGroupField } from './RadioGroupField';
+export { RichInputField } from './RichInputField';
