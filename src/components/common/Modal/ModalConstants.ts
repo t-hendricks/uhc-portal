1d1f7b5eeefd58dd2712928e6e31bf831e462b28
Joachim Schuler <user@example.com>
2022-12-20 17:26:59 +0000

    Merge branch 'ts-common3' into 'master'
    [HAC-2366] migrate components/common/Modal to typescript
    
    See merge request service/uhc-portal!3715
    
    (cherry picked from commit 00f241d83c621efc19b4e8d5b954db19891d21eb)

---
+++ src/components/common/Modal/ModalConstants.ts
@@ -0,0 +1,4 @@
+const OPEN_MODAL = 'OPEN_MODAL';
+const CLOSE_MODAL = 'CLOSE_MODAL';
+
+export { OPEN_MODAL, CLOSE_MODAL };
--- src/components/common/Modal/tests/ModalSelectors.test.ts
+++ src/components/common/Modal/tests/ModalSelectors.test.ts
@@ -1,18 +1,17 @@
+import { GlobalState } from '~/redux/store';
 import reducer from '../ModalReducer';
 import shouldShowModal from '../ModalSelectors';
 
 describe('Modal Selector', () => {
   it('should return true if modal name matches', () => {
-    const activeModalState = { modal: { modalName: 'test-modal' } };
+    const activeModalState = { modal: { modalName: 'test-modal', data: {} } } as GlobalState;
     const result = shouldShowModal(activeModalState, 'test-modal');
-
     expect(result).toBeTruthy();
   });
 
   it('should return false if modal name does not matches', () => {
-    const state = { modal: reducer.initialState };
+    const state = { modal: reducer.initialState } as GlobalState;
     const result = shouldShowModal(state, 'test-modal');
-
     expect(result).toBeFalsy();
   });
 });
