e418ddec5132837852fd55f90bb83888fafb86a9
Dave Taylor <user@example.com>
2022-12-20 14:57:50 +0000

    Merge branch 'HAC-780-kms-key-and-cluster-match' into 'master'
    [HAC-780] Validate kms_key matches the selected region
    
    Closes HAC-780
    
    See merge request service/uhc-portal!3772
    
    (cherry picked from commit d4e20e942edf3f115804c4dade7a86d9994bce2e)

---
+++ src/components/clusters/CreateOSDPage/CreateOSDForm/FormSections/EncryptionSection/AWSEncryptionSection.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { GridItem } from '@patternfly/react-core';
+import { Field } from 'redux-form';
+import ReduxVerticalFormGroup from '../../../../../common/ReduxFormComponents/ReduxVerticalFormGroup';
+import ExternalLink from '../../../../../common/ExternalLink';
+import { validateAWSKMSKeyARN } from '../../../../../../common/validators';
+import { constants } from '../../CreateOSDFormConstants';
+
+interface Props {
+  region: string;
+}
+
+const AWSCustomerManagedEncryption = ({ region }: Props) => (
+  <>
+    <GridItem md={6}>
+      <Field
+        component={ReduxVerticalFormGroup}
+        name="kms_key_arn"
+        type="text"
+        label="Key ARN"
+        placeholder="Key ARN"
+        validate={(value: string) => validateAWSKMSKeyARN(value, region)}
+        isRequired
+        helpText="Unique, fully qualified identifier (Amazon Resource Name (ARN)) for your KMS Key."
+        extendedHelpText={
+          <>
+            <p className="pf-u-mb-sm">{constants.awsKeyARN}</p>
+            <ExternalLink href="https://docs.aws.amazon.com/kms/latest/developerguide/find-cmk-id-arn.html">
+              Finding the key ID and ARN
+            </ExternalLink>
+          </>
+        }
+        showHelpTextOnError={false}
+      />
+    </GridItem>
+    <GridItem md={6} />
+  </>
+);
+
+export default AWSCustomerManagedEncryption;
