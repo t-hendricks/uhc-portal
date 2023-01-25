8e01165a228ab51dc564d1940e8acc8971bdb69f
Jayakrishnan Mekkattillam <user@example.com>
2023-01-25 16:18:26 +0530

    Included cypress-grep plugin changes and related configuration changes.QE specific cases execution scripts (starting with smoke)and it's changes.

---
+++ cypress-qe-triggers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+# cypress-qe-triggers.sh helps to trigger test case execution from external Jenkins source.
+# This is starting point of the executions.
+
+node --version
+
+# Common parameters supplies to the script from jenkins job. 
+# Below parameters will be populated from Jenkins workarea.
+# NOTE : Enable inline ENVIRONMENT, BROWSER and TAGS variables and modify the values for debug runs from local setup.
+# ENVIRONMENT="qaprodauth.console.redhat.com"
+# BROWSER="chrome"
+# TAGS="smoke"
+echo "$ENVIRONMENT"
+echo "$BROWSER"
+echo "$TAGS"
+
+# Install all the required plugins/packages for QE execution enviroument.
+# Installation will be done on the dynamic nodes or your machine as per place of call of this file.
+yarn install
+
+# Calls the qe pod creation and runner script with parameterized values read from Jenkins/Executor. 
+timeout \
+  --signal "TERM" \
+  --kill-after "2m" \
+  "10m" \
+  "run/cypress-qe-executor.sh" $ENVIRONMENT $BROWSER $TAGS
