93a546399b0e36324a650fce91d7911d514ed866
Dave Taylor <user@example.com>
2023-02-09 20:04:57 +0000

    Merge branch 'HAC-3119-incorrect-machine-pool-status' into 'master'
    [HAC-3115] Show machine pool status as number of pools ready
    
    Closes HAC-3119
    
    See merge request service/uhc-portal!3904
    
    (cherry picked from commit c8109bb7d70e70186cbd542a434eb635a41601ab)

--- src/components/clusters/ClusterDetails/components/Overview/DetailsRight/ClusterStatus.tsx
+++ src/components/clusters/ClusterDetails/components/Overview/DetailsRight/ClusterStatus.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 
 import { Flex } from '@patternfly/react-core';
 
-import { Cluster } from '~/types/clusters_mgmt.v1';
+import { Cluster, NodePool } from '~/types/clusters_mgmt.v1';
 import ClusterStateIcon from '~/components/clusters/common/ClusterStateIcon/ClusterStateIcon';
 import clusterStates, { getStateDescription } from '~/components/clusters/common/clusterStates';
 import { isHypershiftCluster } from '~/components/clusters/ClusterDetails/clusterDetailsHelper';
+
+const numberReadyNodePools = (nodePools: NodePool[]) =>
+  nodePools?.filter((pool) => {
+    const current = pool.status?.current_replicas;
+
+    if (!current) {
+      return false;
+    }
+    if (pool.autoscaling) {
+      if (!pool.autoscaling.min_replica || !pool.autoscaling.max_replica) {
+        return false;
+      }
+      return current >= pool.autoscaling.min_replica && current <= pool.autoscaling.max_replica;
+    }
+
+    if (!pool.replicas) {
+      return false;
+    }
+    return pool.replicas === current;
+  }).length || 0;
 
 type ClusterWithStateDescription = Omit<Cluster, 'state'> & {
   state: { state: string; description: string };
@@ -14,29 +34,34 @@ type ClusterWithStateDescription = Omit<Cluster, 'state'> & {
 interface ClusterStatusProps {
   cluster: ClusterWithStateDescription;
   limitedSupport: boolean;
+  machinePools?: NodePool[];
 }
 
-export const ClusterStatus = ({ cluster, limitedSupport }: ClusterStatusProps) => {
+export const ClusterStatus = ({ cluster, limitedSupport, machinePools }: ClusterStatusProps) => {
   const isHypershift = isHypershiftCluster(cluster);
   const {
-    nodes,
-    status,
     state: { state: stateName, description: stateDescription },
   } = cluster;
-  const nodesCompute = nodes?.compute ?? 0;
-  const currentCompute = status?.current_compute ?? 0;
 
-  const machinePoolsState = React.useMemo(() => {
-    if (nodesCompute > currentCompute) {
-      return clusterStates.INSTALLING;
-    }
-
-    if (nodesCompute < currentCompute) {
+  const machinePoolsState = () => {
+    if (stateName === clusterStates.UNINSTALLING) {
       return clusterStates.UNINSTALLING;
     }
 
-    return clusterStates.READY;
-  }, [nodesCompute, currentCompute]);
+    if (machinePools && numberReadyNodePools(machinePools) === 0 && machinePools.length === 0) {
+      switch (stateName) {
+        case clusterStates.WAITING:
+          return clusterStates.WAITING;
+        case clusterStates.INSTALLING:
+          return clusterStates.PENDING;
+        default:
+          return clusterStates.DEPROVISIONED;
+      }
+    }
+    return machinePools && numberReadyNodePools(machinePools) === machinePools.length
+      ? clusterStates.READY
+      : clusterStates.PENDING;
+  };
 
   const clusterWideStateIcon = (
     <ClusterStateIcon clusterState={stateName} limitedSupport={limitedSupport} animated />
@@ -55,19 +80,24 @@ export const ClusterStatus = ({ cluster, limitedSupport }: ClusterStatusProps) =
           <div>{stateDescription}</div>
         </Flex>
 
-        <Flex
-          alignItems={{ default: 'alignItemsCenter' }}
-          spaceItems={{ default: 'spaceItemsXs' }}
-          data-testid="machine-pools-status"
-        >
-          <span className="pf-u-mr-sm">Machine pools:</span>
-          <ClusterStateIcon
-            clusterState={machinePoolsState}
-            limitedSupport={limitedSupport}
-            animated
-          />
-          <div>{getStateDescription(machinePoolsState)}</div>
-        </Flex>
+        {machinePools ? (
+          <Flex
+            alignItems={{ default: 'alignItemsCenter' }}
+            spaceItems={{ default: 'spaceItemsXs' }}
+            data-testid="machine-pools-status"
+          >
+            <span className="pf-u-mr-sm">Machine pools:</span>
+            <ClusterStateIcon
+              clusterState={machinePoolsState()}
+              limitedSupport={limitedSupport}
+              animated
+            />
+            <div>
+              {getStateDescription(machinePoolsState())} {numberReadyNodePools(machinePools)} /{' '}
+              {machinePools.length}
+            </div>
+          </Flex>
+        ) : null}
       </>
     );
   }
