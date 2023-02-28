5b206c28d9e6b50c0cf04c01c9b10cd5e00b0431
Dave Taylor <user@example.com>
2023-02-28 16:32:32 +0000

    Merge branch 'ocp-23060' into 'master'
    auto ocp-23060: Added Cypress tests to get offline token from tokens page
    
    See merge request service/uhc-portal!3834
    
    (cherry picked from commit b2c4d4241b1b2bf4c9ddea8770a24c19d8aa7d51)

--- cypress/pageobjects/Tokens.page.js
+++ cypress/pageobjects/Tokens.page.js
@@ -10,6 +10,26 @@ class TokenPages extends Page {
   navigateToROSAToken() {
     cy.visit('/token/rosa');
   }
+
+  tokenPageIsLoaded() {
+    cy.get('.pf-c-spinner').should('not.exist');
+    cy.get('h1').scrollIntoView().contains('OpenShift Cluster Manager API Token').should('be.visible');
+  }
+
+  //check load token part
+  checkLoadToken = (buttonLabel) => {
+    cy.contains('Connect with offline tokens');
+    cy.getByTestId('load-token-btn').click();
+    cy.getByTestId(`${buttonLabel}`).should('have.attr','href');
+    cy.get('input[aria-label="Copyable token"]').should('exist');
+  }
+
+  //check revoke previous tokens part
+  checkRevokePrevousToken() {
+    cy.contains('Revoke previous tokens');
+    cy.contains('SSO application management').should('have.attr','href');
+  }
+
 }
 
 export default new TokenPages();
