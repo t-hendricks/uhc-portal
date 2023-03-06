867a55ba36902ad4a23cd0a8dbcfa30e19bf0832
Celia Amador <user@example.com>
2023-03-06 08:56:48 +0100

    Fix lint error

--- src/common/versionFormatter.ts
+++ src/common/versionFormatter.ts
@@ -1,6 +1,4 @@
 // expected format to be "openshift-vA.B.C(-suffix)"
 export const versionRegEx = /^.*[v-]([0-9\\.]+).*$/;
 
-export const versionFormatter = (version: string): string => {
-  return version.replace(versionRegEx, '$1');
-};
+export const versionFormatter = (version: string): string => version.replace(versionRegEx, '$1');
