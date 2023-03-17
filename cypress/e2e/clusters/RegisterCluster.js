b2ea79f219b7e9f3ea493f5f3c1dbae1fcfe378b
Dylan Cooper <user@example.com>
2023-03-17 13:10:52 -0700

    Deleted RegisterCluster selenium test and RegisterCluster page objects. Added RegisterCluster cypress test.

---
+++ cypress/e2e/clusters/RegisterCluster.js
@@ -0,0 +1,121 @@
+import { v4 } from 'uuid';
+import Login from '../../pageobjects/login.page';
+import ClusterListPage from '../../pageobjects/ClusterList.page';
+import RegisterClusterPage from '../../pageobjects/RegisterCluster.page';
+import ClusterDetailsPage from '../../pageobjects/ClusterDetails.page';
+
+describe('Register cluster flow', () => {
+  let displayName;
+
+  before(() => {
+    cy.visit('/');
+    Login.isLoginPageUrl();
+    Login.login();
+
+    ClusterListPage.isClusterListUrl();
+    ClusterListPage.waitForDataReady();
+    cy.getByTestId('create_cluster_btn').should('be.visible');
+  });
+
+  it('navigate to register cluster', () => {
+    ClusterListPage.registerCluster().should('be.visible').click();
+    ClusterListPage.isRegisterClusterUrl();
+    ClusterListPage.isRegisterClusterScreen();
+  });
+
+  it('shows an error when cluster ID is not valid', () => {
+    RegisterClusterPage.clusterIDInput().type('not really a uuid').blur();
+    RegisterClusterPage.clusterIDError().should('be.visible');
+    RegisterClusterPage.clusterIDError().should(
+      'have.text',
+      " Cluster ID 'not really a uuid' is not a valid UUID.",
+    );
+    RegisterClusterPage.clusterIDInput().clear();
+    RegisterClusterPage.clusterIDError().should('be.visible');
+    RegisterClusterPage.clusterIDError().should('have.text', ' Cluster ID is required.');
+  });
+
+  it('shows error when display name is not valid', () => {
+    RegisterClusterPage.displayNameInput().type('a'.repeat(70)).blur();
+    RegisterClusterPage.displayNameError().should('be.visible');
+    RegisterClusterPage.displayNameError().should(
+      'have.text',
+      ' Cluster display name may not exceed 63 characters.',
+    );
+  });
+
+  it('shows error when URL is not valid', () => {
+    RegisterClusterPage.clusterURLInput().type('asdf').blur();
+    RegisterClusterPage.clusterURLError().should('be.visible');
+    RegisterClusterPage.clusterURLError().should(
+      'have.text',
+      ' The URL should include the scheme prefix (http://, https://)',
+    );
+    RegisterClusterPage.clusterURLInput().clear();
+    RegisterClusterPage.clusterURLInput().type('https://uwu').blur();
+    RegisterClusterPage.clusterURLError().should('be.visible');
+    RegisterClusterPage.clusterURLError().should('have.text', ' Invalid URL');
+  });
+
+  it('redirects to cluster list when clicking cancel', () => {
+    RegisterClusterPage.cancelButton().click();
+    ClusterListPage.isClusterListScreen();
+  });
+
+  it('creates a new cluster and redirects to its details page', () => {
+    const clusterID = v4();
+    displayName = `cypress-${clusterID}`;
+
+    ClusterListPage.registerCluster().should('be.visible').click();
+    ClusterListPage.isRegisterClusterUrl();
+    RegisterClusterPage.clusterIDInput().type(clusterID).blur();
+    RegisterClusterPage.clusterIDError().should('not.exist');
+    RegisterClusterPage.displayNameInput().type(displayName).blur();
+    RegisterClusterPage.displayNameError().should('not.exist');
+    RegisterClusterPage.submitButton().click();
+    ClusterDetailsPage.isClusterDetailsPage(displayName);
+  });
+
+  it('successfully changes the console URL for the cluster', () => {
+    const url = 'http://example.com';
+    ClusterDetailsPage.addConsoleURLButton().click();
+    ClusterDetailsPage.editConsoleURLDialogInput().type(url);
+    ClusterDetailsPage.editConsoleURLDialogConfirm().click();
+    ClusterDetailsPage.editConsoleURLDialogConfirm().should('not.exist');
+    ClusterDetailsPage.openConsoleLink()
+      .should('have.attr', 'href', url)
+      .find('button')
+      .should('have.text', 'Open console');
+  });
+
+  it('successfully changes display name', () => {
+    ClusterDetailsPage.actionsDropdownToggle().click();
+    ClusterDetailsPage.editDisplayNameDropdownItem().click();
+    ClusterDetailsPage.editDisplayNameInput().should('exist');
+    ClusterDetailsPage.editDisplayNameInput().clear();
+    ClusterDetailsPage.editDisplayNameInput().type(`${displayName}-test`).blur();
+    ClusterDetailsPage.editDisplaynameConfirm().click({ waitForAnimations: true });
+    ClusterDetailsPage.clusterNameTitle().should('have.text', `${displayName}-test`);
+  });
+
+  it('successfully archives the newly created cluster', () => {
+    ClusterDetailsPage.actionsDropdownToggle().click();
+    ClusterDetailsPage.archiveClusterDropdownItem().click();
+    ClusterDetailsPage.archiveClusterDialogConfirm().click();
+    ClusterDetailsPage.successNotification().should('exist');
+    ClusterDetailsPage.unarchiveClusterButton().should('exist');
+  });
+
+  it('successfully unarchives the archived cluster', () => {
+    ClusterDetailsPage.unarchiveClusterButton().click();
+    ClusterDetailsPage.unarchiveClusterDialogConfirm().click();
+    ClusterDetailsPage.successNotification().should('exist');
+    ClusterDetailsPage.actionsDropdownToggle().should('exist');
+  });
+
+  it('Finally, archive the cluster created', () => {
+    ClusterDetailsPage.actionsDropdownToggle(true).click();
+    ClusterDetailsPage.archiveClusterDropdownItem().click();
+    ClusterDetailsPage.archiveClusterDialogConfirm().click();
+  });
+});
---
+++ cypress/pageobjects/ClusterDetails.page.js
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import Page from './page';
+
+class ClusterDetails extends Page {
+  isClusterDetailsPage = (displayName) => cy.contains('h1', displayName);
+
+  addConsoleURLButton = () => cy.get('button').contains('Add console URL');
+
+  editConsoleURLDialogInput = () => cy.get('input[id="edit-console-url-input"]');
+
+  editConsoleURLDialogConfirm = () => cy.get('button').contains('Add URL');
+
+  openConsoleLink = () => cy.get('.pull-left');
+
+  actionsDropdownToggle = (delayed) =>
+    delayed
+      ? cy
+          .get('div[data-testid="cluster-actions-dropdown"]', { timeout: 3000 })
+          .should('exist')
+          .find('button')
+          .first()
+      : cy.get('div[data-testid="cluster-actions-dropdown"]').find('button').first();
+
+  editDisplayNameDropdownItem = () => cy.contains('button', 'Edit display name');
+
+  editDisplayNameInput = () => cy.get('input[id="edit-display-name-input"]');
+
+  editDisplaynameConfirm = () =>
+    cy.get('div[aria-label="Edit display name"]').find('footer').find('button').first();
+
+  archiveClusterDropdownItem = () => cy.contains('button', 'Archive cluster');
+
+  archiveClusterDialogConfirm = () =>
+    cy.get('div[aria-label="Archive cluster"]').find('footer').find('button').first();
+
+  successNotification = () => cy.get('div.pf-c-alert.pf-m-success.notification-item');
+
+  unarchiveClusterButton = () =>
+    cy.get('span[id="cl-details-btns"]').contains('button', 'Unarchive');
+
+  unarchiveClusterDialogConfirm = () => cy.contains('button', 'Unarchive cluster');
+
+  clusterNameTitle = () => cy.get('h1.cl-details-page-title');
+}
+
+ClusterDetails.propTypes = {
+  displayName: PropTypes.string.isRequired,
+  delayed: PropTypes.bool,
+};
+
+export default new ClusterDetails();
