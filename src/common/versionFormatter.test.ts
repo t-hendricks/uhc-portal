f89ee2ff8f857c9845db5b3d3095263e173f2633
Celia Amador <user@example.com>
2023-03-28 08:58:21 +0100

    HAC-3536 Prerelease not working with version prefixes

--- src/common/versionFormatter.test.ts
+++ src/common/versionFormatter.test.ts
@@ -5,13 +5,18 @@ describe('versionFormatter', () => {
     const versions = [
       { value: 'v4.12.0-0', result: '4.12.0' },
       { value: 'v4.11.0-candidate', result: '4.11.0-candidate' },
+      { value: 'openshift-v4.10.0-candidate', result: '4.10.0-candidate' },
       { value: 'v2.17.33-7.nightly-2023-03-15-163541-nightly.3', result: '2.17.33-nightly' },
-      { value: 'v2.17.33-7.nightly-2023-03-15-163541-latest.3', result: '2.17.33-nightly' },
-      { value: 'v4.12.0-0.nightly-2023-03-15-163541-nightly', result: '4.12.0-nightly' },
-      { value: 'v4.12.0-0.nightly-2023-03-15-163541-latest', result: '4.12.0-nightly' },
-      { value: 'v4.12.0-0', result: '4.12.0' },
-      { value: 'v4.12.0', result: '4.12.0' },
-      { value: '4.12.0', result: '4.12.0' },
+      { value: 'v2.16.33-7.nightly-2023-03-15-163541-latest.3', result: '2.16.33-nightly' },
+      {
+        value: 'openshift-v2.15.33-7.nightly-2023-03-15-163541-latest.3',
+        result: '2.15.33-nightly',
+      },
+      { value: 'v4.10.0-0.nightly-2023-03-15-163541-nightly', result: '4.10.0-nightly' },
+      { value: 'v4.9.0-0.nightly-2023-03-15-163541-latest', result: '4.9.0-nightly' },
+      { value: 'v4.8.0-0', result: '4.8.0' },
+      { value: 'v4.7.0', result: '4.7.0' },
+      { value: '4.6.0', result: '4.6.0' },
       { value: 'hello', result: 'hello' },
     ];
     versions.forEach((version) => {
