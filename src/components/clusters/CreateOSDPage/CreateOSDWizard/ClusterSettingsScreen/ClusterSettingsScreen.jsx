3b93f9ee725508711de32cdd1a9bdfc36a21c0e5
Celia Amador Gonzalez <user@example.com>
2023-04-12 08:19:46 +0000

    Merge branch 'hac-2847-bug-v1v2' into 'master'
    [HAC-2847]: Expand advanced encryption section when the field validation error throws under it. (phase 3)
    
    Closes HAC-2847
    
    See merge request service/uhc-portal!4068
    
    (cherry picked from commit e9c7cf489751d4183b07e000cd06cd6e324a764a)

--- src/components/clusters/CreateOSDPage/CreateOSDWizard/ClusterSettingsScreen/ClusterSettingsScreen.jsx
+++ src/components/clusters/CreateOSDPage/CreateOSDWizard/ClusterSettingsScreen/ClusterSettingsScreen.jsx
@@ -32,6 +32,7 @@ function ClusterSettingsScreen({
   formErrors,
   touch,
   isHypershiftSelected,
+  isNextClicked,
 }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
@@ -65,7 +66,14 @@ function ClusterSettingsScreen({
     if (isAdvancedEncryptionExpanded) {
       setIsExpanded(true);
     }
-  }, [customerManagedEncryptionSelected, isGCP, gcpError, kmsKeyArn, selectedRegion]);
+  }, [
+    customerManagedEncryptionSelected,
+    isGCP,
+    gcpError,
+    kmsKeyArn,
+    selectedRegion,
+    isNextClicked,
+  ]);
 
   return (
     <Form
@@ -191,6 +199,7 @@ ClusterSettingsScreen.propTypes = {
   formErrors: PropTypes.object,
   touch: PropTypes.func,
   isHypershiftSelected: PropTypes.bool,
+  isNextClicked: PropTypes.bool,
 };
 
 export default ClusterSettingsScreen;
