fd3ce189efbd75d3f21ad58f9412bd279a9ffb33
Beni Cherniavsky-Paskin <user@example.com>
2023-04-20 14:45:30 +0300

    refactor: useAWSVPCInquiry: reuse `isVPCInquiryValid` selector
    Besides code reuse, there were minor benefits:
    - checks `vpcs.fulfilled`.
      Otherwise, a previous error result with matching metadata could
      inhibit making a new request.
    - `isEqual(vpcs.credentials, credentials)` also compares `account_id`.
      (backend actually seems to ignore account_id in these requests,
      but as long as we send it we must assume it matters.)
    - uses `meta.region` from the action instead of inferring from AZ names.

--- src/components/clusters/CreateOSDPage/CreateOSDWizard/VPCScreen/useVPCInquiry.jsx
+++ src/components/clusters/CreateOSDPage/CreateOSDWizard/VPCScreen/useVPCInquiry.jsx
@@ -38,18 +38,9 @@ export const useAWSVPCInquiry = () => {
   );
   const region = useSelector((state) => valueSelector(state, 'region'));
   const vpcs = useSelector((state) => state.ccsInquiries.vpcs);
-  const { credentials: vpcsCredentials, error: vpcsError } = vpcs;
+  const { error: vpcsError } = vpcs;
 
-  // When vpcs subnet availability zones possess the current selected region and current
-  // CCS credentials match what's stored for vpcs, vpcs results can be considered up-to-date.
-  const vpcsHasLatestCredentials =
-    vpcsCredentials?.sts?.role_arn === credentials?.sts?.role_arn ||
-    (vpcsCredentials?.access_key_id === credentials?.access_key_id &&
-      vpcsCredentials?.secret_access_key === credentials?.secret_access_key);
-  const hasLatestVpcs =
-    Object.values(vpcs.data?.bySubnetID || {})?.some((subnet) =>
-      subnet.availability_zone.includes(region),
-    ) && vpcsHasLatestCredentials;
+  const hasLatestVpcs = useSelector(isVPCInquiryValid);
 
   useEffect(() => {
     // The action works similarly for AWS and GCP,
@@ -62,7 +53,7 @@ export const useAWSVPCInquiry = () => {
 
       dispatch(getAWSCloudProviderVPCs(credentials, region));
     }
-  }, [cloudProviderID, credentials, region, hasLatestVpcs]);
+  }, [cloudProviderID, credentials, region, hasLatestVpcs, vpcsError]);
 
   return vpcs;
 };
