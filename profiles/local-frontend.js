269008c0bdee21c4b511e81b96b2e5b869cb63cc
Eliran Malka <user@example.com>
2023-05-02 20:26:52 +0000

    Merge branch 'preview-dev-server' into 'master'
    [HAC-3206] dev server: support /preview
    
    See merge request service/uhc-portal!4127
    
    (cherry picked from commit 7907bc4a1be90dd0b688132b689d347d1ee0d39f)

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -5,6 +5,7 @@ const API_PORT = 8010;
 const routes = {};
 
 routes[`/beta/${APP_ID}`] = { host: 'http://localhost:8001' };
+routes[`/preview/${APP_ID}`] = { host: 'http://localhost:8001' };
 routes[`/${APP_ID}`] = { host: 'http://localhost:8001' };
 routes[`/beta/apps/${APP_ID}`] = { host: 'http://localhost:8001' };
 routes[`/apps/${APP_ID}`] = { host: 'http://localhost:8001' };
