eee8eda4a0e4ed52ed82d473aa581ec1a3c7e891
Dave Taylor <user@example.com>
2023-05-09 18:27:11 +0000

    Merge branch 'HAC-3790-hypershift-idp-section' into 'master'
    HAC-3790 Allow IDP section earlier for Hypershift clusters
    
    Closes HAC-3790
    
    See merge request service/uhc-portal!4128

--- src/components/clusters/ClusterDetails/components/AccessControl/AccessControl.test.jsx
+++ src/components/clusters/ClusterDetails/components/AccessControl/AccessControl.test.jsx
@@ -1,20 +1,97 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testUtils';
+
+import { normalizedProducts } from '~/common/subscriptionTypes';
 
 import AccessControl from './AccessControl';
 
+const buildCluster = ({ clusterProps, subscriptionProps, consoleUrl, apiUrl }) => ({
+  canEdit: true,
+  id: 'clusterId',
+  cloud_provider: { id: 'azure' },
+  subscription: {
+    id: 'subscriptionId',
+    ...subscriptionProps,
+  },
+  managed: true,
+  state: 'ready',
+  console: {
+    url:
+      consoleUrl === undefined ? 'https://console-openshift-console.apps.example.com' : consoleUrl,
+  },
+  api: { url: apiUrl === undefined ? 'https://api.test-liza.wiex.s1.devshift.org:6443' : apiUrl },
+  ...clusterProps,
+});
+
 describe('<AccessControl />', () => {
-  it('should render', () => {
-    const wrapper = shallow(
-      <AccessControl
-        cluster={{ canEdit: true, id: 'fake id' }}
-        clusterUrls={{
-          console: 'https://console-openshift-console.apps.example.com',
-          api: 'https://api.test-liza.wiex.s1.devshift.org:6443',
-        }}
-        cloudProvider="aws"
-      />,
-    );
-    expect(wrapper).toMatchSnapshot();
+  const buildComponent = (cluster) => (
+    <MemoryRouter>
+      <AccessControl cluster={cluster || buildCluster({})} />
+    </MemoryRouter>
+  );
+
+  describe('Tab grouping', () => {
+    it('has "single-tab" class if only one section is shown', () => {
+      const singleTabCluster = buildCluster({
+        clusterProps: {
+          managed: false,
+        },
+      });
+      render(buildComponent(singleTabCluster));
+
+      const cardBody = document.querySelector('.pf-c-card__body');
+      expect(cardBody.classList).toContain('single-tab');
+      expect(screen.getAllByRole('tab')).toHaveLength(1);
+    });
+
+    it('does not have "single-tab" class if multiple tabs are shown', () => {
+      const multipleTabCluster = buildCluster({
+        clusterProps: {
+          managed: true,
+        },
+      });
+      render(buildComponent(multipleTabCluster));
+
+      const cardBody = document.querySelector('.pf-c-card__body');
+      expect(cardBody.classList).not.toContain('single-tab');
+      expect(screen.getAllByRole('tab')).toHaveLength(3);
+    });
+  });
+
+  describe('Identity Provider section', () => {
+    it('is not shown for OSD/ROSA clusters before they have console URL', () => {
+      const osdCluster = buildCluster({
+        consoleUrl: '',
+      });
+      render(buildComponent(osdCluster));
+
+      expect(screen.queryByRole('tab', { name: 'Identity providers' })).not.toBeInTheDocument();
+    });
+
+    it('is shown for OSD/ROSA clusters after they have console URL', () => {
+      render(buildComponent());
+
+      expect(
+        screen.getByRole('tab', { name: 'Identity providers', selected: true }),
+      ).toBeInTheDocument();
+    });
+
+    it('is shown for Hypershift clusters even before they have console URL', () => {
+      const hypershiftCluster = buildCluster({
+        clusterProps: {
+          hypershift: { enabled: true },
+        },
+        subscriptionProps: {
+          plan: { id: normalizedProducts.ROSA_HyperShift },
+        },
+        consoleUrl: '',
+      });
+
+      render(buildComponent(hypershiftCluster));
+      expect(
+        screen.getByRole('tab', { name: 'Identity providers', selected: true }),
+      ).toBeInTheDocument();
+    });
   });
 });
