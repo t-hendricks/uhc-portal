4bd3067a3d5d4c84b23146dc0277a2a80b21de35
Montse Ortega <user@example.com>
2023-05-11 17:56:03 +0200

    Adjusting snapshots for DatacenterTab

--- src/components/clusters/CreateClusterPage/__tests__/DatacenterTab.test.jsx
+++ src/components/clusters/CreateClusterPage/__tests__/DatacenterTab.test.jsx
@@ -4,12 +4,20 @@ import { shallow } from 'enzyme';
 import DatacenterTab from '../DatacenterTab';
 
 describe('<DatacenterTab />', () => {
-  it('renders correctly with assisted installer', () => {
-    const wrapper = shallow(<DatacenterTab assistedInstallerFeature />);
+  it('renders correctly with assisted installer and multiarch disabled', () => {
+    const wrapper = shallow(
+      <DatacenterTab assistedInstallerFeature multiArchFeatureEnabled={false} />,
+    );
     expect(wrapper).toMatchSnapshot();
   });
-  it('renders correctly without assisted installer', () => {
-    const wrapper = shallow(<DatacenterTab assistedInstallerFeature={false} />);
+  it('renders correctly with assisted installer and multiarch enabled', () => {
+    const wrapper = shallow(<DatacenterTab assistedInstallerFeature multiArchFeatureEnabled />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('renders correctly without assisted installer and without multiarch', () => {
+    const wrapper = shallow(
+      <DatacenterTab assistedInstallerFeature={false} multiArchFeatureEnabled={false} />,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
