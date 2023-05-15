8234d668177ddb9eab054294d7e48fb52ad4ff7a
Dylan Cooper <user@example.com>
2023-05-14 19:29:27 -0700

    port OCMRolesAndAccess test to cypress. Deleted selenium OCMRolesAndAccess test

---
+++ cypress/pageobjects/OCMRolesAndAccess.page.js
@@ -0,0 +1,31 @@
+import Page from './page';
+
+class OCMRolesAndAccess extends Page {
+  accessControlTabButton = () => cy.contains('button', 'Access control');
+
+  grantRoleButton = () => cy.contains('button', 'Grant role');
+
+  OCMRolesAndAccessTable = () => cy.get('table[aria-label="OCM Roles and Access"]');
+
+  OCMRolesAndAccessTableActionButton = () =>
+    cy.get('td.pf-c-table__action > div.pf-c-dropdown > button[aria-label="Actions"]');
+
+  OCMRolesAndAccessTableDeleteButton = () =>
+    cy.get(
+      'td.pf-c-table__action > div.pf-c-dropdown > ul.pf-c-dropdown__menu > li > button.hand-pointer',
+    );
+
+  grantRoleUserInput = () => cy.get('input[id="username"]');
+
+  userInputError = () => cy.get('div[id="username-helper"]');
+
+  submitButton = () => cy.get('button[type="submit"]').should('have.text', 'Grant role');
+
+  usernameCell = () => cy.get('td[data-label="Username"] > span');
+
+  waitForGrantRoleModalToClear = () => {
+    cy.get('div[id="ocm-roles-access-dialog"]', { timeout: 30000 }).should('not.exist');
+  };
+}
+
+export default new OCMRolesAndAccess();
