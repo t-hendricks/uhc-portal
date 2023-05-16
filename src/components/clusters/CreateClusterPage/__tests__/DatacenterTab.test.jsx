e897f8ee743b07dadc4a41d7375eb75a1b990836
Montse Ortega <user@example.com>
2023-05-16 13:04:03 +0200

    Adjusting unit tests

--- src/components/clusters/CreateClusterPage/__tests__/DatacenterTab.test.jsx
+++ src/components/clusters/CreateClusterPage/__tests__/DatacenterTab.test.jsx
@@ -3,28 +3,33 @@ import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import { store } from '../../../../redux/store';
 import DatacenterTab from '../DatacenterTab';
+import { setFeatureAction } from '../../../../redux/actions/featureActions';
+import { ASSISTED_INSTALLER_MULTIARCH_SUPPORTED } from '../../../../redux/constants/featureConstants';
 
 describe('<DatacenterTab />', () => {
   it('renders correctly with assisted installer and multiarch disabled', () => {
+    store.dispatch(setFeatureAction(ASSISTED_INSTALLER_MULTIARCH_SUPPORTED, false));
     const wrapper = shallow(
       <Provider store={store}>
-        <DatacenterTab assistedInstallerFeature multiArchFeatureEnabled={false} />
+        <DatacenterTab assistedInstallerFeature />
       </Provider>,
     );
     expect(wrapper).toMatchSnapshot();
   });
   it('renders correctly with assisted installer and multiarch enabled', () => {
+    store.dispatch(setFeatureAction(ASSISTED_INSTALLER_MULTIARCH_SUPPORTED, true));
     const wrapper = shallow(
       <Provider store={store}>
-        <DatacenterTab assistedInstallerFeature multiArchFeatureEnabled />
+        <DatacenterTab assistedInstallerFeature />
       </Provider>,
     );
     expect(wrapper).toMatchSnapshot();
   });
   it('renders correctly without assisted installer and without multiarch', () => {
+    store.dispatch(setFeatureAction(ASSISTED_INSTALLER_MULTIARCH_SUPPORTED, false));
     const wrapper = shallow(
       <Provider store={store}>
-        <DatacenterTab assistedInstallerFeature={false} multiArchFeatureEnabled={false} />
+        <DatacenterTab assistedInstallerFeature={false} />
       </Provider>,
     );
     expect(wrapper).toMatchSnapshot();
