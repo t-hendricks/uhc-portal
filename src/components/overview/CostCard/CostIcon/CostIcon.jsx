98665a1fa2714dbd55395dd533e183ffe828b7c3
eliran <user@example.com>
2023-05-23 22:27:57 +0300

    fix(overview): broken empty-state image on cost-management card
    fix broken import due to a misuse of disparate import system (i.e. require)

--- src/components/overview/CostCard/CostIcon/CostIcon.jsx
+++ src/components/overview/CostCard/CostIcon/CostIcon.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const icon = require('./CostIcon.svg');
+import CostIconImg from './CostIcon.svg';
 
 const CostIcon = ({ className }) => (
   <img
     className={`ocm--cost-icon ${className}`}
-    src={icon.default}
+    src={CostIconImg}
     alt="Cost Management"
     aria-hidden="true"
   />
