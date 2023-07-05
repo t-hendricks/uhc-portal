a9a67ff4903e846ced019f74fc202d8ff17c0d43
Celia Amador Gonzalez <user@example.com>
2023-07-05 17:09:53 +0000

    Merge branch 'change-base-api-url' into 'master'
    Temp fix to change API url for restricted environment
    
    See merge request service/uhc-portal!4294
    
    (cherry picked from commit 93260b6fad461ccc00b4068e952bdc0f17773df9)

--- src/services/apiRequest.ts
+++ src/services/apiRequest.ts
@@ -1,15 +1,22 @@
 import axios from 'axios';
 import type { AxiosRequestConfig, AxiosInstance } from 'axios';
 import * as Sentry from '@sentry/browser';
+import { isRestrictedEnv, restrictedEnvApi } from '~/restrictedEnv';
 
 import config from '../config';
+
+const getBaseUrl = (baseUrl: string | undefined) => {
+  if (isRestrictedEnv) {
+    return restrictedEnvApi;
+  }
+  return baseUrl || (config.configData.apiGateway ? config.configData.apiGateway : '');
+};
 
 export const authInterceptor = (client: AxiosInstance): AxiosInstance => {
   client.interceptors.request.use(async (cfg) => {
     await insights.chrome.auth.getUser();
     const token = await insights.chrome.auth.getToken();
-    const BASE_URL =
-      cfg.baseURL || (config.configData.apiGateway ? config.configData.apiGateway : '');
+    const BASE_URL = getBaseUrl(cfg.baseURL);
     const updatedCfg: AxiosRequestConfig = { ...cfg, url: `${BASE_URL}${cfg.url}` };
     if (token) {
       updatedCfg.headers = {
