90ce6fd9a97ea72b71ddf02fb94a5ef18ca6d8bf
Joachim Schuler <user@example.com>
2023-07-17 19:08:35 +0000

    Merge branch 'hac-3879' into 'master'
    HAC-3879: Reset getMachinePools data after closing the Edit node count modal
    
    Closes HAC-3879
    
    See merge request service/uhc-portal!4193
    
    (cherry picked from commit 463311116e735fe50e2b9b46bf2940c1048c4b07)

--- src/components/clusters/common/ClusterActionsDropdown/ClusterActionsDropdownItems.jsx
+++ src/components/clusters/common/ClusterActionsDropdown/ClusterActionsDropdownItems.jsx
@@ -152,6 +152,7 @@ function actionResolver(
             isDefaultMachinePool: !isHypershiftCluster(cluster),
             shouldDisplayClusterName: inClusterList,
             isHypershiftCluster: isHypershiftCluster(cluster),
+            clearMachinePoolDataOnExit: isHypershiftCluster(cluster),
           }),
       },
     ),
