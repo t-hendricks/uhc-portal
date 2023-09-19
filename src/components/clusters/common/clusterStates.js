dbddedbabfe8209af1ce11e13b2bcf6c4fc455e1
John Swanke <user@example.com>
2023-09-19 13:00:59 -0400

    add osd trial
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- src/components/clusters/common/clusterStates.js
+++ src/components/clusters/common/clusterStates.js
@@ -97,7 +97,8 @@ const isHibernating = (state) =>
 const isROSA = (cluster) => cluster.product.id === normalizedProducts.ROSA;
 
 // Indicates that this is an OSD cluster
-const isOSD = (cluster) => cluster.product.id === normalizedProducts.OSD;
+const isOSD = (cluster) =>
+  cluster.product.id === normalizedProducts.OSD || normalizedProducts.OSDTrial;
 
 // Indicates that this is a ROSA cluster with manual mode
 const isROSAManualMode = (cluster) =>
