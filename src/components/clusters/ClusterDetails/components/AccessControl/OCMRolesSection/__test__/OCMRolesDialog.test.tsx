cd20d24100e9643ee774b54c53f12d6172d55b23
Celia Amador Gonzalez <user@example.com>
2023-09-25 14:50:23 +0000

    Merge branch 'HAC-4961-hide-cluster-autoscaler-role-hcp' into 'master'
    HAC-4961 Hide cluster autoscaler role for Hypershift
    
    Closes HAC-4961
    
    See merge request service/uhc-portal!4555
    
    (cherry picked from commit 120c0560a05e7cb33a1f4904f820fa342f558216)

--- src/components/clusters/ClusterDetails/components/AccessControl/OCMRolesSection/__test__/OCMRolesDialog.test.tsx
+++ src/components/clusters/ClusterDetails/components/AccessControl/OCMRolesSection/__test__/OCMRolesDialog.test.tsx
@@ -1,40 +1,95 @@
 import React from 'react';
 import { checkAccessibility, render } from '~/testUtils';
 import modals from '~/components/common/Modal/modals';
-import { ocmRoles } from '~/common/subscriptionTypes';
+import { knownProducts, ocmRoles } from '~/common/subscriptionTypes';
 import { row } from './OCMRolesDialog.fixture';
+
 import { OCMRolesDialog } from '../OCMRolesDialog';
 import { OCMRolesRow } from '../OCMRolesRow';
 
+const newCreationRolesRow = () => new OCMRolesRow(null, '0');
+const newEditionRolesRow = () => new OCMRolesRow(row.data, '1');
+
 describe('<OCMRolesDialog />', () => {
-  it('should render in create mode', async () => {
-    const rolesRow = new OCMRolesRow(null, '0');
-    const submit = jest.fn();
-    const { container, user, getByRole } = render(
-      <OCMRolesDialog onSubmit={submit} row={rolesRow} />,
+  const renderComponent = ({
+    submit,
+    rolesRow,
+    productId,
+  }: {
+    rolesRow: OCMRolesRow;
+    submit?: () => void;
+    productId?: string;
+  }) =>
+    render(
+      <OCMRolesDialog onSubmit={submit || jest.fn()} row={rolesRow} productId={productId} />,
       {},
       { modal: { modalName: modals.OCM_ROLES } },
     );
-    expect(getByRole('heading')).toHaveTextContent('Grant role');
-    expect(getByRole('textbox', { name: /username/i })).toHaveTextContent('');
-    await user.type(getByRole('textbox', { name: /username/i }), 'user@example.com');
-    await user.click(getByRole('button', { name: /grant role/i }));
-    expect(submit).toHaveBeenCalledWith(rolesRow, 'user@example.com', ocmRoles.CLUSTER_EDITOR.id);
-    checkAccessibility(container);
+
+  describe('Creation mode', () => {
+    it('should render correctly', async () => {
+      const submit = jest.fn();
+      const creationRolesRow = newCreationRolesRow();
+      const { container, user, getByRole } = renderComponent({
+        submit,
+        rolesRow: creationRolesRow,
+      });
+      expect(getByRole('heading')).toHaveTextContent('Grant role');
+      expect(getByRole('textbox', { name: /username/i })).toHaveTextContent('');
+      await user.type(getByRole('textbox', { name: /username/i }), 'user@example.com');
+      await user.click(getByRole('button', { name: /grant role/i }));
+      expect(submit).toHaveBeenCalledWith(
+        creationRolesRow,
+        'user@example.com',
+        ocmRoles.CLUSTER_EDITOR.id,
+      );
+      await checkAccessibility(container);
+    });
+
+    it("should show all the roles that are not excluded for the cluster's product", async () => {
+      const { user, getByRole, getByText } = renderComponent({
+        rolesRow: newCreationRolesRow(),
+        productId: 'product-that-has-no-role-exclusions',
+      });
+
+      // Expand the menu to display all the roles
+      await user.click(getByRole('button', { expanded: false }));
+
+      expect(getByText(/^machine pool editor$/i)).toBeInTheDocument();
+      expect(getByText(/^cluster autoscaler editor$/i)).toBeInTheDocument();
+    });
+
+    it("should hide the roles that are excluded for the cluster's product", async () => {
+      const { user, getByRole, getByText, queryByText } = renderComponent({
+        rolesRow: newCreationRolesRow(),
+        productId: knownProducts.ROSA_HyperShift, // Does not allow cluster autoscaler role
+      });
+      // Expand the menu to display all the roles
+      await user.click(getByRole('button', { expanded: false }));
+
+      expect(getByText(/^machine pool editor$/i)).toBeInTheDocument();
+      expect(queryByText(/^cluster autoscaler editor$/i)).not.toBeInTheDocument();
+    });
   });
-  it('should render in edit mode', async () => {
-    const rolesRow = new OCMRolesRow(row.data, '1');
-    const submit = jest.fn();
-    const { user, getByText, getByRole } = render(
-      <OCMRolesDialog onSubmit={submit} row={rolesRow} />,
-      {},
-      { modal: { modalName: modals.OCM_ROLES } },
-    );
-    expect(getByRole('heading')).toHaveTextContent('Edit role');
-    expect(getByRole('textbox', { name: /username/i })).toHaveValue('Doris Hudson');
-    await user.click(getByText(/cluster viewer/i));
-    await user.click(getByText(/^machine pool editor$/i));
-    await user.click(getByRole('button', { name: /edit role/i }));
-    expect(submit).toHaveBeenCalledWith(rolesRow, 'Doris Hudson', ocmRoles.MACHINE_POOL_EDITOR.id);
+
+  describe('Edition mode', () => {
+    it('should render correctly', async () => {
+      const rolesRow = newEditionRolesRow();
+      const submit = jest.fn();
+      const { user, getByText, getByRole } = renderComponent({
+        submit,
+        rolesRow,
+      });
+      expect(getByRole('heading')).toHaveTextContent('Edit role');
+      expect(getByRole('textbox', { name: /username/i })).toHaveValue('Doris Hudson');
+      await user.click(getByText(/cluster viewer/i));
+      await user.click(getByText(/^machine pool editor$/i));
+      await user.click(getByRole('button', { name: /edit role/i }));
+      expect(submit).toHaveBeenCalledWith(
+        rolesRow,
+        'Doris Hudson',
+        ocmRoles.MACHINE_POOL_EDITOR.id,
+      );
+    });
   });
 });
