15faee40662edb6770593e03cb081405b33198f8
Roberto Emanuel <user@example.com>
2023-09-28 09:05:23 +0000

    Merge branch 'HAC-4916' into 'master'
    HAC-4916 [Hypershfit ROSA][Wizard] Add installer role arn to create OIDC operator role command
    
    Closes HAC-4916
    
    See merge request service/uhc-portal!4567
    
    (cherry picked from commit bf9bf2c8dc601f9fc518b13a0455412bb3eecdec)

---
+++ src/components/clusters/CreateROSAPage/CreateROSAWizard/ClusterRolesScreen/clusterRolesHelper.test.ts
@@ -0,0 +1,59 @@
+import { getOperatorRolesCommand } from './clusterRolesHelper';
+
+const defaultOptions = {
+  forcedByoOidcType: 'Hypershift' as const,
+  byoOidcConfigID: '22phvja6kkki8f7h8mmk46a7j2h26uk1',
+  customOperatorRolesPrefix: 'operator-prefix',
+  installerRoleArn: 'arn:aws:iam::000000000006',
+  sharedVpcRoleArn: 'arn:aws:iam::444234423423',
+};
+
+describe('getOperatorRolesCommand', () => {
+  test('should return the proper operator roles command for hypershift clusters', () => {
+    const options = { ...defaultOptions };
+    const command = getOperatorRolesCommand(options);
+
+    expect(command).toBe(
+      `rosa create operator-roles --prefix "${options.customOperatorRolesPrefix}" --oidc-config-id "${options.byoOidcConfigID}" --hosted-cp --installer-role-arn ${options.installerRoleArn}`,
+    );
+  });
+
+  test('should return the proper operator roles command for classic clusters with shared vpc', () => {
+    const options = { ...defaultOptions, forcedByoOidcType: 'SharedVPC' as const };
+    const command = getOperatorRolesCommand(options);
+
+    expect(command).toBe(
+      `rosa create operator-roles --prefix "${options.customOperatorRolesPrefix}" --oidc-config-id "${options.byoOidcConfigID}" --installer-role-arn ${options.installerRoleArn} --shared-vpc-role-arn ${options.sharedVpcRoleArn}`,
+    );
+  });
+
+  test('should return the proper operator roles command for classic clusters without shared vpc', () => {
+    const options = { ...defaultOptions, forcedByoOidcType: undefined };
+    const command = getOperatorRolesCommand(options);
+
+    expect(command).toBe(
+      `rosa create operator-roles --prefix "${options.customOperatorRolesPrefix}" --oidc-config-id "${options.byoOidcConfigID}" --installer-role-arn ${options.installerRoleArn}`,
+    );
+  });
+
+  test('should return an empty string if "forcedByoOidcType" is empty', () => {
+    const noByoID = { ...defaultOptions, byoOidcConfigID: '' };
+    const command = getOperatorRolesCommand(noByoID);
+
+    expect(command).toBe('');
+  });
+
+  test('should return an empty string if "customOperatorRolesPrefix" is empty', () => {
+    const noOperatorPrefix = { ...defaultOptions, customOperatorRolesPrefix: '' };
+    const command = getOperatorRolesCommand(noOperatorPrefix);
+
+    expect(command).toBe('');
+  });
+
+  test('should return an empty string if "customOperatorRolesPrefix" is not valid', () => {
+    const noOperatorPrefix = { ...defaultOptions, customOperatorRolesPrefix: 'invalid:value' };
+    const command = getOperatorRolesCommand(noOperatorPrefix);
+
+    expect(command).toBe('');
+  });
+});
