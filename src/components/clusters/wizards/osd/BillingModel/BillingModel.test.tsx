68cdcc724f3dff6fa7a181457d71c2d77d05ac3a
Enrique Mingorance Cano <user@example.com>
2023-10-01 13:08:02 +0000

    [HAC-714] OSD Wizard] Billing Model CCS default Infrastructure type

---
+++ src/components/clusters/wizards/osd/BillingModel/BillingModel.test.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testUtils';
+import { Formik } from 'formik';
+import React from 'react';
+import { initialValues } from '../../rosa/constants';
+import { BillingModel } from './BillingModel';
+
+const buildTestComponent = () => (
+  <Formik
+    initialValues={{
+      ...initialValues,
+    }}
+    initialTouched={{}}
+    onSubmit={() => {}}
+  >
+    <BillingModel />
+  </Formik>
+);
+
+describe('<BillingModel />', () => {
+  describe('check initial state', () => {
+    it('is default infrastructure type ok?', () => {
+      const { container } = render(buildTestComponent());
+      const byocRadioCCSOption = container.querySelector('#form-radiobutton-byoc-true-field');
+      expect(byocRadioCCSOption).toBeInTheDocument();
+      expect(byocRadioCCSOption).toHaveAttribute('checked');
+    });
+  });
+});
