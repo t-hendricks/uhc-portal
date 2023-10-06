b05f035c87636af17b824a6164c72791a8cfdba7
Celia Amador Gonzalez <user@example.com>
2023-10-06 08:40:41 +0000

    Merge branch 'HAC-5102-fix-unit-tests-errors' into 'master'
    HAC-5102 Fix unit tests errors (Part 1)
    
    Closes HAC-5102
    
    See merge request service/uhc-portal!4589
    
    (cherry picked from commit f689aee87142457bcab3df0230bfbee17d5aef10,
     AGAIN — ClusterIngressCard.test.jsx is the remaining part after
     partial cherry-pick commit 145820789c108f80479bf8d9f6ab593100495184)

--- src/components/clusters/ClusterDetails/components/Networking/components/ClusterIngressCard/ClusterIngressCard.test.jsx
+++ src/components/clusters/ClusterDetails/components/Networking/components/ClusterIngressCard/ClusterIngressCard.test.jsx
@@ -4,31 +4,74 @@ import { mockRestrictedEnv, render, screen } from '../../../../../../../testUtil
 import ClusterIngressCard from './ClusterIngressCard';
 
 describe('<ClusterIngressCard />', () => {
-  const isRestrictedEnv = mockRestrictedEnv();
-  describe('in restricted env', () => {
-    const props = {
-      isBYOVPC: true,
-    };
+  const loadedState = {
+    clusters: {
+      details: {
+        cluster: { openshift_version: '4.13.4', api: { url: 'controlPlaneAPIEndpoint' } },
+      },
+    },
+  };
 
-    afterEach(() => {
+  const defaultProps = {
+    isBYOVPC: true,
+    isApiPrivate: true,
+    isAdditionalRouterPrivate: false,
+    hasAdditionalRouter: true,
+    canEdit: false,
+    isReadOnly: false,
+    isSTSEnabled: false,
+    clusterHibernating: false,
+    showConsoleLink: false,
+    openModal: jest.fn(),
+    refreshCluster: jest.fn(),
+    controlPlaneAPIEndpoint: '',
+    additionalRouterAddress: '',
+  };
+
+  describe('in default environment', () => {
+    it('renders footer', async () => {
+      render(<ClusterIngressCard {...defaultProps} />, {}, loadedState);
+      expect(screen.queryByText('Edit cluster ingress')).toBeInTheDocument();
+    });
+
+    it('renders the API as public when isApiPrivate=false', async () => {
+      render(<ClusterIngressCard {...defaultProps} isApiPrivate={false} />, {}, loadedState);
+      expect(screen.queryByText('Public API')).toBeInTheDocument();
+      expect(screen.queryByText('Private API')).not.toBeInTheDocument();
+    });
+
+    it('renders the API as private when isApiPrivate=true', async () => {
+      render(<ClusterIngressCard {...defaultProps} isApiPrivate />, {}, loadedState);
+      expect(screen.queryByText('Public API')).not.toBeInTheDocument();
+      expect(screen.queryByText('Private API')).toBeInTheDocument();
+    });
+  });
+
+  describe('in restricted env', () => {
+    const isRestrictedEnv = mockRestrictedEnv();
+
+    beforeAll(() => {
+      isRestrictedEnv.mockReturnValue(true);
+    });
+
+    afterAll(() => {
       isRestrictedEnv.mockReturnValue(false);
     });
 
     it('does not render footer', async () => {
-      const { rerender } = render(<ClusterIngressCard {...props} />);
-      expect(screen.queryByText('Edit cluster ingress')).toBeInTheDocument();
-
-      isRestrictedEnv.mockReturnValue(true);
-      rerender(<ClusterIngressCard {...props} />);
+      render(<ClusterIngressCard {...defaultProps} />, {}, loadedState);
       expect(screen.queryByText('Edit cluster ingress')).not.toBeInTheDocument();
     });
 
-    it('API and router are shown as private', async () => {
-      const { rerender } = render(<ClusterIngressCard {...props} />);
-      expect(screen.queryByText('Private API')).not.toBeInTheDocument();
+    it('renders the API as private in any case', async () => {
+      const { rerender } = render(
+        <ClusterIngressCard {...defaultProps} isApiPrivate={false} />,
+        {},
+        loadedState,
+      );
+      expect(screen.queryByText('Private API')).toBeInTheDocument();
 
-      isRestrictedEnv.mockReturnValue(true);
-      rerender(<ClusterIngressCard {...props} />);
+      rerender(<ClusterIngressCard {...defaultProps} isApiPrivate />, {}, loadedState);
       expect(screen.queryByText('Private API')).toBeInTheDocument();
     });
   });
