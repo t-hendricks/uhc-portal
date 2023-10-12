f416b9fd10f9be8e58002e89b3c365bda10f2fdc
Bat-Zion Rotman <user@example.com>
2023-10-12 13:12:54 +0300

    ROSA hands-on text polishing

--- src/components/RosaHandsOn/RosaHandsOnGetStartedCard.tsx
+++ src/components/RosaHandsOn/RosaHandsOnGetStartedCard.tsx
@@ -19,7 +19,6 @@ import { global_link_Color_dark } from '@patternfly/react-tokens';
 import RosaHandsOnGetStartedToolbar, {
   RosaHandsOnToolbarProps,
 } from './RosaHandsOnGetStartedToolbar';
-import { DemoExperienceStatusEnum } from './DemoExperienceModels';
 import RosaHandsOnIcon from './RosaHandsOnIcons/RosaHandsOnIcon';
 import ExternalLink from '../common/ExternalLink';
 import { MAX_DURATION, rosaHandsOnLinks } from './constants';
@@ -60,50 +59,44 @@ const RosaHandsOnGetStartedCard = ({
           <Stack hasGutter>
             <StackItem>
               <TextContent>
-                <Text component="h3">Get started with OpenShift</Text>
+                <Text component="h3">What you get with this experience</Text>
               </TextContent>
             </StackItem>
             <StackItem isFilled>
-              {demoExperience.status === DemoExperienceStatusEnum.Started ? (
-                <>
-                  Clicking the launch experience button below will provide a guided walkthrough of
-                  ROSA.
-                </>
-              ) : (
-                <List isPlain>
-                  <CheckListItem
-                    popoverContent={
-                      <>
-                        You may launch a demo up to {demoExperience.quota.limit} times total. If you
-                        run out of attempts, have questions or need help, you can chat with the
-                        customer success team by clicking on the hat icon located at the
-                        bottom-right corner of the page. Alternatively, you can also join the{' '}
-                        <ExternalLink href={rosaHandsOnLinks.slackChannel}>
-                          ROSA community Slack channel
-                        </ExternalLink>
-                      </>
-                    }
-                    popoverHeader="What to expect"
-                  >
-                    Access to a free hands-on experience of ROSA for {MAX_DURATION} hours.
-                  </CheckListItem>
-                  <CheckListItem
-                    popoverContent="Your time will not start until after the cluster is provisioned"
-                    popoverHeader="Getting started"
-                  >
-                    One-click deployment of a ROSA cluster in a demo environment.
-                  </CheckListItem>
-                  <CheckListItem>
-                    A view into a fully managed turnkey application platform with integrated
-                    services and tools, fully backed by a global team of site reliability engineers
-                    (SREs).
-                  </CheckListItem>
-                  <CheckListItem>
-                    Step-by-step guided content on how to get started and build, deploy and scale an
-                    application on ROSA.
-                  </CheckListItem>
-                </List>
-              )}
+              <List isPlain>
+                <CheckListItem
+                  popoverContent={
+                    <>
+                      You may launch a demo up to{' '}
+                      <strong>{demoExperience.quota.limit} times</strong> total. If you have
+                      questions, you can chat with the customer success team by clicking on the hat
+                      icon located at the bottom-right corner of the page. Alternatively, you can
+                      also join the{' '}
+                      <ExternalLink href={rosaHandsOnLinks.slackChannel}>
+                        ROSA community Slack channel
+                      </ExternalLink>
+                    </>
+                  }
+                  popoverHeader="What to expect"
+                >
+                  Access to a free hands-on experience of ROSA that will last for {MAX_DURATION}{' '}
+                  hours.
+                </CheckListItem>
+                <CheckListItem
+                  popoverContent="Your time will not start until after the cluster is provisioned"
+                  popoverHeader="Getting started"
+                >
+                  One-click deployment of a ROSA cluster in a demo environment.
+                </CheckListItem>
+                <CheckListItem>
+                  A view into a fully managed turnkey application platform with integrated services
+                  and tools, fully backed by a global team of site reliability engineers (SREs).
+                </CheckListItem>
+                <CheckListItem>
+                  Step-by-step guided content on how to get started and build, deploy and scale an
+                  application on ROSA.
+                </CheckListItem>
+              </List>
             </StackItem>
             <StackItem>
               <RosaHandsOnGetStartedToolbar
