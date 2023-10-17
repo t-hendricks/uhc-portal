f347a2877cd79f00c2d5ca5181cf0c132ef3d82c
Beni Paskin Cherniavsky <user@example.com>
2023-10-17 11:03:58 +0000

    Merge branch 'osdTrial_quota' into 'master'
    HAC-5013 Fix OSDTrial -> OSD quota calculations
    
    Closes HAC-5013
    
    See merge request service/uhc-portal!4591
    
    (cherry picked from commit 95e51cfcc8d2e06806d88149383717024aab3d07)

--- src/components/clusters/common/UpgradeTrialClusterDialog/UpgradeTrialClusterDialog.test.jsx
+++ src/components/clusters/common/UpgradeTrialClusterDialog/UpgradeTrialClusterDialog.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
 import fixtures from '../../ClusterDetails/__test__/ClusterDetails.fixtures';
-import { emptyQuotaList } from '../__test__/quota.fixtures';
+import { emptyQuotaList, mockQuotaList } from '../__test__/quota.fixtures';
 import UpgradeTrialClusterDialog from './UpgradeTrialClusterDialog';
-import ErrorBox from '../../../common/ErrorBox';
+import { render, screen } from '../../../../testUtils';
 
 describe('<UpgradeTrialClusterDialog />', () => {
   const organizationState = {
@@ -16,7 +16,12 @@ describe('<UpgradeTrialClusterDialog />', () => {
   const machineTypesByID = {
     'm5.xlarge': { id: 'm5.xlarge', generic_name: 'standard-4' },
   };
-  let wrapper;
+  const machinePools = [
+    {
+      instance_type: 'm5.xlarge',
+      replicas: 3,
+    },
+  ];
   let closeModal;
   let onClose;
   let submit;
@@ -29,32 +34,120 @@ describe('<UpgradeTrialClusterDialog />', () => {
     submit = jest.fn();
     resetResponse = jest.fn();
     getOrganizationAndQuota = jest.fn();
-    wrapper = shallow(
-      <UpgradeTrialClusterDialog
-        isOpen
-        closeModal={closeModal}
-        onClose={onClose}
-        submit={submit}
-        resetResponse={resetResponse}
-        organization={organizationState}
-        getOrganizationAndQuota={getOrganizationAndQuota}
-        clusterID="some-cluster-id"
-        cluster={cluster}
-        machineTypesByID={machineTypesByID}
-        upgradeTrialClusterResponse={{ errorMessage: '', error: false, fulfilled: false }}
-      />,
+  });
+
+  it('renders no-quota alert when there is no quota', () => {
+    render(
+      <MemoryRouter>
+        <UpgradeTrialClusterDialog
+          isOpen
+          closeModal={closeModal}
+          onClose={onClose}
+          submit={submit}
+          resetResponse={resetResponse}
+          organization={organizationState}
+          getOrganizationAndQuota={getOrganizationAndQuota}
+          clusterID="some-cluster-id"
+          cluster={cluster}
+          machineTypesByID={machineTypesByID}
+          upgradeTrialClusterResponse={{ errorMessage: '', error: false, fulfilled: false }}
+          machinePools={machinePools}
+        />
+      </MemoryRouter>,
     );
+    expect(screen.getByTestId('no-quota-alert')).toBeInTheDocument();
+    expect(screen.getByText('Contact sales')).toBeInTheDocument();
   });
 
-  it('renders correctly', () => {
-    expect(wrapper).toMatchSnapshot();
+  it('allows upgrade via marketplace billing', () => {
+    const orgState = {
+      fulfilled: true,
+      pending: false,
+      quotaList: mockQuotaList,
+    };
+    render(
+      <MemoryRouter>
+        <UpgradeTrialClusterDialog
+          isOpen
+          closeModal={closeModal}
+          onClose={onClose}
+          submit={submit}
+          resetResponse={resetResponse}
+          organization={orgState}
+          getOrganizationAndQuota={getOrganizationAndQuota}
+          clusterID="some-cluster-id"
+          cluster={cluster}
+          machineTypesByID={machineTypesByID}
+          upgradeTrialClusterResponse={{ errorMessage: '', error: false, fulfilled: false }}
+          machinePools={[
+            {
+              instance_type: 'm5.xlarge',
+              replicas: 140,
+            },
+          ]}
+        />
+      </MemoryRouter>,
+    );
+
+    expect(screen.queryByTestId('no-quota-alert')).not.toBeInTheDocument();
+    expect(screen.getByText('Upgrade using Marketplace billing')).toBeInTheDocument();
+    expect(screen.queryByText('Upgrade using quota')).not.toBeInTheDocument();
   });
 
-  it('renders correctly when an erorr occurs', () => {
-    wrapper.setProps({
-      upgradeTrialClusterResponse: { error: true, errorMessage: 'this is an error' },
-    });
-    expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find(ErrorBox).length).toEqual(1);
+  it('allows upgrade via standard or marketplace billing', () => {
+    const orgState = {
+      fulfilled: true,
+      pending: false,
+      quotaList: mockQuotaList,
+    };
+    render(
+      <MemoryRouter>
+        <UpgradeTrialClusterDialog
+          isOpen
+          closeModal={closeModal}
+          onClose={onClose}
+          submit={submit}
+          resetResponse={resetResponse}
+          organization={orgState}
+          getOrganizationAndQuota={getOrganizationAndQuota}
+          clusterID="some-cluster-id"
+          cluster={cluster}
+          machineTypesByID={machineTypesByID}
+          upgradeTrialClusterResponse={{ errorMessage: '', error: false, fulfilled: false }}
+          machinePools={[
+            {
+              instance_type: 'm5.xlarge',
+              replicas: 130,
+            },
+          ]}
+        />
+      </MemoryRouter>,
+    );
+    expect(screen.queryByTestId('no-quota-alert')).not.toBeInTheDocument();
+    expect(screen.getByText('Upgrade using Marketplace billing')).toBeInTheDocument();
+    expect(screen.getByText('Upgrade using quota')).toBeInTheDocument();
+  });
+
+  it('renders error box when an erorr occurs', () => {
+    render(
+      <MemoryRouter>
+        <UpgradeTrialClusterDialog
+          isOpen
+          closeModal={closeModal}
+          onClose={onClose}
+          submit={submit}
+          resetResponse={resetResponse}
+          organization={organizationState}
+          getOrganizationAndQuota={getOrganizationAndQuota}
+          clusterID="some-cluster-id"
+          cluster={cluster}
+          machineTypesByID={machineTypesByID}
+          upgradeTrialClusterResponse={{ error: true, errorMessage: 'this is an error' }}
+          machinePools={machinePools}
+        />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText('Error upgrading cluster')).toBeInTheDocument();
   });
 });
