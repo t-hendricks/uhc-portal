053b1fc06f76a987dfc3827d30dd24df42fa33d4
Liza Gilman <user@example.com>
2023-11-09 09:36:14 +0000

    Merge branch 'OCMUI-552-security-groups-tooltip' into 'master'
    OCMUI-552 Fix overlapping tooltips issue in Security groups
    
    Closes OCMUI-552
    
    See merge request service/uhc-portal!4801
    
    (cherry picked from commit 32d364794d343dac1f6cf866a75fb7f7fec794eb)

--- src/components/clusters/ClusterDetails/components/MachinePools/components/EditMachinePoolModal/sections/EditSecurityGroupsSection.tsx
+++ src/components/clusters/ClusterDetails/components/MachinePools/components/EditMachinePoolModal/sections/EditSecurityGroupsSection.tsx
@@ -4,6 +4,7 @@ import { ExpandableSection } from '@patternfly/react-core';
 import { SECURITY_GROUPS_FEATURE } from '~/redux/constants/featureConstants';
 
 import { Cluster } from '~/types/clusters_mgmt.v1';
+import WithTooltip from '~/components/common/WithTooltip';
 import { isCompatibleFeature, SupportedFeatures } from '~/common/featureCompatibility';
 import { useFeatureGate } from '~/hooks/useFeatureGate';
 import EditSecurityGroups from './EditSecurityGroups';
@@ -20,7 +21,16 @@ const EditSecurityGroupsSection = ({ cluster, isEdit }: EditSecurityGroupsSectio
     isCompatibleFeature(SupportedFeatures.SECURITY_GROUPS, cluster, { day2: true });
 
   return showSecurityGroupSection ? (
-    <ExpandableSection toggleText="Security groups">
+    <ExpandableSection
+      toggleContent={
+        <WithTooltip
+          showTooltip={isEdit}
+          content="This option cannot be edited from its original setting selection."
+        >
+          <span>Security groups</span>
+        </WithTooltip>
+      }
+    >
       <EditSecurityGroups cluster={cluster} isEdit={isEdit} />
     </ExpandableSection>
   ) : null;
