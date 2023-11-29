367548dade88327bf22a7745d3daf30ec8c5710c
John Swanke <user@example.com>
2023-11-29 16:03:26 +0000

    OCMUI-1069, OCMUI-1071, OCMUI-1085, OCMUI-1105 network-banner-missing-on-network-error

--- src/components/clusters/common/InstallProgress/InstallProgress.tsx
+++ src/components/clusters/common/InstallProgress/InstallProgress.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
-import { InflightCheckState } from '~/types/clusters_mgmt.v1';
 
 import { ClusterFromSubscription } from '~/types/types';
-import clusterStates, { getInflightChecks } from '../clusterStates';
+import clusterStates from '../clusterStates';
 import ProgressList from './ProgressList';
 
 interface InstallProgressProps {
   cluster: ClusterFromSubscription;
+  hasInflightErrors: boolean;
   hasNetworkOndemand: boolean;
 }
 
-const InstallProgress = ({ cluster, hasNetworkOndemand }: InstallProgressProps) => {
-  const isAnyCheckNotPassedState = getInflightChecks(cluster).some(
-    (check) => check.state !== InflightCheckState.PASSED,
-  );
-
-  return isAnyCheckNotPassedState ||
-    cluster.state === clusterStates.INSTALLING ||
-    cluster.state === clusterStates.PENDING ||
-    cluster.state === clusterStates.VALIDATING ||
-    cluster.state === clusterStates.WAITING ? (
+const InstallProgress = ({
+  cluster,
+  hasNetworkOndemand,
+  hasInflightErrors,
+}: InstallProgressProps) =>
+  hasInflightErrors ||
+  cluster.state === clusterStates.INSTALLING ||
+  cluster.state === clusterStates.PENDING ||
+  cluster.state === clusterStates.VALIDATING ||
+  cluster.state === clusterStates.WAITING ? (
     <ProgressList
       cluster={cluster}
       hasNetworkOndemand={hasNetworkOndemand}
       actionRequiredInitialOpen
     />
   ) : null;
-};
 
 export default InstallProgress;
