49240a6c92e5222430777f6da264959b2501c45e
eliran <user@example.com>
2023-11-30 22:37:34 +0200

    fix(wizard): labels bleed in review page
    truncate labels text contents in the review page, and add a tooltip with the full text.
    
    this change fixes node-labels as well as no-proxy domains.

--- src/components/clusters/CreateOSDPage/CreateOSDWizard/ReviewClusterScreen/reviewValues.jsx
+++ src/components/clusters/CreateOSDPage/CreateOSDWizard/ReviewClusterScreen/reviewValues.jsx
@@ -227,7 +227,7 @@ const reviewValues = {
         {
           // eslint-disable-next-line react/destructuring-assignment
           labels.map((label) => (
-            <Label color="blue">{`${label.key} = ${label.value || ''}`}</Label>
+            <Label color="blue" isTruncated>{`${label.key} = ${label.value || ''}`}</Label>
           ))
         }
       </LabelGroup>
@@ -393,7 +393,7 @@ const reviewValues = {
       <LabelGroup>
         {/* eslint-disable-next-line react/destructuring-assignment */}
         {noProxyDomains.map((domain) => (
-          <Label isCompact color="blue">
+          <Label color="blue" isCompact isTruncated>
             {domain}
           </Label>
         ))}
