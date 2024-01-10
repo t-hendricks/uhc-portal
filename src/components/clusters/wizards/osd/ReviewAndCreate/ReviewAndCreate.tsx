d46f13ff46f416428b31714dd46918b1bda17a8b
Joachim Schuler <user@example.com>
2024-01-10 14:51:53 -0500

    [PF5] OCMUI-1239: Preserve previous Wizard button order

--- src/components/clusters/wizards/osd/ReviewAndCreate/ReviewAndCreate.tsx
+++ src/components/clusters/wizards/osd/ReviewAndCreate/ReviewAndCreate.tsx
@@ -1,45 +1,20 @@
 import React from 'react';
 
-import { useWizardFooter } from '@patternfly/react-core';
-
 import { useGlobalState } from '~/redux/hooks/useGlobalState';
 import CreateClusterErrorModal from '~/components/clusters/common/CreateClusterErrorModal';
 import { useFormState } from '~/components/clusters/wizards/hooks';
-import { StepId } from '~/components/clusters/wizards/osd/constants';
 import { ReviewAndCreateContent } from './ReviewAndCreateContent';
 
-interface ReviewAndCreateProps {
-  track(): void;
-}
-
-export const ReviewAndCreate = ({ track }: ReviewAndCreateProps) => {
+export const ReviewAndCreate = () => {
   const { submitForm } = useFormState();
   const createClusterResponse = useGlobalState((state) => state.clusters.createdCluster);
   const [isErrorModalOpen, setIsErrorModalOpen] = React.useState(false);
-  const isSubmitting = createClusterResponse.pending;
 
   React.useEffect(() => {
     if (createClusterResponse.error && !isErrorModalOpen) {
       setIsErrorModalOpen(true);
     }
   }, [createClusterResponse.error, isErrorModalOpen]);
-
-  useWizardFooter(
-    React.useMemo(() => {
-      if (isSubmitting) {
-        return <></>;
-      }
-      return {
-        nextButtonText: 'Create cluster',
-        onNext: () => {
-          submitForm();
-          track();
-        },
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [submitForm, track]),
-    StepId.Review,
-  );
 
   return (
     <div className="pf-v5-u-mb-md">
