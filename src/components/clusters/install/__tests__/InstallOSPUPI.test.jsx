167f61cd7c2286e9d7b8f1dfcc12460e595e52b6
Kim Doberstein <user@example.com>
2024-01-17 13:55:45 -0600

    OCMUI-1283 Remove snapshot tests batch 7

--- src/components/clusters/install/__tests__/InstallOSPUPI.test.jsx
+++ src/components/clusters/install/__tests__/InstallOSPUPI.test.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { screen, checkAccessibility, TestRouter, withState } from '~/testUtils';
 
 import { InstallOSPUPI } from '../InstallOSPUPI';
+import instructionsMapping from '../instructions/instructionsMapping';
+import githubReleases from '../githubReleases.mock';
+
+jest.mock('../../../../redux/actions', () => ({
+  __esModule: true,
+  tollboothActions: {
+    createAuthToken: jest.fn().mockResolvedValue('foo'),
+  },
+  githubActions: {
+    getLatestRelease: jest.fn(),
+  },
+}));
 
 describe('InstallOSPUPI', () => {
-  it('renders correctly', () => {
-    const wrapper = shallow(<InstallOSPUPI token={{}} dispatch={() => {}} />);
-    expect(wrapper).toMatchSnapshot();
+  const dispatch = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('is accessible', async () => {
+    const { container } = withState(githubReleases).render(
+      <TestRouter>
+        <InstallOSPUPI token={{}} dispatch={dispatch} />
+      </TestRouter>,
+    );
+
+    expect(await screen.findByText(instructionsMapping.openstack.upi.title)).toBeInTheDocument();
+
+    await checkAccessibility(container);
   });
 });
--- src/components/clusters/install/__tests__/InstallPlatformAgnosticUPI.test.jsx
+++ src/components/clusters/install/__tests__/InstallPlatformAgnosticUPI.test.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { screen, checkAccessibility, TestRouter, withState } from '~/testUtils';
 
 import { InstallPlatformAgnosticUPI } from '../InstallPlatformAgnosticUPI';
+import instructionsMapping from '../instructions/instructionsMapping';
+import githubReleases from '../githubReleases.mock';
+
+jest.mock('../../../../redux/actions', () => ({
+  __esModule: true,
+  tollboothActions: {
+    createAuthToken: jest.fn().mockResolvedValue('foo'),
+  },
+  githubActions: {
+    getLatestRelease: jest.fn(),
+  },
+}));
 
 describe('Platform agnostic UPI install', () => {
-  it('renders correctly', () => {
-    const wrapper = shallow(<InstallPlatformAgnosticUPI token={{}} dispatch={() => {}} />);
-    expect(wrapper).toMatchSnapshot();
+  const dispatch = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('is accessible', async () => {
+    const { container } = withState(githubReleases).render(
+      <TestRouter>
+        <InstallPlatformAgnosticUPI token={{}} dispatch={dispatch} />
+      </TestRouter>,
+    );
+
+    expect(await screen.findByText(instructionsMapping.generic.upi.title)).toBeInTheDocument();
+
+    await checkAccessibility(container);
   });
 });
