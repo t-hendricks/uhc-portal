326197bdb8e2294355a2d39960e51b78ed537841
Celia Amador <user@example.com>
2024-01-26 11:21:31 +0100

    Refactor to TS and add test to actionResolver

--- src/components/dashboard/ClustersWithIssuesTableCard/ClustersWithIssuesActionResolver.ts
+++ src/components/dashboard/ClustersWithIssuesTableCard/ClustersWithIssuesActionResolver.ts
@@ -1,10 +1,12 @@
+import { Subscription } from '~/types/accounts_mgmt.v1';
+
 /**
  * This function is used by PF tables to determine which dropdown items are displayed
  * on each row of the table. It returns a list of objects, containing props for DropdownItem
  * PF table renders automatically.
  * @param {*} subscription             The subscription object corresponding to the current row
  */
-function actionResolver(subscription) {
+function actionResolver(subscription: Subscription) {
   const baseProps = {
     title: 'Open console',
     key: `${subscription.id}.menu.adminconsole`,
