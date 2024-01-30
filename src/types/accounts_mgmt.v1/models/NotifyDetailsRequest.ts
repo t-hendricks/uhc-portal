33c6ccb98a29a58a3b3a7be7751a5938f9a452eb
Liza Gilman <user@example.com>
2024-01-30 11:28:51 +0000

    Merge branch 'OCMUI-1301' into 'master'
    [OCMUI-1301] types filters from Cluster history section updated
    
    Closes OCMUI-1301
    
    See merge request service/uhc-portal!5054
    
    (cherry picked from commit 2a6bafd59de828a7d478270bbd8529b7444a6720)

--- src/types/accounts_mgmt.v1/models/NotifyDetailsRequest.ts
+++ src/types/accounts_mgmt.v1/models/NotifyDetailsRequest.ts
@@ -2,17 +2,16 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import type { TemplateParameter } from './TemplateParameter';
-
-export type NotificationRequest = {
+export type NotifyDetailsRequest = {
   bcc_address?: string;
   cluster_id?: string;
   cluster_uuid?: string;
   include_red_hat_associates?: boolean;
+  /**
+   * The `internal_only` parameter is used for validation. Specifically to check if there is a discrepancy between the email address and the log type.
+   */
   internal_only?: boolean;
   org_id?: string;
   subject?: string;
   subscription_id?: string;
-  template_name: string;
-  template_parameters?: Array<TemplateParameter>;
 };
