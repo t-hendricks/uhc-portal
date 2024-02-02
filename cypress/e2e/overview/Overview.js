96eabeddaeee9cae0ff6a78b3f13d25d0bdbd8e6
Vitor Graziano <user@example.com>
2024-02-02 17:44:31 +0100

    Attempt to fix overview randomic failures

--- cypress/e2e/overview/Overview.js
+++ cypress/e2e/overview/Overview.js
@@ -94,8 +94,7 @@ describe('OCM Overview Page tests (OCP-65189)', { tags: ['smoke'] }, () => {
         'Learn more on IBM',
         'https://cloud.ibm.com/kubernetes/catalog/create?platformType=openshift',
       )
-      .opensInRightTab()
-      .successfullyOpens();
+      .opensInRightTab();
 
     card = Overview.centralSectionCard('offering-card_DEVSNBX');
     card.cyObj.contains('Developer Sandbox');
