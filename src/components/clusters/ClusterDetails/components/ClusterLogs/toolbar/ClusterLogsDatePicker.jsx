a5da71a36c3ad60301b14a3c0645cdc0b34a5a3c
David Aznaurov <user@example.com>
2024-02-07 18:11:04 +0000

    OCMUI-767: fixed that user had to press enter to update date and fixed wrong...

--- src/components/clusters/ClusterDetails/components/ClusterLogs/toolbar/ClusterLogsDatePicker.jsx
+++ src/components/clusters/ClusterDetails/components/ClusterLogs/toolbar/ClusterLogsDatePicker.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { DatePicker, ToolbarItem, isValidDate } from '@patternfly/react-core';
+import {
+  DatePicker,
+  HelperText,
+  HelperTextItem,
+  ToolbarItem,
+  isValidDate,
+} from '@patternfly/react-core';
 import {
   Select as SelectDeprecated,
   SelectOption as SelectOptionDeprecated,
 } from '@patternfly/react-core/deprecated';
 import './ClusterLogsDatePicker.scss';
-
 // Local timezone UTC offset
 const offset = new Date().getTimezoneOffset() * 60000;
 
@@ -117,6 +122,21 @@ const ClusterLogsDatePicker = ({ setFilter, currentFilter, createdAt }) => {
   const [selected, setSelected] = useState(options[0].value);
   // used to force re-render due to bug https://github.com/patternfly/patternfly-react/issues/7818
   const [counter, setCounter] = useState(0);
+  const endDateObject = dateParse(endDateStr);
+  const startDateObject = dateParse(startDateStr);
+
+  let invalidDateError;
+
+  if (
+    startDateObject > endDateObject &&
+    startDateObject <= now &&
+    endDateObject <= now &&
+    endDateObject >= minDate
+  ) {
+    invalidDateError = 'The end date cannot be before the start date.';
+  } else {
+    invalidDateError = '';
+  }
 
   const onDateChange = (filters) => {
     const filterObject = {
@@ -142,21 +162,13 @@ const ClusterLogsDatePicker = ({ setFilter, currentFilter, createdAt }) => {
    * Validate input against min/max dates in UTC time
    * Also determines the clickable range in the picker
    */
-  const endDateObject = dateParse(endDateStr);
-  const startDateObject = dateParse(startDateStr);
 
   const rangeFromValidators = [
-    (date) =>
-      endDateObject < date && date < now ? 'The start date cannot be after the end date.' : '',
     (date) => (date < minDate ? 'The start date cannot be before the cluster creation date.' : ''),
     (date) => (date > now ? 'The start date cannot be in the future.' : ''),
   ];
 
   const rangeToValidators = [
-    (date) =>
-      startDateObject > date && date > minDate
-        ? 'The end date cannot be before the start date.'
-        : '',
     (date) => (date < minDate ? 'The end date cannot be before the cluster creation date.' : ''),
     (date) => (date > now ? 'The end date cannot be in the future.' : ''),
   ];
@@ -211,6 +223,7 @@ const ClusterLogsDatePicker = ({ setFilter, currentFilter, createdAt }) => {
   const pickerFrom = (
     <DatePicker
       onChange={(_, dateStr, date) => inputOnChangeFrom(dateStr, date)}
+      onBlur={(_, dateStr, date) => inputOnChangeFrom(dateStr, date)}
       value={startDateStr}
       dateParse={dateParse}
       validators={rangeFromValidators}
@@ -221,6 +234,7 @@ const ClusterLogsDatePicker = ({ setFilter, currentFilter, createdAt }) => {
   const pickerTo = (
     <DatePicker
       onChange={(_, dateStr, date) => inputOnChangeTo(dateStr, date)}
+      onBlur={(_, dateStr, date) => inputOnChangeTo(dateStr, date)}
       value={endDateStr}
       dateParse={dateParse}
       validators={rangeToValidators}
@@ -285,7 +299,14 @@ const ClusterLogsDatePicker = ({ setFilter, currentFilter, createdAt }) => {
     <>
       <ToolbarItem>{dateSelector}</ToolbarItem>
       <ToolbarItem>{pickerFrom}</ToolbarItem>
-      <ToolbarItem>{pickerTo}</ToolbarItem>
+      <ToolbarItem>
+        {pickerTo}{' '}
+        {invalidDateError ? (
+          <HelperText>
+            <HelperTextItem variant="error">{invalidDateError}</HelperTextItem>
+          </HelperText>
+        ) : undefined}
+      </ToolbarItem>
     </>
   );
 };
