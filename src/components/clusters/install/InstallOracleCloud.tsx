955221aa8d87780c87e78b3d573f348fb7b0d5d3
Montse Ortega Gallart <user@example.com>
2024-02-14 08:25:56 +0000

    Merge branch 'OCMUI-1408-add-oracle-create-cluster-to-landing-page' into 'master'
    OCMUI-1408: Add Oracle Cloud[tech preview] to create cluster landing page
    
    Closes OCMUI-1408
    
    See merge request service/uhc-portal!5151
    
    (cherry picked from commit 483eae318155c629adbddbb6a821731491f49948)

---
+++ src/components/clusters/install/InstallOracleCloud.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { PageSection } from '@patternfly/react-core';
+
+import { AppPage } from '~/components/App/AppPage';
+import Breadcrumbs from '../../common/Breadcrumbs';
+import ExternalLink from '../../common/ExternalLink';
+import links from '../../../common/installLinks.mjs';
+import { InstructionsChooser } from './instructions/InstructionsChooser';
+import { InstructionsChooserPageTitle } from './instructions/InstructionsChooserPageTitle';
+
+const breadcrumbs = (
+  <Breadcrumbs
+    path={[
+      { label: 'Clusters' },
+      { label: 'Cluster Type', path: '/create' },
+      { label: 'Oracle Cloud Infrastructure (virtual machines)' },
+    ]}
+  />
+);
+
+const nonTestedPlatformsLink = (
+  <>
+    For&nbsp;
+    <ExternalLink href={links.INSTALL_GENERIC_NON_TESTED_PLATFORMS} stopClickPropagation>
+      non-tested platforms
+    </ExternalLink>
+  </>
+);
+
+const InstallOracleCloud = () => (
+  <AppPage title="Install OpenShift 4 | Red Hat OpenShift Cluster Manager | Oracle Cloud Infrastructure (virtual machines)">
+    <InstructionsChooserPageTitle
+      cloudName="Oracle Cloud Infrastructure (virtual machines)"
+      breadcrumbs={breadcrumbs}
+    />
+    <PageSection>
+      <InstructionsChooser
+        aiPageLink="/assisted-installer/clusters/~new"
+        aiLearnMoreLink={links.INSTALL_ASSISTED_LEARN_MORE}
+        hideIPI
+        ipiPageLink=""
+        hideUPI
+        upiPageLink=""
+        agentBasedPageLink="/install/platform-agnostic/agent-based"
+        agentBasedLearnMoreLink={links.INSTALL_AGENT_LEARN_MORE}
+        providerSpecificFeatures={{
+          abi: [nonTestedPlatformsLink, 'For air-gapped/restricted networks'],
+          ai: [nonTestedPlatformsLink],
+          upi: [nonTestedPlatformsLink],
+        }}
+        name="oracle-cloud"
+      />
+    </PageSection>
+  </AppPage>
+);
+
+export default InstallOracleCloud;
