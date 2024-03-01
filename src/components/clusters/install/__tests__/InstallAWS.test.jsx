4318b480ddaf0d05e07ead273b5155f879fafd6d
Kim Doberstein <user@example.com>
2024-03-01 21:31:55 +0000

    OCMUI-1334 Check for docs links in tests

--- src/components/clusters/install/__tests__/InstallAWS.test.jsx
+++ src/components/clusters/install/__tests__/InstallAWS.test.jsx
@@ -4,6 +4,8 @@ import { screen, checkAccessibility, TestRouter, render } from '~/testUtils';
 
 import InstallAWS from '../InstallAWS';
 
+import { version } from '../InstallTestConstants';
+
 describe('InstallAWS', () => {
   it('is accessible', async () => {
     const { container } = render(
@@ -16,5 +18,24 @@ describe('InstallAWS', () => {
 
     expect(await screen.findByText('Create an OpenShift Cluster: AWS')).toBeInTheDocument();
     await checkAccessibility(container);
+  });
+  it('displays expected doc links', () => {
+    render(
+      <TestRouter>
+        <CompatRouter>
+          <InstallAWS />
+        </CompatRouter>
+      </TestRouter>,
+    );
+
+    expect(screen.getByRole('link', { name: /Learn more about automated/ })).toHaveAttribute(
+      'href',
+      `https://docs.openshift.com/container-platform/${version}/installing/installing_aws/installing-aws-default.html`,
+    );
+
+    expect(screen.getByRole('link', { name: /Learn more about full control/ })).toHaveAttribute(
+      'href',
+      `https://docs.openshift.com/container-platform/${version}/installing/installing_aws/installing-aws-user-infra.html`,
+    );
   });
 });
--- src/components/clusters/install/__tests__/InstallAzure.test.jsx
+++ src/components/clusters/install/__tests__/InstallAzure.test.jsx
@@ -4,6 +4,8 @@ import { screen, checkAccessibility, TestRouter, render } from '~/testUtils';
 
 import InstallAzure from '../InstallAzure';
 
+import { version } from '../InstallTestConstants';
+
 describe('InstallAzure', () => {
   it('is accessible', async () => {
     const { container } = render(
@@ -16,5 +18,25 @@ describe('InstallAzure', () => {
 
     expect(await screen.findByText('Create an OpenShift Cluster: Azure')).toBeInTheDocument();
     await checkAccessibility(container);
+  });
+
+  it('displays expected doc links', () => {
+    render(
+      <TestRouter>
+        <CompatRouter>
+          <InstallAzure />
+        </CompatRouter>
+      </TestRouter>,
+    );
+
+    expect(screen.getByRole('link', { name: /Learn more about automated/ })).toHaveAttribute(
+      'href',
+      `https://docs.openshift.com/container-platform/${version}/installing/installing_azure/installing-azure-default.html`,
+    );
+
+    expect(screen.getByRole('link', { name: /Learn more about full control/ })).toHaveAttribute(
+      'href',
+      `https://docs.openshift.com/container-platform/${version}/installing/installing_azure/installing-azure-user-infra.html`,
+    );
   });
 });
