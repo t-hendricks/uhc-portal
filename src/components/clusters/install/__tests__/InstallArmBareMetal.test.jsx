3a1e2bcbe3846146bc32808ce0f840fdc71c1f7e
David Aznaurov <user@example.com>
2024-03-01 21:31:55 +0000

    Merge branch 'OCMUI-1334-Check-for-docs-links-in-tests' into 'master'
    OCMUI-1334 Check for docs links in tests
    
    Closes OCMUI-1334
    
    See merge request service/uhc-portal!5190
    
    (cherry picked from commit 2cc52d2c179d07f428f668f21af992d0cd8a9cc1)

--- src/components/clusters/install/__tests__/InstallArmBareMetal.test.jsx
+++ src/components/clusters/install/__tests__/InstallArmBareMetal.test.jsx
@@ -4,6 +4,8 @@ import { screen, render, checkAccessibility, TestRouter } from '~/testUtils';
 
 import InstallArmBareMetal from '../InstallArmBareMetal';
 
+import { version } from '../InstallTestConstants';
+
 describe('ARM Bare Metal install', () => {
   it('is accessible', async () => {
     const { container } = render(
@@ -16,5 +18,29 @@ describe('ARM Bare Metal install', () => {
 
     expect(screen.getByText('ARM Bare Metal')).toBeInTheDocument();
     await checkAccessibility(container);
+  });
+
+  it('displays expected doc links', () => {
+    render(
+      <TestRouter>
+        <CompatRouter>
+          <InstallArmBareMetal />
+        </CompatRouter>
+      </TestRouter>,
+    );
+
+    expect(screen.getByRole('link', { name: /Learn more about interactive/ })).toHaveAttribute(
+      'href',
+      `https://docs.openshift.com/container-platform/${version}/installing/installing_on_prem_assisted/installing-on-prem-assisted.html`,
+    );
+    expect(screen.getByRole('link', { name: /Learn more about automated/ })).toHaveAttribute(
+      'href',
+      `https://docs.openshift.com/container-platform/${version}/installing/installing_bare_metal_ipi/ipi-install-overview.html`,
+    );
+
+    expect(screen.getByRole('link', { name: /Learn more about full control/ })).toHaveAttribute(
+      'href',
+      `https://docs.openshift.com/container-platform/${version}/installing/installing_bare_metal/installing-bare-metal.html`,
+    );
   });
 });
