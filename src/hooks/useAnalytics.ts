a7b75ded7d851c2e50f5c73950109486b440918a
David Aznaurov <user@example.com>
2024-03-04 13:55:45 +0000

    Merge branch 'OCMUI-1472-setup-rosa-page-link' into 'master'
    OCMUI-1472: fixed links in rosa wizard
    
    Closes OCMUI-1472
    
    See merge request service/uhc-portal!5225
    
    (cherry picked from commit 565face662292875dd68a338b5cb0c471942dccc)

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -1,4 +1,5 @@
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+import { useCallback } from 'react';
 import { getTrackEvent, TrackEvent, TrackEventOptions } from '~/common/analytics';
 
 /** a convenience function that composes the track event parsing and the actual tracking. */
@@ -13,15 +14,18 @@ interface Track {
 const useAnalytics = (): Track => {
   const { analytics } = useChrome();
 
-  const track: Track = (event, properties) => {
-    if (typeof event === 'string') {
-      analytics.track(event, typeof properties === 'string' ? { type: properties } : properties);
-    } else {
-      const eventObj = getTrackEvent(event, properties as TrackEventOptions);
-      // https://segment.com/docs/connections/spec/track/
-      analytics.track(eventObj.event, eventObj.properties);
-    }
-  };
+  const track: Track = useCallback(
+    (event, properties) => {
+      if (typeof event === 'string') {
+        analytics.track(event, typeof properties === 'string' ? { type: properties } : properties);
+      } else {
+        const eventObj = getTrackEvent(event, properties as TrackEventOptions);
+        // https://segment.com/docs/connections/spec/track/
+        analytics.track(eventObj.event, eventObj.properties);
+      }
+    },
+    [analytics],
+  );
 
   return track;
 };
