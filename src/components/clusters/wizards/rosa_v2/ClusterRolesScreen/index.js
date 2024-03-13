d7071df7325028438383c87400d89ce033592bed
Dylan Cooper <user@example.com>
2024-03-13 19:36:39 +0000

    OCMUI-814: ROSA wizard v2: Refactor Cluster roles and policies step

--- src/components/clusters/wizards/rosa_v2/ClusterRolesScreen/index.js
+++ src/components/clusters/wizards/rosa_v2/ClusterRolesScreen/index.js
@@ -1,7 +1,5 @@
 import { connect } from 'react-redux';
-import { formValueSelector } from 'redux-form';
 
-import wizardConnector from '~/components/clusters/wizards/common/WizardConnector';
 import ClusterRolesScreen from './ClusterRolesScreen';
 import {
   getOCMRole,
@@ -9,37 +7,18 @@ import {
   getUserOidcConfigurations,
 } from '../../../../../redux/actions/rosaActions';
 
-const mapStateToProps = (state) => {
-  const { getOCMRoleResponse } = state.rosaReducer;
-  const valueSelector = formValueSelector('CreateCluster');
-  const isHypershiftSelected = valueSelector(state, 'hypershift') === 'true';
-  const sharedVpcSettings = valueSelector(state, 'shared_vpc');
-  const isSharedVpcSelected = sharedVpcSettings.is_selected;
-
-  let forcedByoOidcType;
-  if (isHypershiftSelected) {
-    forcedByoOidcType = 'Hypershift';
-  } else if (isSharedVpcSelected) {
-    forcedByoOidcType = 'SharedVPC';
-  }
-
-  return {
-    awsAccountID: valueSelector(state, 'associated_aws_id'),
-    clusterName: valueSelector(state, 'name'),
-    rosaCreationMode: valueSelector(state, 'rosa_roles_provider_creation_mode'),
-    customOperatorRolesPrefix: valueSelector(state, 'custom_operator_roles_prefix'),
-    byoOidcConfigID: valueSelector(state, 'byo_oidc_config_id'),
-    installerRoleArn: valueSelector(state, 'installer_role_arn'),
-    sharedVpcRoleArn: sharedVpcSettings?.hosted_zone_role_arn,
-    getOCMRoleResponse,
-    forcedByoOidcType,
-  };
-};
-
 const mapDispatchToProps = (dispatch) => ({
   getOCMRole: (awsAccountID) => dispatch(getOCMRole(awsAccountID)),
   clearGetOcmRoleResponse: () => dispatch(clearGetOcmRoleResponse()),
   getUserOidcConfigurations: (awsAccountID) => dispatch(getUserOidcConfigurations(awsAccountID)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(wizardConnector(ClusterRolesScreen));
+const mapStateToProps = (state) => {
+  const { getOCMRoleResponse } = state.rosaReducer;
+
+  return {
+    getOCMRoleResponse,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClusterRolesScreen);
