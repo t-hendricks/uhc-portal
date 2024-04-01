9227c34b4f763125f90162bb5815fee126153077
Beni Cherniavsky-Paskin <user@example.com>
2024-04-01 14:24:39 +0300

    chore: Apply eslint & prettier to more scripts
    - `yarn lint` now covers run/*js, deploy_info.mjs.
      - Avoiding recursive globs under run/ because people may have
        large repositories checked out under it (see Makefile).
    - Let linter understand `PATH/FILE.mjs` imports.
    - Allow more things in files that run in NodeJS, not browser.
      - Instead of no-extraneous-dependencies's special support for globs,
        use centralized "overrides" which allows many rules.
      - Allow console.log() and friends in scripts.
      - Allow __dirname, __filename.
      - "for await" is great in scripts, but hard to exclude it from
        "no-restricted-syntax" so added comment to each rules.
    - Fix/ignore remaining issues.

--- run/compress-git-log.mjs
+++ run/compress-git-log.mjs
@@ -1,22 +1,29 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-control-regex */
+
 import { realpathSync } from 'fs';
 import { fileURLToPath } from 'url';
 
-export const compressGitLog = text => {
+export const compressGitLog = (text) => {
+  /* eslint-disable no-param-reassign */
   // Move "See merge request ..." text onto end of previous text line.
   // Repeat this to "climb" across multiple lines
   // (which may be original in git log, or inserted by git --graph for drawing diagonals).
   let moved;
+  const handleMatch = (match, graph, mr) => {
+    moved = true;
+    return ` — See merge request ${mr}\n${graph}\n`;
+  };
+
   do {
     moved = false;
     // git --graph --color uses ANSI color escape sequences from e.g. ESC[1;31m or just ESC[m.
-    text = text.replace(/\n((?:[|\\/ ]|\x1B\[[0-9;]*m)*)(?: — )?See merge request ([^\n]*)\n/mg,
-                        (match, graph, mr) => {
-                          moved = true;
-                          return ` — See merge request ${mr}\n${graph}\n`;
-                        });
-  } while(moved);
+    text = text.replace(
+      /\n((?:[|\\/ ]|\x1B\[[0-9;]*m)*)(?: — )?See merge request ([^\n]*)\n/gm,
+      handleMatch,
+    );
+  } while (moved);
 
   // Condense the wording.
   text = text.replace(/See merge request /g, 'See MR ');
@@ -24,15 +31,17 @@ export const compressGitLog = text => {
   // Elide empty log lines (both original and left after moving) in git --graph safe way.
   // Must keep lines that contain graph diagonals `/` or `\`;
   // safe to remove if it contains only [colored] verticals `|` and/or spaces.
-  text = text.replace(/\n(?:[| ]|\x1B\[[0-9;]*m)*$/mg, '');
+  text = text.replace(/\n(?:[| ]|\x1B\[[0-9;]*m)*$/gm, '');
 
   return text;
-}
+  /* eslint-enable no-param-reassign */
+};
 
 if (realpathSync(process.argv[1]) === fileURLToPath(import.meta.url)) {
   // Running as script, process stdin -> stdout.
   // Line-by-line is weirdly painful in NodeJS, so slurp whole input.
   const chunks = [];
+  // eslint-disable-next-line no-restricted-syntax
   for await (const chunk of process.stdin) {
     chunks.push(chunk);
   }
