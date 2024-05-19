2292de9e8479cce72046f2927dc9d365d2c0813f
Enrique Mingorance Cano <user@example.com>
2024-05-19 06:39:20 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/components/clusters/ClusterDetails/components/SecurityGroups/SecurityGroupsDetailDisplay.test.tsx
+++ src/components/clusters/ClusterDetails/components/SecurityGroups/SecurityGroupsDetailDisplay.test.tsx
@@ -129,7 +129,7 @@ describe('<SecurityGroupsDetailDisplayByNode />', () => {
             <SecurityGroupsDisplayByNode
               securityGroups={securityGroups}
               securityGroupIdsForControl={securityGroupIdsForControl}
-              securityGroupIdsForInfra={emptySecurityGroups}
+              securityGroupIdsForInfra={[]}
             />
           </CompatRouter>
         </TestRouter>,
