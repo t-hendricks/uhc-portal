22caec8094b32b8f7190108a365bae863d9588f5
John Swanke <user@example.com>
2024-05-20 10:12:40 -0400

    fix lint
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- src/components/clusters/wizards/rosa_v2/ReviewClusterScreen/ReviewClusterScreen.jsx
+++ src/components/clusters/wizards/rosa_v2/ReviewClusterScreen/ReviewClusterScreen.jsx
@@ -7,8 +7,8 @@ import { Bullseye, Spinner, Stack, StackItem, Title } from '@patternfly/react-co
 
 import { hasExternalAuthenticationCapability } from '~/common/externalAuthHelper';
 import { hasSelectedSecurityGroups } from '~/common/securityGroupsHelpers';
-import { canAutoScaleOnCreateSelector } from '~/components/clusters/ClusterDetails/components/MachinePools/machinePoolsSelectors';
 import useOrganization from '~/components/CLILoginPage/useOrganization';
+import { canAutoScaleOnCreateSelector } from '~/components/clusters/ClusterDetails/components/MachinePools/machinePoolsSelectors';
 import { useFormState } from '~/components/clusters/wizards/hooks';
 import { canSelectImds } from '~/components/clusters/wizards/rosa/constants';
 import { getUserRoleForSelectedAWSAccount } from '~/components/clusters/wizards/rosa_v2/AccountsRolesScreen/AccountsRolesScreen';
