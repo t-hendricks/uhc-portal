5a2c0b62d451e901c66db001e1fc92db00506bd4
rhowingt@redhat.com <user@example.com>
2024-06-13 14:12:54 -0400

    init commit sans v2 wiz changes.  remove forcedbyo paths for sharedvpc
    Signed-off-by: rhowingt@redhat.com <rhowingt@redhat.com>

--- src/components/clusters/wizards/rosa/ClusterRolesScreen/clusterRolesHelper.ts
+++ src/components/clusters/wizards/rosa/ClusterRolesScreen/clusterRolesHelper.ts
@@ -4,16 +4,14 @@ import {
   MAX_CUSTOM_OPERATOR_ROLES_PREFIX_LENGTH,
 } from '~/common/validators';
 
-type ForcedByoOidcType = 'Hypershift' | 'SharedVPC' | undefined;
-
 const getOperatorRolesCommand = ({
-  forcedByoOidcType,
+  isHypershift,
   byoOidcConfigID,
   customOperatorRolesPrefix,
   installerRoleArn,
   sharedVpcRoleArn,
 }: {
-  forcedByoOidcType: ForcedByoOidcType;
+  isHypershift: boolean;
   byoOidcConfigID: string;
   customOperatorRolesPrefix: string;
   installerRoleArn?: string;
@@ -30,29 +28,10 @@ const getOperatorRolesCommand = ({
   const rosaBaseCommand = `rosa create operator-roles --prefix "${customOperatorRolesPrefix}" --oidc-config-id "${byoOidcConfigID}"`;
   const installerRoleArnOption = installerRoleArn ? `--installer-role-arn ${installerRoleArn}` : '';
 
-  if (forcedByoOidcType === 'Hypershift') {
+  if (isHypershift) {
     return `${rosaBaseCommand} --hosted-cp ${installerRoleArnOption}`;
   }
-  if (forcedByoOidcType === 'SharedVPC') {
-    return `${rosaBaseCommand} ${installerRoleArnOption} --shared-vpc-role-arn ${sharedVpcRoleArn}`;
-  }
   return `${rosaBaseCommand} ${installerRoleArnOption}`;
-};
-
-const getForcedByoOidcReason = (forcedByoOidcType: ForcedByoOidcType) => {
-  let mandatoryByoOidcReason;
-  switch (forcedByoOidcType) {
-    case 'Hypershift':
-      mandatoryByoOidcReason = 'Hosted control plane clusters require a specified OIDC provider.';
-      break;
-    case 'SharedVPC':
-      mandatoryByoOidcReason =
-        'When using Shared VPC, OIDC provider and operator roles must be created prior to the cluster creation.';
-      break;
-    default:
-      break;
-  }
-  return mandatoryByoOidcReason;
 };
 
 const OPERATOR_ROLES_HASH_LENGTH = 4;
@@ -77,4 +56,4 @@ const createOperatorRolesPrefix = (clusterName: string) => {
   return `${operatorRolesClusterName}-${createOperatorRolesHash()}`;
 };
 
-export { getOperatorRolesCommand, getForcedByoOidcReason, createOperatorRolesPrefix };
+export { getOperatorRolesCommand, createOperatorRolesPrefix };
