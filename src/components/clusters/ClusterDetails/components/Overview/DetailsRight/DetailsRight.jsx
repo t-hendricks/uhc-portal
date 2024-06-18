cafaef74c72dfe59b7dede340c9036764a705d98
eliran <user@example.com>
2024-06-18 03:48:47 +0300

    feat: actionable errors for cluster provision-status
    update the Cluster Details' Overview page to show doc-links in errors' contents (i.e. update current impl' at ClusterStatusMonitor.jsx and DetailsRight.jsx to use ClusterStatusErrorDisplay)

--- src/components/clusters/ClusterDetails/components/Overview/DetailsRight/DetailsRight.jsx
+++ src/components/clusters/ClusterDetails/components/Overview/DetailsRight/DetailsRight.jsx
@@ -18,6 +18,7 @@ import {
   isHypershiftCluster,
   isROSA,
 } from '~/components/clusters/common/clusterStates';
+import ClusterStatusErrorDisplay from '~/components/clusters/common/ClusterStatusErrorDisplay';
 import { useAWSVPCFromCluster } from '~/components/clusters/common/useAWSVPCFromCluster';
 import { IMDSType } from '~/components/clusters/wizards/common';
 import useCanClusterAutoscale from '~/hooks/useCanClusterAutoscale';
@@ -138,8 +139,7 @@ function DetailsRight({
                   <DescriptionListGroup>
                     <DescriptionListTerm>Details:</DescriptionListTerm>
                     <DescriptionListDescription>
-                      {cluster.status.provision_error_code}{' '}
-                      {cluster.status?.provision_error_message}
+                      <ClusterStatusErrorDisplay clusterStatus={cluster.status} showErrorCode />
                     </DescriptionListDescription>
                   </DescriptionListGroup>
                 </DescriptionList>
