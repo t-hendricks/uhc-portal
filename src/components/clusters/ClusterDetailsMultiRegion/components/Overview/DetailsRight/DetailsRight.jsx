8f19fa8a92a85baeb6db87a786414e5b083c7e95
eliran <user@example.com>
2024-06-20 02:23:24 +0300

    feat: actionable errors for cluster provision-status
    port changes over to multi-region copied directories:
    - copy the common component `ClusterStatusErrorDisplay` to `~components/clusters/commonMultiRegion`
    - update usages in `ClusterStatusMonitor` and `DetailsRight`, under `~components/clusters/ClusterDetailsMultiRegion`
    - adapt ported unit-tests to accommodate differing impl' (`ClusterStatusMonitor.test` uses specialized hooks for handling mocks, in its multi-region variation)

--- src/components/clusters/ClusterDetailsMultiRegion/components/Overview/DetailsRight/DetailsRight.jsx
+++ src/components/clusters/ClusterDetailsMultiRegion/components/Overview/DetailsRight/DetailsRight.jsx
@@ -19,6 +19,7 @@ import {
   isROSA,
 } from '~/components/clusters/common/clusterStates';
 import { useAWSVPCFromCluster } from '~/components/clusters/common/useAWSVPCFromCluster';
+import ClusterStatusErrorDisplay from '~/components/clusters/commonMultiRegion/ClusterStatusErrorDisplay';
 import { IMDSType } from '~/components/clusters/wizards/common';
 import useCanClusterAutoscale from '~/hooks/useCanClusterAutoscale';
 import { useGlobalState } from '~/redux/hooks';
@@ -154,8 +155,7 @@ function DetailsRight({ cluster, hasAutoscaleCluster, isDeprovisioned, clusterDe
                   <DescriptionListGroup>
                     <DescriptionListTerm>Details:</DescriptionListTerm>
                     <DescriptionListDescription>
-                      {cluster.status.provision_error_code}{' '}
-                      {cluster.status?.provision_error_message}
+                      <ClusterStatusErrorDisplay clusterStatus={cluster.status} showErrorCode />
                     </DescriptionListDescription>
                   </DescriptionListGroup>
                 </DescriptionList>
