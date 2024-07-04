5c5a35e05f2be6a59580dabc57882981bfd9f04e
Lior Keren <user@example.com>
2024-07-04 18:33:31 +0300

    (feat): Add correct icons

--- src/components/overview/Overview.tsx
+++ src/components/overview/Overview.tsx
@@ -18,6 +18,10 @@ import InternalTrackingLink from '~/components/common/InternalTrackingLink';
 
 import docLinks from '../../common/installLinks.mjs';
 import OpenShiftProductIcon from '../../styles/images/OpenShiftProductIcon.svg';
+import RedHatOpenShiftGitOpsIcon from '../../styles/images/RedHatOpenShiftGitOpsIcon.svg';
+import RedHatOpenShiftPipelinesIcon from '../../styles/images/RedHatOpenShiftPipelinesIcon.svg';
+import RedHatOpenShiftServiceMeshIcon from '../../styles/images/RedHatOpenShiftServiceMeshIcon.svg';
+import RedHatOperatorHubIcon from '../../styles/images/RedHatOperatorHubIcon.svg';
 import { AppPage } from '../App/AppPage';
 import {
   ListTextLabelLinkCard,
@@ -114,57 +118,50 @@ const icons = {
   }; */
   }
 
+  // export type recommendedLayeredServicesCards Props = {
+  //   icon?: React.ReactNode;
+  //   learnMoreLink?: React.ReactNode;
+  //   title?: string;
+  //   text?: string | React.ReactNode;
+  //   iconCardBodyClassName?: string;
+  //   breadcrumbs?: React.ReactNode;
+  //   dataTestId?: string;
+  // };
+
   // todo: This should be in the subcomponent: RecommendedLayeredServicesCardView
   // Could also be in a constants file
   const recommendedLayeredServicesCards = [
     {
       title: 'Red Hat OpenShift GitOps',
-      // todo: Does this should be a part of the Card? the '...' or is there some more description ?
       description:
         'Integrate git repositories, continuous integration/continuous delivery (CI/CD) tools, and Kubernetes.',
-      // todo: check which icon should I put here - check doc: https://docs.google.com/document/d/1F0aTywHu2kjJlgn6VUbeslWQ-AAuHBmK584ZjkbSAik/edit
-      // All product icons can be found here: https://redhat.brand-portal.adobe.com/mediaportal.html/content/dam/mac/redhat/brand
-      icon: 'some icon',
-      // todo: check which logo should I put here
-      logo: 'some logo',
+      icon: RedHatOpenShiftGitOpsIcon,
       labelText: 'Free',
       // todo: change link to the proper learn more link !
       learnMoreLink: 'https://prod.foo.redhat.com:1337/openshift/',
     },
     {
       title: 'Red Hat OpenShift Pipelines',
-      // todo: Does this should be a part of the Card? the '...' or is there some more description ?
       description:
         'Automate your application delivery using a continuous integration and continuous deployment (CI/CD) framework.',
-      // todo: check which icon should I put here
-      icon: 'some icon',
-      // todo: check which logo should I put here
-      logo: 'some logo',
+      icon: RedHatOpenShiftPipelinesIcon,
       labelText: 'Free',
       // todo: change link to the proper learn more link !
       learnMoreLink: 'https://prod.foo.redhat.com:1337/openshift/',
     },
     {
       title: 'Red Hat OpenShift Service Mesh',
-      // todo: Does this should be a part of the Card? the '...' or is there some more description ?
       description:
         'Connect, manage, and observe microservices-based applications in a uniform way.',
-      // todo: check which icon should I put here
-      icon: 'some icon',
-      // todo: check which logo should I put here
-      logo: 'some logo',
+      icon: RedHatOpenShiftServiceMeshIcon,
       labelText: 'Free',
       // todo: change link to the proper learn more link !
       learnMoreLink: 'https://prod.foo.redhat.com:1337/openshift/',
     },
     {
       title: 'OperatorHub',
-      // todo: Does this should be a part of the Card? the '...' or is there some more description ?
       description: 'Discover and install Kubernetes operators quickly and easily.',
-      // todo: check which icon should I put here
-      icon: 'some icon',
-      // todo: check which logo should I put here
-      logo: 'some logo',
+      icon: RedHatOperatorHubIcon,
       // todo: change link to the proper learn more link !
       learnMoreLink: 'https://prod.foo.redhat.com:1337/openshift/',
     },
@@ -228,32 +225,28 @@ const icons = {
           Recommended operators
         </Title>
         <Flex className="pf-v5-u-mb-lg">
-          {/* todo: This should be an array of cards.map -> every element should be a <Card /> wrapped in a <FlexItem /> */}
           {recommendedLayeredServicesCards.map(
-            ({ title, description, icon, logo, labelText, learnMoreLink }) => (
+            ({ title, description, icon, labelText, learnMoreLink }) => (
               <FlexItem className="pf-v5-u-pt-md" data-testid={`product-overview-card-${title}`}>
-                {/* prev implementation: <OfferingCard offeringType="RHOSD" /> */}
-                {/* todo: This should be a sub component - Product Overview Card */}
                 <Card className="product-overview-card">
                   <CardHeader>
                     <Split hasGutter style={{ width: '100%' }}>
-                      {/* todo: check how this is implemented in the original OfferingCard component */}
-                      {/* todo: Should this be with this logo ? OR should it be an icon -> like down */}
-                      <SplitItem>{logo}</SplitItem>
+                      <SplitItem>
+                        <img
+                          src={icon}
+                          alt={`${title} icon`}
+                          className="product-overview-card__icon"
+                        />
+                      </SplitItem>
                       <SplitItem isFilled />
                       <SplitItem>
                         {labelText ? (
                           <Label data-testtag="label" color="blue">
-                            {/* todo: check how this is implemented in the original OfferingCard component */}
-                            {
-                              labelText /* should be free for all cards, but I should specify it in the array of card properties to make it more generic */
-                            }
+                            {labelText}
                           </Label>
                         ) : undefined}
                       </SplitItem>
                     </Split>
-                    {/* todo: check what is the output on the card -> this should be the appropriate icon for each card */}
-                    <img src={icon} alt={`${title} icon`} style={{ maxWidth: '60px' }} />
                   </CardHeader>
 
                   <CardTitle>
@@ -262,9 +255,7 @@ const icons = {
                   </CardTitle>
                   <CardBody>{description}</CardBody>
                   <CardFooter>
-                    {/* todo: Icon should be changed to a different one !! 
-                    check doc: https://docs.google.com/document/d/1F0aTywHu2kjJlgn6VUbeslWQ-AAuHBmK584ZjkbSAik/edit   
-                    Check Prototype: https://www.figma.com/proto/5AOQ544g8dSoV4eOwxnsIg/Increasing-Operator-Visibility-v1-02?page-id=0%3A1&node-id=0-3089&viewport=2384%2C-1017%2C1&t=bkRn1QfIbSrDiVh4-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=0%3A3089 */}
+                    {/* todo: change to drawer icon */}
                     {learnMoreLink ? (
                       <ExternalLink href={learnMoreLink}>Learn more</ExternalLink>
                     ) : undefined}
