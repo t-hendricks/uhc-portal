b2c493471b98e74c54106e96f577eb20695be974
Jayakrishnan Mekkattillam <user@example.com>
2024-07-11 16:51:57 +0530

    [OCMUI-2028,OCMUI-2033,OCMUI-2019]ROSA hosted detailed test case definition with supported changes.

---
+++ cypress/e2e/rosa-hosted/RosaHostedClusterPublicOverview.js
@@ -0,0 +1,87 @@
+import ClusterListPage from '../../pageobjects/ClusterList.page';
+import ClusterDetailsPage from '../../pageobjects/ClusterDetails.page';
+const clusterDetails = require('../../fixtures/rosa-hosted/RosaHostedClusterCreatePublic.json');
+const clusterProfiles = ['rosa-hosted-public', 'rosa-hosted-public-advanced'];
+const awsAccountID = Cypress.env('QE_AWS_ID');
+
+describe(
+  'Rosa hosted cluster (hypershift) -create public cluster Overview properties',
+  { tags: ['day2', 'hosted', 'rosa', 'public', 'hcp'] },
+  () => {
+    beforeEach(() => {
+      if (Cypress.currentTest.title.match(/Open.*cluster/g)) {
+        cy.visit('/');
+        ClusterListPage.waitForDataReady();
+      }
+    });
+    // Iterate via all the available public cluster profiles.
+    clusterProfiles.forEach((clusterProfile) => {
+      let clusterProperties = clusterDetails[clusterProfile]['day1-profile'];
+      let clusterName = clusterProperties.ClusterName;
+
+      it(`Open ${clusterName} cluster`, () => {
+        ClusterListPage.filterTxtField().should('be.visible').click();
+        ClusterListPage.filterTxtField().clear().type(clusterName);
+        ClusterListPage.waitForDataReady();
+        ClusterListPage.openClusterDefinition(clusterName);
+      });
+
+      it(`Checks on overview tab : ${clusterName} cluster`, () => {
+        ClusterDetailsPage.waitForInstallerScreenToLoad();
+        ClusterDetailsPage.clusterNameTitle().contains(clusterName);
+        ClusterDetailsPage.clusterTypeLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.Type);
+        ClusterDetailsPage.clusterDomainPrefixLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.DomainPrefix);
+        ClusterDetailsPage.clusterControlPlaneTypeLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.ControlPlaneType);
+        ClusterDetailsPage.clusterRegionLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.Region.split(',')[0]);
+        ClusterDetailsPage.clusterAvailabilityLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.Availability);
+        ClusterDetailsPage.clusterInfrastructureAWSaccountLabelValue()
+          .scrollIntoView()
+          .contains(awsAccountID);
+        ClusterDetailsPage.clusterBillingMarketplaceAccountLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.AWSBillingAccountId);
+
+        if (clusterProperties.MachinePools.Autoscaling.includes('Enabled')) {
+          cy.contains(
+            `Min: ${clusterProperties.MachinePools.MachinePoolCount * parseInt(clusterProperties.MachinePools.MiniNodeCount)}`,
+          );
+          cy.contains(
+            `Max: ${clusterProperties.MachinePools.MachinePoolCount * parseInt(clusterProperties.MachinePools.MaxNodeCount)}`,
+          );
+        } else {
+          ClusterDetailsPage.clusterComputeNodeCountValue()
+            .scrollIntoView()
+            .contains(
+              clusterProperties.MachinePools.MachinePoolCount *
+                parseInt(clusterProperties.MachinePools.NodeCount),
+            );
+        }
+
+        ClusterDetailsPage.clusterMachineCIDRLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.MachineCIDR);
+        ClusterDetailsPage.clusterServiceCIDRLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.ServiceCIDR);
+        ClusterDetailsPage.clusterPodCIDRLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.PodCIDR);
+        ClusterDetailsPage.clusterHostPrefixLabelValue()
+          .scrollIntoView()
+          .contains(clusterProperties.HostPrefix.replace('/', ''));
+        cy.contains('Type:').next().contains(clusterProperties.OidcConfigType);
+        cy.contains('ID:').next().contains(clusterProperties.OidcConfigId);
+      });
+    });
+  },
+);
