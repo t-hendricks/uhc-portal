a32cc74048f9a5049033f8997f9a0b55c0e8f13c
Roberto Emanuel <user@example.com>
2024-08-01 07:39:46 +0000

    Merge branch 'OCMUI-2082' into 'master'
    OCMUI-2082 [OSD-GCP] Change "Production Support" to "Enhanced Support" in the OSD create cluster wizard - GCP prerequisites
    
    Closes OCMUI-2082
    
    See merge request service/uhc-portal!5810
    
    (cherry picked from commit de17a0cbe0f1daa60f1afe4a03cbaa8b99a7e379)

--- src/components/clusters/wizards/osd/ClusterSettings/CloudProvider/GcpByocFields.tsx
+++ src/components/clusters/wizards/osd/ClusterSettings/CloudProvider/GcpByocFields.tsx
@@ -158,7 +158,7 @@ export const GcpByocFields = () => {
               </ul>
 
               <Text component={TextVariants.p} className="ocm-secondary-text">
-                Production Support from GCP is also recommended. To prevent potential conflicts, we
+                Enhanced Support from GCP is also recommended. To prevent potential conflicts, we
                 recommend that you have no other resources provisioned in the project prior to
                 provisioning OpenShift Dedicated. For more guidance, see the{' '}
                 <ExternalLink noIcon href={links.OSD_CCS_GCP}>
