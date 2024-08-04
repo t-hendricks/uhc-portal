bad450dc6536e3ced537a16fae540257c040ae99
Lior Keren <user@example.com>
2024-08-04 08:00:16 +0000

    Merge branch 'remove-hcp-rosa-getting-started-page-feature-flag' into 'master'
    OCMUI-2035: Remove feature flag 'hcp-rosa-getting-started-page' and Remove Un-relevant Components
    
    See merge request service/uhc-portal!5816
    
    (cherry picked from commit b95809ef7a13990aa8477f9b958406c1750b61ff)

--- src/components/clusters/wizards/rosa/CreateRosaGetStarted/CreateRosaGetStarted.test.tsx
+++ src/components/clusters/wizards/rosa/CreateRosaGetStarted/CreateRosaGetStarted.test.tsx
@@ -2,23 +2,12 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { CompatRouter } from 'react-router-dom-v5-compat';
 
-import { HCP_ROSA_GETTING_STARTED_PAGE } from '~/redux/constants/featureConstants';
-import {
-  checkAccessibility,
-  mockRestrictedEnv,
-  mockUseChrome,
-  mockUseFeatureGate,
-  render,
-  screen,
-  waitFor,
-} from '~/testUtils';
+import { checkAccessibility, mockUseChrome, render, screen, waitFor } from '~/testUtils';
 
 import CreateRosaGetStarted from './CreateRosaGetStarted';
 
 mockUseChrome();
 
-const hypershiftMessage =
-  /For now, you can only create ROSA with Hosted Control Plane clusters using the CLI/;
 const completeAWSMessage = /complete aws prerequisites/i;
 
 describe('<CreateRosaGetStarted />', () => {
@@ -32,41 +21,6 @@ describe('<CreateRosaGetStarted />', () => {
       </MemoryRouter>,
     );
     await checkAccessibility(container);
-  });
-
-  it('shows hypershift info alert if feature flag is enabled and is accessible', async () => {
-    // Arrange
-    mockUseFeatureGate([[HCP_ROSA_GETTING_STARTED_PAGE, true]]);
-
-    render(
-      <MemoryRouter>
-        <CompatRouter>
-          <CreateRosaGetStarted />
-        </CompatRouter>
-      </MemoryRouter>,
-    );
-
-    // Assert
-    // There is no natural role for this message
-    expect(await screen.findByText(hypershiftMessage)).toBeInTheDocument();
-  });
-
-  it('hides hypershift info alert if feature flag is not enabled', async () => {
-    // Arrange
-    mockUseFeatureGate([[HCP_ROSA_GETTING_STARTED_PAGE, false]]);
-
-    render(
-      <MemoryRouter>
-        <CompatRouter>
-          <CreateRosaGetStarted />
-        </CompatRouter>
-      </MemoryRouter>,
-    );
-    // Assert
-    // There is no natural role for this message
-    await waitFor(() => {
-      expect(screen.queryByText(hypershiftMessage)).not.toBeInTheDocument();
-    });
   });
 
   it('navigated to quick start from aws setup', async () => {
@@ -98,40 +52,6 @@ describe('<CreateRosaGetStarted />', () => {
       expect(
         screen.getByRole('button', { name: completeAWSMessage }).querySelector('svg.warning'),
       ).toBeInTheDocument();
-    });
-  });
-
-  describe('in Restricted env', () => {
-    const isRestrictedEnv = mockRestrictedEnv();
-
-    afterEach(() => {
-      isRestrictedEnv.mockReturnValue(false);
-    });
-
-    it('does not show HCP directions', async () => {
-      mockUseFeatureGate([[HCP_ROSA_GETTING_STARTED_PAGE, true]]);
-      const { rerender } = render(
-        <MemoryRouter>
-          <CompatRouter>
-            <CreateRosaGetStarted />
-          </CompatRouter>
-        </MemoryRouter>,
-      );
-
-      expect(await screen.findByTestId('hcp-directions')).toBeInTheDocument();
-
-      isRestrictedEnv.mockReturnValue(true);
-      rerender(
-        <MemoryRouter>
-          <CompatRouter>
-            <CreateRosaGetStarted />
-          </CompatRouter>
-        </MemoryRouter>,
-      );
-
-      await waitFor(() => {
-        expect(screen.queryByTestId('hcp-directions')).not.toBeInTheDocument();
-      });
     });
   });
 });
