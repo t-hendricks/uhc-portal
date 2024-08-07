32d524d19759d3ebd911013a5003073b76de453c
Lior Keren <user@example.com>
2024-08-07 15:37:35 +0300

    (test): Add test to check each Card data seperately to keep functionality test (Drawer content switch) more simple
    + Change test name and add a few checks on general things that should be present on the page

--- src/components/overview/Overview.test.jsx
+++ src/components/overview/Overview.test.jsx
@@ -10,7 +10,7 @@ import Overview from './Overview';
 import '@testing-library/jest-dom';
 
 describe('<Overview />', () => {
-  it('contains correct links', async () => {
+  it('contains a few elements of the page', async () => {
     const { container } = render(
       <BrowserRouter>
         <CompatRouter>
@@ -18,6 +18,10 @@ describe('<Overview />', () => {
         </CompatRouter>
       </BrowserRouter>,
     );
+
+    // Featured OpenShift cluster types:
+
+    expect(screen.getByText('Featured OpenShift cluster types')).toBeInTheDocument();
 
     expect(
       screen.getByText('View all OpenShift cluster types', {
@@ -25,11 +29,95 @@ describe('<Overview />', () => {
       }),
     ).toHaveAttribute('href', '/create');
 
+    // Recommended Operator Cards:
+
+    expect(screen.getByText('Recommended operators')).toBeInTheDocument();
+
+    expect(
+      screen.getByText('View all in Ecosystem Catalog', {
+        selector: 'a',
+      }),
+    ).toHaveAttribute(
+      'href',
+      'https://catalog.redhat.com/search?searchType=software&deployed_as=Operator',
+    );
+
     await checkAccessibility(container);
   });
 
-  it('tests Recommended Operators section Functionality', async () => {
-    const { container } = render(
+  it.each([
+    {
+      ...RECOMMENDED_OPERATORS_CARDS_DATA[0],
+      index: 0,
+      someDrawerContent:
+        'Consistently configure and deploy Kubernetes-based infrastructure and applications across clusters and development lifecycles using Red Hat OpenShift GitOps.',
+      learnMoreLinkDestination:
+        'https://catalog.redhat.com/software/container-stacks/detail/5fb288c70a12d20cbecc6056',
+    },
+    {
+      ...RECOMMENDED_OPERATORS_CARDS_DATA[1],
+      index: 1,
+      someDrawerContent:
+        'Speed up the delivery of your applications with advanced continuous integration (CI) workflows and automation.',
+      learnMoreLinkDestination:
+        'https://catalog.redhat.com/software/container-stacks/detail/5ec54a4628834587a6b85ca5',
+    },
+    {
+      ...RECOMMENDED_OPERATORS_CARDS_DATA[2],
+      index: 2,
+      someDrawerContent:
+        'Red Hat OpenShift Service Mesh adds tracing and visualization so you have a greater understanding of what is happening in and across applications as they are running, from start to finish.',
+      learnMoreLinkDestination:
+        'https://catalog.redhat.com/software/container-stacks/detail/5ec53e8c110f56bd24f2ddc4',
+    },
+  ])(
+    'verifies Recommended Operators Card "$title" content (basic functionality verification of each Card separately)',
+    async ({ title, description, index, someDrawerContent, learnMoreLinkDestination }) => {
+      render(
+        <BrowserRouter>
+          <CompatRouter>
+            <Overview />
+          </CompatRouter>
+        </BrowserRouter>,
+      );
+
+      // ensure Cards's description is shown:
+      expect(screen.getByText(description)).toBeInTheDocument();
+
+      // ensure the content of the Card's Drawers is not shown:
+      expect(screen.queryByText(someDrawerContent)).not.toBeInTheDocument();
+
+      // Basic functionality checks:
+
+      const learnMoreBtns = screen.getAllByTestId('product-overview-card__learn-more-button');
+      expect(learnMoreBtns).toHaveLength(3);
+
+      // click on Card's Learn more button
+      await userEvent.click(learnMoreBtns[index]);
+
+      // ensure the title and some content is shown
+      expect(screen.getByTestId('drawer-panel-content__title')).toHaveTextContent(title);
+      expect(screen.getByText('by Red Hat')).toBeInTheDocument();
+      expect(screen.getByText(someDrawerContent)).toBeInTheDocument();
+
+      // Learn more link
+      const learnMoreLink = screen.getByText(`Learn more about ${title}`);
+      expect(learnMoreLink).toBeInTheDocument();
+      expect(learnMoreLink).toHaveAttribute('href', learnMoreLinkDestination);
+
+      // click on close Drawer button:
+      const closeDrawerBtn = screen.getByTestId('drawer-close-button');
+      expect(closeDrawerBtn).toBeInTheDocument();
+      await userEvent.click(closeDrawerBtn);
+
+      // ensure some of the previously shown content of the Drawer is no longer shown:
+      expect(screen.queryByText(someDrawerContent)).not.toBeInTheDocument();
+      expect(screen.queryByText('by Red Hat')).not.toBeInTheDocument();
+    },
+  );
+
+  it('tests Recommended Operators Cards Functionality -> Click on the learnMore of each card and verify that the content of the Drawer switches to the appropriate card that was clicked', async () => {
+    render(
       <BrowserRouter>
         <CompatRouter>
           <Overview />
@@ -37,60 +125,25 @@ describe('<Overview />', () => {
       </BrowserRouter>,
     );
 
-    // Ensure some elements of the section is presented on the screen
-    // title:
-    expect(screen.getByText('Recommended operators')).toBeInTheDocument();
-
-    // ensure Cards's description is shown:
-    const gitopsCardData = RECOMMENDED_OPERATORS_CARDS_DATA[0];
-    const pipelinesCardData = RECOMMENDED_OPERATORS_CARDS_DATA[1];
-    const serviceMeshCardData = RECOMMENDED_OPERATORS_CARDS_DATA[2];
-
-    expect(screen.getByText(gitopsCardData.description)).toBeInTheDocument();
-    expect(screen.getByText(pipelinesCardData.description)).toBeInTheDocument();
-    expect(screen.getByText(serviceMeshCardData.description)).toBeInTheDocument();
-
-    // ensure the content of the Card's Drawers is not shown
-    expect(
-      screen.queryByText(
-        'Consistently configure and deploy Kubernetes-based infrastructure and applications across clusters and development lifecycles using Red Hat OpenShift GitOps.',
-      ),
-    ).not.toBeInTheDocument();
-    expect(
-      screen.queryByText(
-        'Speed up the delivery of your applications with advanced continuous integration (CI) workflows and automation.',
-      ),
-    ).not.toBeInTheDocument();
-    expect(
-      screen.queryByText('Red Hat OpenShift Service Mesh is based on the open source'),
-    ).not.toBeInTheDocument();
-
-    // Functionality checks:
+    const gitopsCardData = { ...RECOMMENDED_OPERATORS_CARDS_DATA[0] };
+    const pipelinesCardData = { ...RECOMMENDED_OPERATORS_CARDS_DATA[1] };
+    const serviceMeshCardData = { ...RECOMMENDED_OPERATORS_CARDS_DATA[2] };
 
     const learnMoreBtns = screen.getAllByTestId('product-overview-card__learn-more-button');
     expect(learnMoreBtns).toHaveLength(3);
 
-    // click on Gitops's Learn more button
+    // click on Gitops card's Learn more button
     await userEvent.click(learnMoreBtns[0]);
 
-    // ensure the title and some content is shown
+    // ensure the title and some drawer content is shown
     expect(screen.getByTestId('drawer-panel-content__title')).toHaveTextContent(
       gitopsCardData.title,
     );
-    expect(screen.getByText('by Red Hat')).toBeInTheDocument();
     expect(
       screen.getByText(
         'Consistently configure and deploy Kubernetes-based infrastructure and applications across clusters and development lifecycles using Red Hat OpenShift GitOps.',
       ),
     ).toBeInTheDocument();
-
-    // Learn more link
-    const gitopsLearnMoreLink = screen.getByText('Learn more about Red Hat OpenShift GitOps');
-    expect(gitopsLearnMoreLink).toBeInTheDocument();
-    expect(gitopsLearnMoreLink).toHaveAttribute(
-      'href',
-      'https://catalog.redhat.com/software/container-stacks/detail/5fb288c70a12d20cbecc6056',
-    );
 
     // click on close Drawer button:
     const closeDrawerBtn = screen.getByTestId('drawer-close-button');
@@ -103,54 +156,31 @@ describe('<Overview />', () => {
         'Consistently configure and deploy Kubernetes-based infrastructure and applications across clusters and development lifecycles using Red Hat OpenShift GitOps.',
       ),
     ).not.toBeInTheDocument();
-    expect(screen.queryByText('by Red Hat')).not.toBeInTheDocument();
 
-    // click on Pipelines's Learn more button
+    // click on Pipelines card's Learn more button
     await userEvent.click(learnMoreBtns[1]);
 
-    // ensure the title and some content is shown
+    // ensure the title and some drawer content is shown
     expect(screen.getByTestId('drawer-panel-content__title')).toHaveTextContent(
       pipelinesCardData.title,
     );
-    expect(screen.getByText('by Red Hat')).toBeInTheDocument();
     expect(
       screen.getByText(
         'Speed up the delivery of your applications with advanced continuous integration (CI) workflows and automation.',
       ),
     ).toBeInTheDocument();
 
-    // Learn more link
-    const pipelinesLearnMoreLink = screen.getByText('Learn more about Red Hat OpenShift Pipelines');
-    expect(pipelinesLearnMoreLink).toBeInTheDocument();
-    expect(pipelinesLearnMoreLink).toHaveAttribute(
-      'href',
-      'https://catalog.redhat.com/software/container-stacks/detail/5ec54a4628834587a6b85ca5',
-    );
-
-    // click on Service Mesh's Learn more button
+    // click on Service Mesh card's Learn more button
     await userEvent.click(learnMoreBtns[2]);
 
-    // ensure the title and some content is shown
+    // ensure the title and some drawer content is shown
     expect(screen.getByTestId('drawer-panel-content__title')).toHaveTextContent(
       serviceMeshCardData.title,
     );
-    expect(screen.getByText('by Red Hat')).toBeInTheDocument();
     expect(
       screen.getByText(
         'Red Hat OpenShift Service Mesh adds tracing and visualization so you have a greater understanding of what is happening in and across applications as they are running, from start to finish.',
       ),
     ).toBeInTheDocument();
-
-    // Learn more link
-    const serviceMeshLearnMoreLink = screen.getByText(
-      'Learn more about Red Hat OpenShift Service Mesh',
-    );
-    expect(serviceMeshLearnMoreLink).toBeInTheDocument();
-    expect(serviceMeshLearnMoreLink).toHaveAttribute(
-      'href',
-      'https://catalog.redhat.com/software/container-stacks/detail/5ec53e8c110f56bd24f2ddc4',
-    );
-
-    await checkAccessibility(container);
   });
 });
