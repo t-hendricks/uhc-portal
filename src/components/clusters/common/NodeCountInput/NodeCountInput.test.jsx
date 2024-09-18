2108718302b70c813b65d5c8a4b9d941c2d96309
Lior Keren <user@example.com>
2024-09-18 13:35:08 +0000

    Merge branch 'add-comment-of-the-reason-behind-the-skipped-test' into 'master'
    (chore): Add a comment with the reason behind this skipped test
    
    See merge request service/uhc-portal!5958
    
    (cherry picked from commit 1aa4986537f9da7ff05cbf1ac79af2cc38dda8ef)

--- src/components/clusters/common/NodeCountInput/NodeCountInput.test.jsx
+++ src/components/clusters/common/NodeCountInput/NodeCountInput.test.jsx
@@ -452,6 +452,7 @@ describe('<NodeCountInput>', () => {
         expect(onChange).toBeCalledWith(minNodes); // Returns min value of 2 vs 1 node * 1 pool (which would fail cluster creation)
       });
 
+      // todo: we should investigate the cause for this test's failure - https://issues.redhat.com/browse/OCMUI-2379
       it.skip('sends onchange with minimum nodes when the number the user picked * number of new pools is greater than max nodes', () => {
         const maxNodes = MAX_NODES_HCP;
         const maxNodesForThreePools = MAX_NODES_HCP / 3;
