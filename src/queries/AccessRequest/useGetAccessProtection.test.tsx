d938448499534866812b6b3b5503bc84b77eabb2
Kim Doberstein <user@example.com>
2024-10-29 14:04:41 +0000

    Merge branch 'OCMUI-1911-Ensure-FedRAMP-logic-in-new-cluster-list' into 'master'
    OCMUI-1911 Ensure FedRAMP logic is in new cluster list
    
    Closes OCMUI-1911
    
    See merge request service/uhc-portal!6061
    
    (cherry picked from commit 4f27321830c1bd5337ea71d8a461cab26b3c6c7a)

--- src/queries/AccessRequest/useGetAccessProtection.test.tsx
+++ src/queries/AccessRequest/useGetAccessProtection.test.tsx
@@ -62,6 +62,31 @@ describe('useGetAccessProtection', () => {
     expect(apiRequestMock.get).not.toHaveBeenCalled();
   });
 
+  it('does not make an API call if restricted environment', async () => {
+    apiRequestMock.get.mockResolvedValueOnce({ data: apiResponse });
+    const isRestrictedEnv = true;
+
+    const { result } = renderHook(() =>
+      useGetAccessProtection(
+        {
+          subscriptionId: 'mySubscriptionId',
+          organizationId: 'myOrgId',
+          clusterId: 'myClusterId',
+        },
+        isRestrictedEnv,
+      ),
+    );
+    await waitFor(() => {
+      expect(result.current.isLoading).not.toBeUndefined();
+    });
+
+    expect(result.current.isFetched).toBeFalsy();
+    expect(result.current.enabled).toBeUndefined();
+    expect(result.current.isPending).toBeTruthy();
+
+    expect(apiRequestMock.get).not.toHaveBeenCalled();
+  });
+
   it('makes api call with only subscription id', async () => {
     apiRequestMock.get.mockResolvedValueOnce({ data: apiResponse });
 
