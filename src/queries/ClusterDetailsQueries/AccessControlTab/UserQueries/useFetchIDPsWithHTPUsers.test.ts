0874b28fccde22b757e05075f6036b706aa04d90
Kim Doberstein <user@example.com>
2025-03-06 21:21:22 -0600

    OCMUI-2965 - View htpasswrd users when viewing IDPs  (rebased) (#233)
    Co-authored-by: Dylan Cooper <dcooper@redhat.com>

---
+++ src/queries/ClusterDetailsQueries/AccessControlTab/UserQueries/useFetchIDPsWithHTPUsers.test.ts
@@ -0,0 +1,141 @@
+import { formatErrorData } from '~/queries/helpers';
+import { renderHook, waitFor } from '~/testUtils';
+
+import clusterService, { getClusterServiceForRegion } from '../../../../services/clusterService';
+
+import { useFetchIDPsWithHTPUsers } from './useFetchIDPsWithHTPUsers';
+
+jest.mock('../../../../services/clusterService', () => ({
+  __esModule: true,
+  default: {
+    getIdentityProviders: jest.fn(),
+  },
+}));
+
+jest.mock('../../../../services/clusterService', () => ({
+  __esModule: true,
+  default: {
+    getHtpasswdUsers: jest.fn(),
+  },
+}));
+
+jest.mock('../../../../services/clusterService', () => ({
+  getClusterServiceForRegion: jest.fn(),
+}));
+
+const clusterIDP = {
+  kind: 'IdentityProviderList',
+  page: 1,
+  size: 1,
+  total: 1,
+  items: [
+    {
+      kind: 'IdentityProvider',
+      type: 'HTPasswdIdentityProvider',
+      id: 'idpId1',
+      name: 'mockedIDPName',
+      mapping_method: 'claim',
+    },
+  ],
+};
+
+const htpUsers = {
+  items: [
+    {
+      kind: 'HTPasswdUser',
+      id: 'userId2',
+      username: 'user2',
+    },
+    {
+      kind: 'HTPasswdUser',
+      id: 'userId1',
+      username: 'user1',
+    },
+  ],
+};
+
+const idpsWithHTPasswdUsers = [
+  {
+    kind: 'IdentityProvider',
+    type: 'HTPasswdIdentityProvider',
+    id: 'idpId1',
+    name: 'mockedIDPName',
+    mapping_method: 'claim',
+    htpUsers: [
+      {
+        kind: 'HTPasswdUser',
+        id: 'userId1',
+        username: 'user1',
+      },
+      {
+        kind: 'HTPasswdUser',
+        id: 'userId2',
+        username: 'user2',
+      },
+    ],
+  },
+];
+
+describe('useFetchIDPsWithHTPUsers', () => {
+  const mockGetIdentityProviders = jest.fn();
+  const mockGetHtpasswdUsers = jest.fn();
+  const mockedIDPsWithHTPUsers = jest.fn();
+  const mockGetClusterServiceForRegion = getClusterServiceForRegion as jest.Mock;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    clusterService.getIdentityProviders = mockGetIdentityProviders;
+    clusterService.getHtpasswdUsers = mockGetHtpasswdUsers;
+    mockGetClusterServiceForRegion.mockReturnValue({
+      getIdentityProviders: mockGetIdentityProviders,
+      getHtpasswdUsers: mockGetHtpasswdUsers,
+    });
+  });
+
+  it('returns idps and users from apis', async () => {
+    mockGetIdentityProviders.mockResolvedValueOnce({ data: clusterIDP });
+    mockGetHtpasswdUsers.mockResolvedValueOnce({ data: htpUsers });
+
+    mockedIDPsWithHTPUsers.mockResolvedValue({ data: idpsWithHTPasswdUsers });
+
+    const { result } = renderHook(() => useFetchIDPsWithHTPUsers('myClusterId'));
+    await waitFor(() => expect(result.current.isLoading).toBeTruthy());
+    await waitFor(() => expect(result.current.isLoading).toBeFalsy());
+    expect(mockGetClusterServiceForRegion).toHaveBeenCalledWith(undefined);
+    const { data, isError } = result.current;
+
+    expect(isError).toBeFalsy();
+    expect(data).toEqual(idpsWithHTPasswdUsers);
+    expect(isError).toBeFalsy();
+  });
+
+  it.skip('displays error correctly', async () => {
+    const clusterID = 'test-cluster';
+    const mockError = new Error('test error');
+    const mockFormattedError = formatErrorData as jest.Mock;
+
+    mockGetIdentityProviders.mockResolvedValueOnce({ data: clusterIDP });
+
+    mockGetHtpasswdUsers.mockRejectedValueOnce(mockError);
+    mockedIDPsWithHTPUsers.mockResolvedValue(mockError);
+    mockFormattedError.mockReturnValueOnce({ message: 'formatted error' });
+
+    const { result } = renderHook(() => useFetchIDPsWithHTPUsers(clusterID));
+
+    await waitFor(() => {
+      expect(result.current.isError).toBe(true);
+    });
+
+    expect(result.current.error).toEqual({ message: 'formatted error' });
+
+    expect(mockFormattedError).toHaveBeenCalledWith(
+      result.current.isPending,
+      result.current.isError,
+      mockError,
+    );
+  });
+});
---
+++ src/queries/ClusterDetailsQueries/AccessControlTab/UserQueries/useFetchIDPsWithHTPUsers.ts
@@ -0,0 +1,83 @@
+import get from 'lodash/get';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { queryClient } from '~/components/App/queryClient';
+import { formatErrorData, getHtpasswdIds } from '~/queries/helpers';
+import { HTPasswdIdpUsers, HTPasswdUser } from '~/queries/types';
+import { getClusterServiceForRegion } from '~/services/clusterService';
+import { IdentityProvider } from '~/types/clusters_mgmt.v1';
+
+const usersErrors: any = [];
+
+export const useFetchIDPsWithHTPUsers = (clusterID: string, region?: string) => {
+  const { isLoading, data, isError, error, isSuccess } = useQuery({
+    queryKey: ['fetchIDPsWithHTPUsers', clusterID, region],
+    queryFn: async () => {
+      const clusterService = getClusterServiceForRegion(region);
+      const response = clusterService.getIdentityProviders(clusterID).then(async (res) => {
+        const idpItems = get(res.data, 'items', []);
+
+        const htpUsers: HTPasswdIdpUsers[] = [];
+
+        const htPasswdIds = getHtpasswdIds(idpItems);
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const htpId of htPasswdIds) {
+          try {
+            // eslint-disable-next-line no-await-in-loop
+            const response: any = await clusterService.getHtpasswdUsers(clusterID, htpId);
+
+            const users: HTPasswdUser[] = response?.data?.items;
+
+            htpUsers.push({ idpId: htpId, htpUsers: users });
+          } catch (error) {
+            usersErrors.push({ idpId: htpId, error });
+          }
+        }
+
+        const mergeHtpUsersToIdp = (idps: IdentityProvider[], htpUsersData: HTPasswdIdpUsers[]) => {
+          const mergedIdps = new Map();
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const idp of idps) {
+            mergedIdps.set(idp.id, { ...idp });
+          }
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const item of htpUsersData) {
+            if (mergedIdps.has(item.idpId)) {
+              const existingIdp = mergedIdps.get(item.idpId);
+              const orderedUsers = item.htpUsers?.reverse();
+              mergedIdps.set(item.idpId, { ...existingIdp, htpUsers: orderedUsers });
+            }
+          }
+          return Array.from(mergedIdps.values());
+        };
+
+        const mergedIdps = mergeHtpUsersToIdp(idpItems, htpUsers);
+
+        return mergedIdps;
+      });
+
+      return response;
+    },
+    enabled: !!clusterID,
+  });
+
+  const formattedError = formatErrorData(isLoading, isError, error);
+
+  return {
+    data,
+    isLoading,
+    isError,
+    error: formattedError,
+    isSuccess,
+  };
+};
+
+export const refetchIdentityProvidersWithHTPUsers = (clusterID: string, region?: string) => {
+  queryClient.invalidateQueries({
+    queryKey: ['fetchIDPsWithHTPUsers', clusterID, region],
+  });
+};
