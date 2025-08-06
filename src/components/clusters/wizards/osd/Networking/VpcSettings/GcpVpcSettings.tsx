7bd03bf7ecba7b4ca348d46d2315eeb6abf9808e
meirinetta <nmeiri@redhat.com>
2025-08-06 13:21:59 +0300

    OCMUI-3480: Fix 'existing VPC' tooltip text in GCP wizard (#15)

--- src/components/clusters/wizards/osd/Networking/VpcSettings/GcpVpcSettings.tsx
+++ src/components/clusters/wizards/osd/Networking/VpcSettings/GcpVpcSettings.tsx
@@ -1,7 +1,15 @@
 import React, { ReactElement, useMemo } from 'react';
 import { Field } from 'formik';
 
-import { Alert, AlertActionLink, GridItem, Title, useWizardContext } from '@patternfly/react-core';
+import {
+  Alert,
+  AlertActionLink,
+  Content,
+  ContentVariants,
+  GridItem,
+  Title,
+  useWizardContext,
+} from '@patternfly/react-core';
 
 import links from '~/common/installLinks.mjs';
 import { required, validateGCPHostProjectId, validateGCPSubnet } from '~/common/validators';
@@ -112,9 +120,12 @@ export const GcpVpcSettings = () => {
             iconClassName="pf-v6-u-ml-sm"
             hint={
               <>
-                Install into a non-default subnet shared by another account in your CP organization
+                <Content component={ContentVariants.p}>
+                  Install into a user-defined subnet within a custom VPC network that is provisioned
+                  and fully managed within the same GCP project.
+                </Content>
                 <ExternalLink href={links.INSTALL_GCP_VPC}>
-                  Learn more about GCP shared VPC
+                  Learn more about installing into an existing VPC
                 </ExternalLink>
               </>
             }
