5f3df8109431bb43231ac302626da3f7275dffad
Trevor <thendric@redhat.com>
2026-09-10 11:25:24 -0400

    Update code-guide.md for coderabbitai test scenario

--- src/components/releases/ReleaseChannelName.tsx
+++ src/components/releases/ReleaseChannelName.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-type Props = { children: React.ReactNode };
+type Props = {
+  children: React.ReactNode;
+  highlight?: boolean;
+};
 
-const ReleaseChannelName = ({ children }: Props) => (
-  <dt className="pf-v6-c-description-list__term pf-v6-u-mt-md">{children}</dt>
-);
+const ReleaseChannelName = ({ children, highlight }: Props) => {
+  const content = React.useMemo(() => children, [children]);
+
+  return (
+    <dt
+      className="pf-v6-c-description-list__term pf-v6-u-mt-md"
+      style={highlight ? { fontWeight: 700 } : undefined}
+    >
+      {content}
+    </dt>
+  );
+};
 
 export default ReleaseChannelName;
