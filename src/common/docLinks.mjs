175bcb058007ade3d227b5be05a6b4c55511fe1e
meirinetta <nmeiri@redhat.com>
2026-09-23 10:42:08 +0300

    OCMUI-5365: Fix broken ROSA external auth links (#737)

--- src/common/docLinks.mjs
+++ src/common/docLinks.mjs
@@ -79,6 +79,8 @@ const docLinks = {
   ROSA_AWS_FEDRAMP: `${ROSA_DOCS_BASE}/getting_started_with_red_hat_openshift_service_on_aws_in_aws_govcloud/index`,
   ROSA_HCP_CLI_URL: `${ROSA_DOCS_BASE}/install_clusters/rosa-hcp-sts-creating-a-cluster-quickly`,
   ROSA_CLI_DOCS: `${ROSA_DOCS_BASE}/cli_tools/rosa-cli`,
+  ROSA_HCP_EXT_AUTH: `${ROSA_DOCS_BASE}/install_clusters/rosa-hcp-sts-creating-a-cluster-ext-auth`,
+  ROSA_HCP_BREAK_GLASS: `${ROSA_DOCS_BASE}/install_clusters/rosa-hcp-sts-creating-a-cluster-ext-auth#rosa-hcp-sts-accessing-a-break-glass-cred-cli_rosa-hcp-sts-creating-a-cluster-ext-auth`,
   LEARN_MORE_SSO_ROSA: `${ROSA_DOCS_BASE}/cli_tools/rosa-cli#rosa-login-sso_rosa-getting-started-cli`,
   ROSA_AUTONODE: `${ROSA_DOCS_BASE}-single/cluster_administration/index#rosa-nodes-autonode-managing`,
   ROSA_HCP_SPOT_INTERRUPTION_SET_UP: `${ROSA_DOCS_BASE}/install_clusters/rosa-hcp-config-aws-spot-instances-cli`,
